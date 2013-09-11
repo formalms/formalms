5092f0fa6d92bc24c7b43edfb540d10103e00df6
Francesco Di Iorio <f.di.iorio@purplenetwork.it>
2013-09-11 13:58:53 +0000

    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@50 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/appLms/menu/menu_lat.php
+++ html/appLms/menu/menu_lat.php
@@ -71,186 +71,186 @@ if(!Docebo::user()->isAnonymous() && isset($_SESSION['idCourse'])) {
 
 	} // end while
 	
-	/*
+
 	// NUOVO MENU
 	if(isset($_SESSION['idCourse'])) {
-        $path = $GLOBALS['where_files_relative'].'/doceboLms/'.Get::sett('pathcourse');
-        $course_img 	= Docebo::course()->getValue('img_course');
-        
-        if($course_img != '') {
-            $logo = '<img class="course_logo" src="'.$path.$course_img.'" alt="'.Lang::t('_COURSE_LOGO', 'course').' : '.$course_name.'" />';
-        }
-    }
-	$menu_reborn = $logo.'<h1>'.Docebo::course()->getValue('name').'</h1>';
-	$menu_bottom = '<div id="menu-oriz">';
-	
-	$menu_right = '<ul id="float-right">';
-	
-	while(list($id_main, $menu) = each($menu_module)) {
-		if(!empty($menu['submenu'])) {
-		
-			$menu_right .= '<li><div class="menu-area'.( $_SESSION['current_main_menu'] == $id_main ? ' menu-selected' : '' ).'">';
-			$menu_right .= '<a href="#" rel="'.  str_replace(" ","_",strtolower($menu['main']['name'])).'">'.$menu['main']['name'].'</a></div></li>';
-			
-            $menu_left .= '<ul id="'.  str_replace(" ","_",strtolower($menu['main']['name'])).'" class="float-left '.( $_SESSION['current_main_menu'] == $id_main ? 'sub_visible' : 'sub_hide' ).'">';
-			
-			$count = count($menu['submenu']);
-			$i = 1;
-			while(list($id_sub, $sub) = each($menu['submenu'])) {
-				$active = '';
-				if ($id_sub == $_GET['id_module_sel'])
-					$active = ' class="active"';	
-				$menu_left .= '<li><a href="'.$sub['link'].'"'.$active.'>'.$sub['name'];
-				$i == $count ? $menu_left .= '</a></li>' : $menu_left .= '</a> | </li>';
-				$i++;
+            $path = $GLOBALS['where_files_relative'].'/doceboLms/'.Get::sett('pathcourse');
+            $course_img 	= Docebo::course()->getValue('img_course');
+            
+            if($course_img != '') {
+                $logo = '<img class="course_logo" src="'.$path.$course_img.'" alt="'.Lang::t('_COURSE_LOGO', 'course').' : '.$course_name.'" />';
             }
-            $menu_left  .= '</ul>';
-		}
-    }
-	$menu_right .= '</ul>';
+        }
+		$menu_reborn = $logo.'<h1>'.Docebo::course()->getValue('name').'</h1>';
+		$menu_bottom = '<div id="menu-oriz">';
 		
+		$menu_right = '<ul id="float-right">';
 		
-    // STATS
-    $user_stats = array('head'=>array(),'body'=>array());
-        if(!isset($_SESSION['is_ghost']) || $_SESSION['is_ghost'] !== true) {
+		while(list($id_main, $menu) = each($menu_module)) {
+			if(!empty($menu['submenu'])) {
+			
+				$menu_right .= '<li><div class="menu-area'.( $_SESSION['current_main_menu'] == $id_main ? ' menu-selected' : '' ).'">';
+				$menu_right .= '<a href="#" rel="'.  str_replace(" ","_",strtolower($menu['main']['name'])).'">'.$menu['main']['name'].'</a></div></li>';
+				
+                $menu_left .= '<ul id="'.  str_replace(" ","_",strtolower($menu['main']['name'])).'" class="float-left '.( $_SESSION['current_main_menu'] == $id_main ? 'sub_visible' : 'sub_hide' ).'">';
+				
+				$count = count($menu['submenu']);
+				$i = 1;
+				while(list($id_sub, $sub) = each($menu['submenu'])) {
+					$active = '';
+					if ($id_sub == $_GET['id_module_sel'])
+						$active = ' class="active"';
+					$menu_left .= '<li><a href="'.$sub['link'].'"'.$active.'>'.$sub['name'];
+					$i == $count ? $menu_left .= '</a></li>' : $menu_left .= '</a> | </li>';
+					$i++;
+	            }
+                $menu_left  .= '</ul>';
+			}
+        }
+		$menu_right .= '</ul>';
+		
+        //PURPLE STATS
+        $user_stats = array('head'=>array(),'body'=>array());
+            if(!isset($_SESSION['is_ghost']) || $_SESSION['is_ghost'] !== true) {
 
-            if(Docebo::course()->getValue('show_time') == 1) {
+                if(Docebo::course()->getValue('show_time') == 1) {
 
-                $tot_time_sec 		= TrackUser::getUserPreviousSessionCourseTime(getLogUserId(), $_SESSION['idCourse']);
-                $partial_time_sec 	= TrackUser::getUserCurrentSessionCourseTime($_SESSION['idCourse']);
-                $tot_time_sec  		+= $partial_time_sec;
+                    $tot_time_sec 		= TrackUser::getUserPreviousSessionCourseTime(getLogUserId(), $_SESSION['idCourse']);
+                    $partial_time_sec 	= TrackUser::getUserCurrentSessionCourseTime($_SESSION['idCourse']);
+                    $tot_time_sec  		+= $partial_time_sec;
 
-                $hours 		= (int)($partial_time_sec / 3600);
-                $minutes 	= (int)(($partial_time_sec % 3600) / 60);
-                $seconds 	= (int)($partial_time_sec % 60);
-                if($minutes < 10) $minutes = '0'.$minutes;
-                if($seconds < 10) $seconds = '0'.$seconds;
-                $partial_time = ( $hours != 0 ? $hours.'h ' : '' ).$minutes.'m ';//.$seconds.'s ';
+                    $hours 		= (int)($partial_time_sec / 3600);
+                    $minutes 	= (int)(($partial_time_sec % 3600) / 60);
+                    $seconds 	= (int)($partial_time_sec % 60);
+                    if($minutes < 10) $minutes = '0'.$minutes;
+                    if($seconds < 10) $seconds = '0'.$seconds;
+                    $partial_time = ( $hours != 0 ? $hours.'h ' : '' ).$minutes.'m ';//.$seconds.'s ';
 
-                $hours 		= (int)($tot_time_sec/3600);
-                $minutes 	= (int)(($tot_time_sec%3600)/60);
-                $seconds 	= (int)($tot_time_sec%60);
-                if($minutes < 10) $minutes = '0'.$minutes;
-                if($seconds < 10) $seconds = '0'.$seconds;
-                $tot_time = ( $hours != 0 ? $hours.'h ' : '' ).$minutes.'m ';//.$seconds.'s ';
+                    $hours 		= (int)($tot_time_sec/3600);
+                    $minutes 	= (int)(($tot_time_sec%3600)/60);
+                    $seconds 	= (int)($tot_time_sec%60);
+                    if($minutes < 10) $minutes = '0'.$minutes;
+                    if($seconds < 10) $seconds = '0'.$seconds;
+                    $tot_time = ( $hours != 0 ? $hours.'h ' : '' ).$minutes.'m ';//.$seconds.'s ';
 
-                Util::get_js(Get::rel_path('lms').'/lib/lib.track_user.js', true, true);
+                    Util::get_js(Get::rel_path('lms').'/lib/lib.track_user.js', true, true);
+                    $GLOBALS['page']->add(
+                        '<script type="text/javascript">'
+                        .'	userCounterStart('.(int)$partial_time_sec.', '.(int)$tot_time_sec.');'
+                        .'</script>'."\n"
+                    , 'page_head');
+
+
+                    $user_stats['head'][0] = Lang::t('_PARTIAL_TIME', 'course');
+                    $user_stats['body'][0] = '<span id="partial_time">'.$partial_time.'</span>';
+
+                    $user_stats['head'][1] = Lang::t('_TOTAL_TIME', 'course');
+                    $user_stats['body'][1] = '<span id="total_time">'.$tot_time.'</span>';
+                }
+
+            }
+            if(Docebo::course()->getValue('show_who_online') == _SHOW_INSTMSG) {
+
+                addCss('instmsg');
+                addJs($GLOBALS['where_lms_relative'].'/modules/instmsg/','instmsg.js');
+
                 $GLOBALS['page']->add(
                     '<script type="text/javascript">'
-                    .'	userCounterStart('.(int)$partial_time_sec.', '.(int)$tot_time_sec.');'
-                    .'</script>'."\n"
-                , 'page_head');
+                    ." setup_instmsg( '".Docebo::user()->getIdSt()."', "
+                    ."'".$userid."', "
+                    ."'".getPathImage('fw')."' ); "
+                    .'</script>'."\n", 'page_head');
 
+                $user_stats['head'][2] = Lang::t('_WHOIS_ONLINE', 'course');
+                $user_stats['body'][2] = '<b id="user_online_n">'
+                    .'<a id="open_users_list" href="javascript:void(0)">'
+                        .TrackUser::getWhoIsOnline($_SESSION['idCourse'])
+                    .'</a></b>';
 
-                $user_stats['head'][0] = Lang::t('_PARTIAL_TIME', 'course');
-                $user_stats['body'][0] = '<span id="partial_time">'.$partial_time.'</span>';
+            } elseif(Docebo::course()->getValue('show_who_online') == _SHOW_COUNT) {
 
-                $user_stats['head'][1] = Lang::t('_TOTAL_TIME', 'course');
-                $user_stats['body'][1] = '<span id="total_time">'.$tot_time.'</span>';
+                $user_stats['head'][2] = Lang::t('_WHOIS_ONLINE', 'course');
+                $user_stats['body'][2] = '<b id="user_online_n">'
+                        .TrackUser::getWhoIsOnline($_SESSION['idCourse'])
+                    .'</b>';
             }
 
+        $all_stats = '<div id="accordion"><h3>';
+        $all_stats .='<span class="tempo">'.Lang::t('_TOTAL_TIME', 'course').' : '.$tot_time.' ';
+        // print progress bar -------------------------------------------------
+        if(Docebo::course()->getValue('show_progress') == 1) {
+
+            require_once( $GLOBALS['where_lms'].'/lib/lib.stats.php' );
+            $total = getNumCourseItems( $_SESSION['idCourse'], 
+                                        FALSE, 
+                                        getLogUserId(), 
+                                        FALSE );
+            $tot_complete = getStatStatusCount(	getLogUserId(), 
+                                                $_SESSION['idCourse'],
+                                                array( 'completed', 'passed' ) );
+            $tot_failed = getStatStatusCount(	getLogUserId(), 
+                                                $_SESSION['idCourse'],
+                                                array( 'failed' ) );
+
+            $user_stats_table = '<table id="user_stats" class="quick_table">'
+            .'<thead><tr>'
+            .( isset($user_stats['head'][0]) ? '<th scope="col">'.$user_stats['head'][0].'</th>' : '' )
+            .( isset($user_stats['head'][1]) ? '<th scope="col">'.$user_stats['head'][1].'</th>' : '' )
+            .( isset($user_stats['head'][2]) ? '<th scope="col">'.$user_stats['head'][2].'</th>' : '' )
+            .'</tr></thead><tbody><tr>'
+            .( isset($user_stats['body'][0]) ? '<td>'.$user_stats['body'][0].'</td>' : '' )
+            .( isset($user_stats['body'][1]) ? '<td>'.$user_stats['body'][1].'</td>' : '' )
+            .( isset($user_stats['body'][2]) ? '<td>'.$user_stats['body'][2].'</td>' : '' )
+            .'</tr></tbody>'
+            .'</table>';
+
+            $all_stats .="- Progress : </span>".renderProgress($tot_complete, $tot_failed, $total, false);
         }
-        if(Docebo::course()->getValue('show_who_online') == _SHOW_INSTMSG) {
+        if(Docebo::course()->getValue('show_progress') == 1) {
 
-            addCss('instmsg');
-            addJs($GLOBALS['where_lms_relative'].'/modules/instmsg/','instmsg.js');
+            require_once( $GLOBALS['where_lms'].'/lib/lib.stats.php' );
+            $total = getNumCourseItems( $_SESSION['idCourse'], 
+                                        FALSE, 
+                                        getLogUserId(), 
+                                        FALSE );
+            $tot_complete = getStatStatusCount(	getLogUserId(), 
+                                                $_SESSION['idCourse'],
+                                                array( 'completed', 'passed' ) );
+            $tot_failed = getStatStatusCount(	getLogUserId(), 
+                                                $_SESSION['idCourse'],
+                                                array( 'failed' ) );
 
-            $GLOBALS['page']->add(
-                '<script type="text/javascript">'
-                ." setup_instmsg( '".Docebo::user()->getIdSt()."', "
-                ."'".$userid."', "
-                ."'".getPathImage('fw')."' ); "
-                .'</script>'."\n", 'page_head');
 
-            $user_stats['head'][2] = Lang::t('_WHOIS_ONLINE', 'course');
-            $user_stats['body'][2] = '<b id="user_online_n">'
-                .'<a id="open_users_list" href="javascript:void(0)">'
-                    .TrackUser::getWhoIsOnline($_SESSION['idCourse'])
-                .'</a></b>';
-
-        } elseif(Docebo::course()->getValue('show_who_online') == _SHOW_COUNT) {
-
-            $user_stats['head'][2] = Lang::t('_WHOIS_ONLINE', 'course');
-            $user_stats['body'][2] = '<b id="user_online_n">'
-                    .TrackUser::getWhoIsOnline($_SESSION['idCourse'])
-                .'</b>';
+            $course_stats_table .= '<table id="course_stats" class="quick_table">'
+            .'<thead><tr>'
+                .'<th scope="col">'.Lang::t('_PROGRESS_ALL', 'course').'</th>'
+                .'<th scope="col">'.Lang::t('_COMPLETED', 'course').'</th>'
+                .'<th scope="col">'.Lang::t('_PROGRESS_FAILED', 'course').'</th>'
+            .'</tr></thead><tbody><tr>'
+                .'<td>'.$total.'</td>'
+                .'<td>'.$tot_complete.'</td>'
+                .'<td>'.$tot_failed.'</td>'
+            .'</tr></tbody>'
+            .'</table>';	
         }
 
-    $all_stats = '<div id="accordion"><h3>';
-    $all_stats .='<span class="tempo">'.Lang::t('_TOTAL_TIME', 'course').' : '.$tot_time.' ';
-    // print progress bar -------------------------------------------------
-    if(Docebo::course()->getValue('show_progress') == 1) {
+        $course_stats_table .= "\n";
+		
+		$all_stats .= '<div id="arrow"></div><div class="clear"></div>';
+        $all_stats .= '</h3><div>'.$user_stats_table.''.$course_stats_table.'</div>';
 
-        require_once( $GLOBALS['where_lms'].'/lib/lib.stats.php' );
-        $total = getNumCourseItems( $_SESSION['idCourse'], 
-                                    FALSE, 
-                                    getLogUserId(), 
-                                    FALSE );
-        $tot_complete = getStatStatusCount(	getLogUserId(), 
-                                            $_SESSION['idCourse'],
-                                            array( 'completed', 'passed' ) );
-        $tot_failed = getStatStatusCount(	getLogUserId(), 
-                                            $_SESSION['idCourse'],
-                                            array( 'failed' ) );
+        $all_stats .='</div>';
+		
+		$menu_bottom .= $menu_left.$menu_right.'</div>';
+		
+		$menu_reborn .= $all_stats.'<div class="clear"></div>'.$menu_bottom;
 
-        $user_stats_table = '<table id="user_stats" class="quick_table">'
-        .'<thead><tr>'
-        .( isset($user_stats['head'][0]) ? '<th scope="col">'.$user_stats['head'][0].'</th>' : '' )
-        .( isset($user_stats['head'][1]) ? '<th scope="col">'.$user_stats['head'][1].'</th>' : '' )
-        .( isset($user_stats['head'][2]) ? '<th scope="col">'.$user_stats['head'][2].'</th>' : '' )
-        .'</tr></thead><tbody><tr>'
-        .( isset($user_stats['body'][0]) ? '<td>'.$user_stats['body'][0].'</td>' : '' )
-        .( isset($user_stats['body'][1]) ? '<td>'.$user_stats['body'][1].'</td>' : '' )
-        .( isset($user_stats['body'][2]) ? '<td>'.$user_stats['body'][2].'</td>' : '' )
-        .'</tr></tbody>'
-        .'</table>';
+        cout($menu_reborn,'menu');
 
-        $all_stats .="- Progress : </span>".renderProgress($tot_complete, $tot_failed, $total, false);
-    }
-    if(Docebo::course()->getValue('show_progress') == 1) {
-
-        require_once( $GLOBALS['where_lms'].'/lib/lib.stats.php' );
-        $total = getNumCourseItems( $_SESSION['idCourse'], 
-                                    FALSE, 
-                                    getLogUserId(), 
-                                    FALSE );
-        $tot_complete = getStatStatusCount(	getLogUserId(), 
-                                            $_SESSION['idCourse'],
-                                            array( 'completed', 'passed' ) );
-        $tot_failed = getStatStatusCount(	getLogUserId(), 
-                                            $_SESSION['idCourse'],
-                                            array( 'failed' ) );
-
-
-        $course_stats_table .= '<table id="course_stats" class="quick_table">'
-        .'<thead><tr>'
-            .'<th scope="col">'.Lang::t('_PROGRESS_ALL', 'course').'</th>'
-            .'<th scope="col">'.Lang::t('_COMPLETED', 'course').'</th>'
-            .'<th scope="col">'.Lang::t('_PROGRESS_FAILED', 'course').'</th>'
-        .'</tr></thead><tbody><tr>'
-            .'<td>'.$total.'</td>'
-            .'<td>'.$tot_complete.'</td>'
-            .'<td>'.$tot_failed.'</td>'
-        .'</tr></tbody>'
-        .'</table>';	
-    }
-
-    $course_stats_table .= "\n";
 	
-	$all_stats .= '<div id="arrow"></div><div class="clear"></div>';
-    $all_stats .= '</h3><div>'.$user_stats_table.''.$course_stats_table.'</div>';
-
-    $all_stats .='</div>';
-	
-	$menu_bottom .= $menu_left.$menu_right.'</div>';
-	
-	$menu_reborn .= $all_stats.'<div class="clear"></div>'.$menu_bottom;
-
-    cout($menu_reborn,'menu');
-	*/
 	// Vecchio menu (FINO ALLA FINE DEL CODICE)
 	// Print of the menu
-
+/*
 	cout('<div class="title_block">'
 			.'<h1>'.Docebo::course()->getValue('name').'</h1>'
 		.'</div>'
@@ -496,6 +496,6 @@ if(!Docebo::user()->isAnonymous() && isset($_SESSION['idCourse'])) {
 			.'</script>'."\n"
 		, 'scripts');
 	}
-
+*/
 	
 }
