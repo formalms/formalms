29b3e338a1e1c28b4f8225ae2f629994cee6adcd
Francesco Di Iorio <f.di.iorio@purplenetwork.it>
2013-09-16 16:00:04 +0000

    risolto #479
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@59 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/appLms/modules/htmlpage/htmlpage.php
+++ html/appLms/modules/htmlpage/htmlpage.php
@@ -21,15 +21,38 @@ function addpage($object_page) {
 	$lang =& DoceboLanguage::createInstance('htmlpage');
 	
 	$GLOBALS['page']->add(getTitleArea($lang->def('_SECT_PAGE'), 'htmlpage')
+		.'<script>'."\n"
+		.'my_n=1;'."\n"
+		.'function addAttachment() {'."\n"
+		.'	my_file = "attach"+my_n;'."\n"
+		.'	my_filevalue = \'\';'."\n"
+		.'	if (document.getElementById(my_file))'."\n"
+		.'		my_filevalue = document.getElementById(my_file).value;'."\n"
+		.'	my_html = "'.str_replace(array("\r", "\r\n", "\n"), '', addslashes(Form::getFilefield($lang->def('_UPLOAD'), 'attach%%', 'attach%%'))).'";'."\n"
+		.'	if (my_filevalue != \'\') {'."\n"
+		.'		my_n=my_n+1;'."\n"
+		.'		my_html = my_html.replace(/%%/gi,my_n);'."\n"
+		.'		newdiv = document.createElement("div");'."\n"
+		.'		newdiv.innerHTML = my_html;'."\n"
+		.'		my_divhtml = document.getElementById(\'attachment_area\');'."\n"
+		//.'		my_oldhtml = my_divhtml.innerHTML;'."\n"
+		.'		my_divhtml.appendChild(newdiv);'."\n"
+		.'	}'."\n"
+		.'}'."\n"
+		.'</script>'."\n"
 		.'<div class="std_block">'
 		.getBackUi( Util::str_replace_once('&', '&amp;', $object_page->back_url).'&amp;create_result=0', $lang->def('_BACK') )
 		
-		.Form::openForm('pageform', 'index.php?modname=htmlpage&amp;op=inspage')
+		.Form::openForm('pageform', 'index.php?modname=htmlpage&amp;op=inspage', false, false, 'multipart/form-data')
 		.Form::openElementSpace()
 		.Form::getHidden('back_url', 'back_url', htmlentities(urlencode($object_page->back_url)) )
 		
 		.Form::getTextfield($lang->def('_TITLE'), 'title', 'title', 150, $lang->def('_TITLE') )
 		.Form::getTextarea($lang->def('_TEXTOF'), 'textof', 'textof', $lang->def('_TEXTOF'))
+		.'<div id="attachment_area">'
+		.Form::getFilefield($lang->def('_UPLOAD'), 'attach1', 'attach1')
+		.'</div>'
+		.'<a href="javascript:addAttachment();">(+)</a>'
 		.Form::closeElementSpace()
 		.Form::openButtonSpace()
 		.Form::getButton('addhtmlpage', 'addhtmlpage', $lang->def('_INSERT'))
@@ -43,7 +66,7 @@ function inspage() {
 	checkPerm('view', false, 'storage');
 	
 	$back_url = urldecode($_POST['back_url']);
-	
+		
 	$insert_query = "
 	INSERT INTO ".$GLOBALS['prefix_lms']."_htmlpage
 	SET title = '".( (trim($_POST['title']) == '') ? Lang::t('_NOTITLE', 'htmlpage', 'lms') : $_POST['title'] )."',
@@ -55,6 +78,19 @@ function inspage() {
 		Util::jump_to( $back_url.'&create_result=0' );
 	}
 	list($idPage) = sql_fetch_row(sql_query("SELECT LAST_INSERT_ID()"));
+	
+	if ($_FILES) {
+		$n=0;		
+		foreach($_FILES as $_FILE) {
+			$n++;
+			$file = save_file($_FILE);
+			if($file) {
+				$insert_query = "INSERT INTO ".$GLOBALS['prefix_lms']."_htmlpage_attachment SET file = '".$file."', title = '".trim($_FILE['name'])."', idpage = ".$idPage;
+				sql_query($insert_query);
+			}
+		}		
+	}
+
 	Util::jump_to( $back_url.'&id_lo='.$idPage.'&create_result=1' );
 }
 
@@ -111,6 +147,28 @@ function uppage() {
 	Track_Object::updateObjectTitle($_POST['idPage'], 'htmlpage', $_POST['title']);
 	
 	Util::jump_to( $back_url.'&id_lo='.$_POST['idPage'].'&mod_result=1' );
+}
+
+function save_file($file) {
+	require_once(_base_.'/lib/lib.upload.php');
+
+	$path = '/appLms/htmlpages/';
+
+	if($file['name'] != '') {
+
+		$savefile = $_SESSION['idCourse'].'_'.rand(0,100).'_'.time().'_'.$file['name'];
+		if(!file_exists($GLOBALS['where_files_relative'].$path.$savefile)) {
+
+			sl_open_fileoperations();
+			if(!sl_upload($file['tmp_name'], $path.$savefile)) {
+
+				$savefile = '';
+			}
+			sl_close_fileoperations();
+			return $savefile;
+		}
+	}
+	return '';
 }
 
 // XXX: switch
