79f8f853009c872a33abcb9c2899330df19f733c
Francesco Di Iorio <f.di.iorio@purplenetwork.it>
2013-09-26 11:36:30 +0000

    id #478
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@85 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- db/478.sql
+++ db/478.sql
@@ -4,4 +4,4 @@ VALUES ('maintenance', 'off', 'enum', 3, 'security', 8, 25);
 
 INSERT INTO `core_setting` 
 (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`) 
-VALUES ('maintenance_pw', 'manutenzione', 'enum', 3, 'security', 8, 25);
+VALUES ('maintenance_pw', 'manutenzione', 'string', 3, 'security', 8, 25);
