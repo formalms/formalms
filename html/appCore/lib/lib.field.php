fa1f7053ea734e344712f88b4d6b210663da742c
Francesco Di Iorio <f.di.iorio@purplenetwork.it>
2013-09-27 15:15:29 +0000

    id #412
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@96 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/appCore/lib/lib.field.php
+++ html/appCore/lib/lib.field.php
@@ -162,7 +162,7 @@ class FieldList {
 
 		$query = "SELECT id_common, type_field, translation"
 				."  FROM ".$this->getFieldTable()
-				." WHERE lang_code = '".getLanguage()."' ";
+				." WHERE lang_code = '".$lang_code."' AND type_field != 'textlabel'";
 		if($type_field != false) {
 			$query .= " AND type_field = '".$type_field."'";
 		}
--- html/appCore/modules/field_manager/field_manager.php
+++ html/appCore/modules/field_manager/field_manager.php
@@ -34,9 +34,9 @@ function field_list() {
 
 	//find available field type
 	$re_field = sql_query("
-	SELECT type_field
-	FROM ".$GLOBALS['prefix_fw']."_field_type
-	 WHERE type_field != \"textfield\" ORDER BY type_field");
+	SELECT type_field FROM "
+	.$GLOBALS['prefix_fw']
+	."_field_type ORDER BY type_field");
 	$field_av = array();
 	$field_select = array('all_field' => $lang->def('_ALL_FIELD_TYPE'));
 	while(list($type_field) = sql_fetch_row($re_field)) {
