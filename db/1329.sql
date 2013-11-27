9959feaa76e9ee1e5bdc57acc4c9dccb038e6e41
Claudio Anelli <claudio.anelli@joint-tech.it>
2013-11-27 16:35:17 +0000

    risolto #1329: category permission for admins
    NOTA: nelle note di rilascio ricordiamoci di avvisare gli utenti che dovranno aggiornare i profili degli amministratori, altrimenti restano tutti senza permessi sulle cartelle
    
    Creato il file db/1329.sql con SQL.
    
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@176 5a8d0769-ed62-49d0-83b3-c9f762b3009b

---
+++ db/1329.sql
@@ -0,0 +1,16 @@
+INSERT INTO `core_role` (`idst`, `roleid`, `description`) VALUES(228, '/lms/admin/course/add_category', NULL);
+INSERT INTO `core_role` (`idst`, `roleid`, `description`) VALUES(229, '/lms/admin/course/mod_category', NULL);
+INSERT INTO `core_role` (`idst`, `roleid`, `description`) VALUES(230, '/lms/admin/course/del_category', NULL);
+
+
+INSERT INTO `learning_menu_under` (`idUnder`, `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`, `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path`) 
+VALUES
+(8, 1, 'coursecategory', '_COURSECATEGORY', '', 'view', NULL, 4, '', '', 'alms/coursecategory/show');
+
+
+INSERT INTO `core_role` (`idst`, `roleid`, `description`) VALUES(231, '/lms/admin/coursecategory/add', NULL);
+INSERT INTO `core_role` (`idst`, `roleid`, `description`) VALUES(232, '/lms/admin/coursecategory/mod', NULL);
+INSERT INTO `core_role` (`idst`, `roleid`, `description`) VALUES(233, '/lms/admin/coursecategory/del', NULL);
+
+INSERT INTO `core_lang_text` (`id_text` ,`text_key` ,`text_module` ,`text_attributes`) VALUES (NULL , '_COURSECATEGORY', 'standard', '');
+INSERT INTO `core_lang_translation` ( `id_text`, `lang_code`,  `translation_text`, `save_date` ) VALUES (LAST_INSERT_ID(), 'english', 'Course categories', now());
