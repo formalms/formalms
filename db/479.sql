7cd8786a843d1bfbcd9ecada6e8bcb67956c2a78
Claudio Anelli <claudio.anelli@joint-tech.it>
2014-01-22 16:45:59 +0000

    #479 : Default storage engine è Innodb . ATTENZIONE!!
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@223 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- db/479.sql
+++ db/479.sql
@@ -4,4 +4,4 @@ CREATE TABLE IF NOT EXISTS `learning_htmlpage_attachment` (
   `file` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
