16d7273591d003317306da52560f1e2769591c32
Claudio Anelli <claudio.anelli@joint-tech.it>
2014-01-22 16:47:53 +0000

    inserito clausola IGNORE in insert e update per poter eseguire run multipli dell'upgrade
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@224 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/upgrade/data/upg_data/1000_db.sql
+++ html/upgrade/data/upg_data/1000_db.sql
@@ -6,17 +6,17 @@
 --
 
 -- maintenance mode settings
-INSERT INTO `core_setting`
+INSERT IGNORE INTO `core_setting`
 (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`) VALUES
 ('maintenance', 'off', 'enum', 3, 'security', 8, 25);
 
-INSERT INTO `core_setting`
+INSERT IGNORE INTO `core_setting`
 (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`) VALUES
 ('maintenance_pw', 'manutenzione', 'string', 3, 'security', 8, 25);
 
 -- new field type
-INSERT INTO `core_field_type` (`type_field`, `type_file`, `type_class`) VALUES
+INSERT IGNORE INTO `core_field_type` (`type_field`, `type_file`, `type_class`) VALUES
 ('textlabel', 'class.label.php', 'Field_Textlabel');
 
 -- update key language
-UPDATE `core_lang_text` SET `text_key`= '_IMPORT_NOTHINGTOPROCESS' WHERE `text_key` = '_DOCEBO_IMPORT_NOTHINGTOPROCESS';
+UPDATE IGNORE `core_lang_text` SET `text_key`= '_IMPORT_NOTHINGTOPROCESS' WHERE `text_key` = '_DOCEBO_IMPORT_NOTHINGTOPROCESS';
