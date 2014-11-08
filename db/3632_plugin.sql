556be2af7cb57d297dd51e9b83711facb3e34dcb
Claudio Anelli <claudio.anelli@joint-tech.it>
2014-11-08 13:37:52 +0000

    bug #3632 : well format sql script
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@468 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- db/3632_plugin.sql
+++ db/3632_plugin.sql
@@ -1,16 +1,26 @@
-INSERT INTO `core_menu_under` (`idUnder`, `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`, `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path`) 
-VALUES(26, 3, 'pluginmanager', '_PLUGIN_MANAGER', '', 'view', NULL, 7, '', '', 'adm/pluginmanager/show');
+-- ---------------
+-- PLUGIN MANAGER
+-- ---------------
+
+
+INSERT INTO `core_menu_under` (`idUnder`, `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`, `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path`)
+VALUES(9, 3, 'pluginmanager', '_PLUGIN_MANAGER', '', 'view', NULL, 7, '', '', 'adm/pluginmanager/show');
 
 insert into core_role values(280, '/framework/admin/pluginmanager/view', '');
 insert into core_role_members values(280, 3);
-CREATE TABLE IF NOT EXISTS `core_plugin` (`plugin_id` int(10) NOT NULL AUTO_INCREMENT
-, `name` varchar(255) NOT NULL
-, `title` varchar(64) NOT NULL
-, `code` varchar(32) NOT NULL
-, `category` VARCHAR(255)
-, `version` varchar(16) NOT NULL
-, `author` varchar(128) NOT NULL
-, `link` varchar(255) NOT NULL
-, `priority` int(5) NOT NULL
-, `description` text NOT NULL
-, `active` int(1) NOT NULL, PRIMARY KEY (`plugin_id`), UNIQUE KEY `name` (`name`,`code`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `core_plugin` (
+  `plugin_id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `title` varchar(64) NOT NULL,
+  `code` varchar(32) NOT NULL,
+  `category` VARCHAR(255),
+  `version` varchar(16) NOT NULL,
+  `author` varchar(128) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `priority` int(5) NOT NULL,
+  `description` text NOT NULL,
+  `active` int(1) NOT NULL,
+  PRIMARY KEY (`plugin_id`),
+  UNIQUE KEY `name` (`name`,`code`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
