0f9617647245cdd79ec4f13e86afd565991de409
Claudio Anelli <claudio.anelli@joint-tech.it>
2015-02-16 11:31:56 +0000

    fix #3690:  reformat sql statement for insert language translation , change default text
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@564 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- db/3690.sql
+++ db/3690.sql
@@ -2,5 +2,24 @@
 INSERT INTO core_lang_text(text_key, text_module, text_attributes)VALUES ('_MAINTENANCE_TEXT', 'login', '');
 
 -- translation
-insert into core_lang_translation (id_text, lang_code, translation_text) values ((SELECT id_text FROM core_lang_text where text_key = '_MAINTENANCE_TEXT'), 'english', 'Mainteinance mode: To change these words please go to Admin/Language management, search for login and edit the key _MAINTENANCE_TEXT');
-insert into core_lang_translation (id_text, lang_code, translation_text) values ((SELECT id_text FROM core_lang_text where text_key = '_MAINTENANCE_TEXT'), 'italian', 'In Manutenzione: Per cambiare questa frase andare su Admin/Gestione lingue, cercare per LMS/login e modificare la chiave _MAINTENANCE_TEXT');
+INSERT IGNORE INTO core_lang_translation( id_text, lang_code, translation_text, save_date )
+SELECT clt.id_text, cll.lang_code,
+        'System in mainteinance mode.<br/><br/>To change these text please go to Admin/Language management, search for login and edit the key _MAINTENANCE_TEXT'
+        , now()
+FROM   core_lang_text clt, core_lang_language cll
+WHERE  text_key = '_MAINTENANCE_TEXT' and lang_code in ('english');
+
+INSERT IGNORE INTO core_lang_translation( id_text, lang_code, translation_text, save_date )
+SELECT clt.id_text, cll.lang_code,
+       'Sistema in manutenzione.<br/><br/>Per cambiare questa frase andare su Admin/Gestione lingue, cercare il modulo login e modificare la chiave _MAINTENANCE_TEXT'
+       , now()
+FROM   core_lang_text clt, core_lang_language cll
+WHERE  text_key = '_MAINTENANCE_TEXT' and lang_code in ('italian');
+
+INSERT IGNORE INTO core_lang_translation( id_text, lang_code, translation_text, save_date )
+SELECT clt.id_text, cll.lang_code,
+       'System in mainteinance mode.<br/><br/>To change these text please go to Admin/Language management, search for login and edit the key _MAINTENANCE_TEXT'
+       , now()
+FROM   core_lang_text clt, core_lang_language cll
+WHERE  text_key = '_MAINTENANCE_TEXT' and lang_code not in ('english','italian');
+
