398a5427462dfa4ae71cba28ee96800d350f8ee6
Claudio Anelli <claudio.anelli@joint-tech.it>
2015-02-16 14:29:44 +0000

    fix #3926 :  modifiche all'html generato come suggerito dalla community
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@568 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/appLms/modules/glossary/do.glossary.php
+++ html/appLms/modules/glossary/do.glossary.php
@@ -15,20 +15,20 @@ if(!Docebo::user()->isAnonymous()) {
 
 function play( $object_glos, $id_param ) {
 	//-kb-play-// if(!checkPerm('view', true, 'organization') && !checkPerm('view', true, 'storage')) die("You can't access");
-	
+
 	$lang =& DoceboLanguage::createInstance('glossary');
-	
+
 	$letter = importVar('letter', true, '');
 	$idGlossary = $object_glos->getId();
 	$back_coded = htmlentities(urlencode( $object_glos->back_url ));
 	$search = urldecode(importVar('search'));
-	
+
 	require_once(dirname(__FILE__).'/../../lib/lib.param.php' );
-	
+
 	require_once(_base_.'/lib/lib.form.php' );
-	
+
 	$idReference = getLOParam($id_param, 'idReference');
-	// NOTE: Track only if $idReference is present 
+	// NOTE: Track only if $idReference is present
 	if( $idReference !== FALSE ) {
 		require_once( dirname(__FILE__).'/../../class.module/track.glossary.php' );
 		list( $exist, $idTrack) = Track_Glossary::getIdTrack($idReference, getLogUserId(), $idGlossary, TRUE );
@@ -42,15 +42,15 @@ function play( $object_glos, $id_param ) {
 			$ti->createTrack( $idReference, $idTrack, getLogUserId(), date('Y-m-d H:i:s'), 'completed', 'glossary' );
 		}
 	}
-	
+
 	list($title) = sql_fetch_row(sql_query("
-	SELECT title 
-	FROM ".$GLOBALS['prefix_lms']."_glossary 
+	SELECT title
+	FROM ".$GLOBALS['prefix_lms']."_glossary
 	WHERE idGlossary = '".$idGlossary."'"));
-	
+
 	$termsQuery = "
 	SELECT idTerm, term
-	FROM ".$GLOBALS['prefix_lms']."_glossaryterm 
+	FROM ".$GLOBALS['prefix_lms']."_glossaryterm
 	WHERE idGlossary = '".$idGlossary."'";
 	if($search != '' && !isset($_POST['empty'])) {
 		$termsQuery .= " AND ( term LIKE '%".$search."%' OR description LIKE '%".$search."%' ) ";
@@ -58,50 +58,52 @@ function play( $object_glos, $id_param ) {
 	if( $letter != 0 ) $termsQuery .= " AND term LIKE '".chr($letter)."%'";
 	$termsQuery .= ' ORDER BY term';
 	$reTerms = sql_query($termsQuery);
-	
+
 	$page_title = array(
 		$lang->def('_GLOSSARY')
 	);
-	
+
 	$GLOBALS['page']->add(
 		getTitleArea($page_title, 'glossary')
 		.'<div class="std_block">'
 		.'<div class="colum_container">'
 		.getBackUi( $object_glos->back_url, $lang->def('_BACK') )
-		
+
 		.'<b>'.$lang->def('_GLOSSARY').' : '.$title.'</b>'
-		
+
 		.Form::openForm('glossary_play', 'index.php?modname=glossary&amp;op=play')
-		
+
 		.Form::getOpenFieldset($lang->def('_FILTER'))
 			.Form::getHidden('idGlossary', 'idGlossary', $idGlossary)
 			.Form::getHidden('idParams', 'idParams', $id_param)
 			.Form::getHidden('back_url', 'back_url', $back_coded)
-		
-		.Form::getTextfield($lang->def('_SEARCH'), 'search', 'search', 255, 
+
+		.Form::getTextfield($lang->def('_SEARCH'), 'search', 'search', 255,
 			( $search != '' && !isset($_POST['empty']) ? $search : '' ))
 		, 'content');
-	$GLOBALS['page']->add( '[ ', 'content');
+
+	// html enanchements by Roby Kirk
+	$GLOBALS['page']->add( '<div align="center">[ ', 'content');
 	//letter selection
 	for($i = 97; $i < 123; $i++) {
-		if($letter == $i) $GLOBALS['page']->add( '<span class="text_bold">(', 'content');
+		if($letter == $i) $GLOBALS['page']->add( '<span class="nav-current">', 'content');
 		$GLOBALS['page']->add( '<a href="index.php?modname=glossary&amp;op=play&amp;idGlossary='.$idGlossary.'&amp;idParams='.$id_param
 				.'&amp;letter='.$i.'&amp;back_url='.$back_coded.'">'.chr($i).'</a>', 'content');
-		
-		if($letter == $i) $GLOBALS['page']->add( ')</span>', 'content');
-		if($i < 122) $GLOBALS['page']->add( '-', 'content');
+
+		if($letter == $i) $GLOBALS['page']->add( '</span>', 'content');
+		if($i < 122) $GLOBALS['page']->add( ' ', 'content');
 	}
-	$GLOBALS['page']->add( '&nbsp;]&nbsp;[&nbsp;', 'content');
+	$GLOBALS['page']->add( ']<br/><br/>[ ', 'content');
 	for($i = 48; $i < 58; $i++) {
-		if($letter == $i) $GLOBALS['page']->add( '<span class="text_bold">(');
+		if($letter == $i) $GLOBALS['page']->add( '<span class="nav-current ">','content');
 		$GLOBALS['page']->add( '<a href="index.php?modname=glossary&amp;op=play&amp;idGlossary='.$idGlossary.'&amp;idParams='.$id_param
 				.'&amp;letter='.$i.'&amp;back_url='.$back_coded.'">'.chr($i).'</a>', 'content');
-		
-		if($letter == $i) $GLOBALS['page']->add( ')</span>', 'content');
-		if($i < 57) $GLOBALS['page']->add( '-', 'content');
+
+		if($letter == $i) $GLOBALS['page']->add( '</span>', 'content');
+		if($i < 57) $GLOBALS['page']->add( ' ', 'content');
 	}
-	$GLOBALS['page']->add( ' ] '
-		
+	$GLOBALS['page']->add( ']</div>'
+
 		.Form::getBreakRow()
 		.Form::openButtonSpace()
 		.Form::getButton('do_search', 'do_search', $lang->def('_SEARCH'))
@@ -127,9 +129,9 @@ function play( $object_glos, $id_param ) {
 	if( isset($_GET['idTerm']) ) {
 		list($term, $descr) = sql_fetch_row(sql_query("
 		SELECT term, description
-		FROM ".$GLOBALS['prefix_lms']."_glossaryterm 
+		FROM ".$GLOBALS['prefix_lms']."_glossaryterm
 		WHERE idTerm = '".(int)$_GET['idTerm']."'"));
-		
+
 		$GLOBALS['page']->add(
 			'<div class="boxinfo_title">'.$term.'</div>'
 			.'<div class="boxinfo_container">'
