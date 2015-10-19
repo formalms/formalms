3cd45d2fde68352fc20af306da09ffdf9dc7d5bc
Gianluigi Mammarella <gianluigi.mammarella@purplenetwork.it>
2015-10-19 23:34:22 +0000

    Refactoring dell'autoloader del TwigManager + Introduzione del componente VarDumper
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@675 5a8d0769-ed62-49d0-83b3-c9f762b3009b

---
+++ html/appCore/Template/TwigManager.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace appCore\Template;
+
+/* ======================================================================== \
+  |   FORMA - The E-Learning Suite                                            |
+  |                                                                           |
+  |   Copyright (c) 2013 (Forma)                                              |
+  |   http://www.formalms.org                                                 |
+  |   License  http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt           |
+  |                                                                           |
+  |   from docebo 4.0.5 CE 2008-2012 (c) docebo                               |
+  |   License http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt            |
+  \ ======================================================================== */
+
+class TwigManager {
+
+    private static $instance = null;
+    private $twig = null;
+
+    /**
+     * Singleton class, the constructor is private
+     */
+    private function __construct() {
+        $loader = new \Twig_Loader_Filesystem();
+        $this->twig = new \Twig_Environment($loader, array(
+            'cache' => _files_ . '/tmp',
+            'debug' => \Get::cfg('twig_debug', false)
+        ));
+        $this->twig->addFunction('translate', new \Twig_Function_Function(function ($key, $module = false, $substitution = array(), $lang_code = false, $default = false) {
+            return \Lang::t($key, $module, $substitution, $lang_code, $default);
+        }));
+        $this->twig->addFunction('pluginUrl', new \Twig_Function_Function(function ($resource) {
+            $plugin_files = \PluginManager::find_files();
+            return '/' . _folder_plugins_ . '/' . $plugin_files[$resource] . '/' . $resource;
+        }));
+        $this->twig->addFunction('evalPhp', new \Twig_Function_Function(function ($phpCode, $args = array()) {
+            return call_user_func_array($phpCode, $args);
+        }, array(
+            'is_safe' => array('html')
+        )));
+        $this->twig->addGlobal('GLOBALS', $GLOBALS);
+        $this->twig->addGlobal('Docebo', Docebo);
+    }
+
+    /**
+     * Get the TwigManager instance
+     *
+     * @param string $mvc_name
+     * @return TwigManager
+     * @throws Exception
+     */
+    public static function getInstance() {
+        if (self::$instance == null) {
+            $c = __CLASS__;
+            self::$instance = new $c;
+        }
+
+        return self::$instance;
+    }
+
+    public function render($view_name, $data_for_view, $view_path = null) {
+        if ($view_path == null) {
+            throw new \Exception('mvc_name cannot be null!');
+        }
+
+        if (!$this->twig->getLoader()->exists($view_name)) {
+            $this->twig->getLoader()->addPath($view_path);
+        }
+
+        return $this->twig->render($view_name, $data_for_view);
+    }
+
+}
