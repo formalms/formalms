671f33e88afa604787a2680e002cb4399a9d2058
Livio Melfi <l.melfi@grifomultimedia.it>
2015-12-16 08:58:18 +0000

    Forma 2.0: Addeg catalogue link on top menu
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@692 5a8d0769-ed62-49d0-83b3-c9f762b3009b

---
+++ db/4375.sql
@@ -0,0 +1,4 @@
+-- adding catalogue link onto the top menu
+insert into  learning_module (module_name, default_name, token_associated,module_info, mvc_path ) values ('course','_CATALOGUE','view','all','lms/catalog/show');
+insert   into learning_middlearea (obj_index,disabled,idst_list,sequence)  values ('mo_46',0,'a:0:{}',0);
+insert into learning_menucourse_under (idCourse, idModule, idMain,sequence) values (0,46,0,3);
