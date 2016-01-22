0ce86b70fb439ba58703ae5a0bd019323d7e0134
Davide Anceschi <davide.anceschi@joint-tech.it>
2016-01-22 14:14:16 +0000

    Corrette traduzioni.
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@770 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/appLms/admin/views/subscription/import_course.php
+++ html/appLms/admin/views/subscription/import_course.php
@@ -4,7 +4,7 @@
 	$_subs_url = '&id_course='.(int)$id_course.($id_edition ? '&id_edition='.(int)$id_edition : '').($id_date ? '&id_date='.(int)$id_date : '');
 	$title = array(	'index.php?r='.$this->link.'/show&amp;id_course='.$model->getIdCourse() => Lang::t('_COURSE', 'course'),
 					'index.php?r='.$this->link.'/show'.$_subs_url => Lang::t('_SUBSCRIBE', 'subscribe').' : '.$course_name,
-					Lang::t('_IMPORT_SUBSCRIPTION_FROM_COURSE', 'course'));
+					Lang::t('_IMPORT_FROM_COURSE', 'subscribe'));
 
 	echo	getTitleArea($title)
 			.'<div class="std_block">'
