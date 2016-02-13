5ee1adcf3562e63187df2fb60da871150be0e39f
Gianluigi Mammarella <gianluigi.mammarella@purplenetwork.it>
2016-02-13 00:49:43 +0000

    Fixato script di modifica del db
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@862 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- db/retain_answers_history.sql
+++ db/retain_answers_history.sql
@@ -3,7 +3,7 @@ ALTER TABLE learning_testtrack_answer ADD COLUMN number_time TINYINT(4) NULL DEF
 ALTER TABLE learning_test ADD COLUMN retain_answers_history TINYINT(1) NOT NULL DEFAULT '0' COMMENT '' AFTER obj_type;
 
 ALTER TABLE learning_testtrack_answer
-CHANGE COLUMN number_time number_time TINYINT(4) NOT NULL DEFAULT '1' COMMENT '' ,
+CHANGE COLUMN number_time number_time TINYINT(4) NOT NULL DEFAULT '1',
 DROP PRIMARY KEY,
-ADD PRIMARY KEY (idTrack, idQuest, idAnswer, number_time)  COMMENT '';
+ADD PRIMARY KEY (idTrack, idQuest, idAnswer, number_time);
 
