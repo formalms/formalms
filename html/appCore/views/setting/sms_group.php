114ba0fa235ac024cfccc88c8710cf45cabd081e
Gianluigi Mammarella <gianluigi.mammarella@purplenetwork.it>
2016-03-08 19:32:15 +0000

    Fix
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@899 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/appCore/views/setting/sms_group.php
+++ html/appCore/views/setting/sms_group.php
@@ -1,19 +1,35 @@
 <?php
 require_once(Docebo::inc(_adm_ . '/lib/Sms/SmsGatewayManager.php'));
-$credit = SmsGatewayManager::getCredit();
+try {
+    $credit = SmsGatewayManager::getCredit();
+} catch (SmsGatewayException $e) {
+    $credit = null;
+}
 ?>
 <div class="container-smsmarket">
-<!--	<p>-->
-<!--		<a href="http://www.skebby.it/" onclick="window.open(this.href); return false;" title="--><?php //echo Lang::t('_SKEBBY_LOGO', 'configuration'); ?><!--">-->
-<!--			<img src="--><?php //echo getPathImage(); ?><!--config/smsmarket.gif" alt="--><?php //echo Lang::t('_SMSMARKET_LOGO', 'configuration'); ?><!--" title="--><?php //echo Lang::t('_SMSMARKET_LOGO', 'configuration'); ?><!--" />-->
-<!--		</a>-->
-<!--	</p>-->
-	<p>
-		<b><?php echo Lang::t('_SMS_CREDIT', 'configuration').': '.$credit['credit_left']; ?></b><br>
-		<b><?php echo Lang::t('_SMS_CREDIT', 'configuration').': '.$credit[SkebbySmsGateway::SMS_TYPE_BASIC]; ?></b><br>
-		<b><?php echo Lang::t('_SMS_CREDIT', 'configuration').': '.$credit[SkebbySmsGateway::SMS_TYPE_CLASSIC]; ?></b>
-	</p>
-	<p>
-		<a href="http://www.skebby.it/" onclick="window.open(this.href); return false;"><?php echo Lang::t('_SMS_BUY_RECHARGE', 'configuration'); ?></a>
-	</p>
+    <!--	<p>-->
+    <!--		<a href="http://www.skebby.it/" onclick="window.open(this.href); return false;" title="-->
+    <?php //echo Lang::t('_SKEBBY_LOGO', 'configuration'); ?><!--">-->
+    <!--			<img src="--><?php //echo getPathImage(); ?><!--config/smsmarket.gif" alt="-->
+    <?php //echo Lang::t('_SMSMARKET_LOGO', 'configuration'); ?><!--" title="-->
+    <?php //echo Lang::t('_SMSMARKET_LOGO', 'configuration'); ?><!--" />-->
+    <!--		</a>-->
+    <!--	</p>-->
+    <p>
+        <?php
+        if ($credit != null) {
+            ?>
+            <b><?php echo Lang::t('_SMS_CREDIT', 'configuration') . ': ' . $credit['credit_left']; ?></b><br>
+            <b><?php echo Lang::t('_SMS_CREDIT', 'configuration') . ': ' . $credit[SkebbySmsGateway::SMS_TYPE_BASIC]; ?></b>
+            <br>
+            <b><?php echo Lang::t('_SMS_CREDIT', 'configuration') . ': ' . $credit[SkebbySmsGateway::SMS_TYPE_CLASSIC]; ?></b>
+            <?php
+        }
+        ?>
+    </p>
+
+    <p>
+        <a href="http://www.skebby.it/"
+           onclick="window.open(this.href); return false;"><?php echo Lang::t('_SMS_BUY_RECHARGE', 'configuration'); ?></a>
+    </p>
 </div>
