0724443de0525d1b703d5f2d72e8b6d3ffc6caca
Gianluigi Mammarella <gianluigi.mammarella@purplenetwork.it>
2016-03-11 10:14:31 +0000

    Impostato viewport
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@903 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/templates/standard/layout/lms.php
+++ html/templates/standard/layout/lms.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo Layout::lang_code(); ?>">
 	<head>
-    
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
