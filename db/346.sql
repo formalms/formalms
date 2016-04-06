b15b7e02fbc8fa6aa47a248cb6f75a8319691440
Gianluigi Mammarella <gianluigi.mammarella@purplenetwork.it>
2016-04-06 16:57:33 +0000

    Eliminato riferimento al db nello script sql
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@961 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- db/346.sql
+++ db/346.sql
@@ -1,2 +1,2 @@
-INSERT INTO `forma_20_purple`.`core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`) 
+INSERT INTO `core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`)
 VALUES ('orgchart_singlenode', 'off', 'enum', '3', '0', '3', '21', '1', '0', '');
