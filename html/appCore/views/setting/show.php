95d303c6121727acdf2b971e80998a2ebf135c0d
Andrea Margiovanni <andrea.margiovanni@purplenetwork.it>
2016-05-02 10:34:36 +0000

    Fix separazione tab content su pagina di configurazione
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@998 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/appCore/views/setting/show.php
+++ html/appCore/views/setting/show.php
@@ -4,15 +4,15 @@
 	<div id="global_conf" class="tabs-wrapper">
 		<ul class="nav nav-tabs bordered">
 			<li class="dropdown">
-				<a href="javascript:void(0)" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <span class="dropdown-title"><?php echo Lang::t('_MAIN_OPTIONS', 'configuration'); ?></span> &nbsp;<span class="caret"></span>
+				<a href="javascript:void(0)" class="dropdown-toggle" role="button" data-toggle="dropdown">
+          <span class="dropdown-title"><?php echo Lang::t('_OTHER_OPTION', 'course'); ?></span> &nbsp;<span class="caret"></span>
         </a>
         <ul class="dropdown-menu dropdown-menu-left tabs-dropdown">
 				<?php
 				while(list($id, $canonical_name) = each($regroup)) {
 					// echo '<li'.($id == $active_tab?' class="selected"':'').'>'
-					echo '<li'.($id == $active_tab?' class="active"':'').'>'
-						.'<a href="#tab_g_'.$id.'">'
+					echo '<li class="'.($id == $active_tab ? ' active' : '').'"">'
+						.'<a data-toggle="tab" href="#tab_g_'.$id.'">'
 						.'<em>'.Lang::t('_'.strtoupper($canonical_name), 'configuration').'</em>'
 						.'</a>'
 						.'</li>';
@@ -22,12 +22,12 @@
 				</ul>
 			</li>
 		</ul>
-		<div class="yui-content">
+		<div class="tab-content">
 			<?php
 			while(list($id, $canonical_name) = each($regroup)) {
 
 				// print the tab content
-				echo '<div id="tab_g_'.$id.'">'
+				echo '<div class="tab-pane'.($id == $active_tab ? ' active' : '').'" id="tab_g_'.$id.'">'
 					.'<h2>'.Lang::t('_'.strtoupper($canonical_name), 'configuration').'</h2>'
 					//.'<p>'.Lang::t('_CONF_DESCR_'.$id, 'configuration').'</p>'
 
