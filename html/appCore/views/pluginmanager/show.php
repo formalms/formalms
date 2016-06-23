0c8f52af3453f8408c9289f14d7941a8d60e7511
Giuseppe Nucifora <giuseppe.nucifora@purplenetwork.it>
2016-06-23 08:45:32 +0000

    - FIX #5928 impostato stile come pagina di configurazione (admin > pringipale > configurazione)
    git-svn-id: http://formalms.purplenetwork.it/svn/trunk@1017 5a8d0769-ed62-49d0-83b3-c9f762b3009b

--- html/appCore/views/pluginmanager/show.php
+++ html/appCore/views/pluginmanager/show.php
@@ -1,50 +1,16 @@
 <?php echo getTitleArea(Lang::t('_PLUGIN_LIST', 'configuration')); ?>
+
 <div class="std_block">
 
-    <div id="global_conf" class="yui-navset">
-        <!--<ul class="yui-nav">
-		<?php
-        /*while(list($id, $canonical_name) = each($plugins)) {
-            echo '<li'.((($plugins_info[$canonical_name]['id'] == $active_tab) || (!$active_tab && $id==0))?' class="selected"':'').'>'
-                .'<a href="#tab_g_'.$id.'">'
-                .'<em>'.Lang::t('_'.strtoupper($canonical_name), 'configuration').'</em>'
-                .'</a>'
-                .'</li>';
-        }
-        reset($plugins);*/
-        ?>
-		</ul>
-		<div class="yui-content">
-			<?php
-        /*
-            $model->plugins=$plugins;
-            while(list($id, $canonical_name) = each($plugins)) {
+    <div id="global_conf" class="tabs-wrapper">
 
-                // print the tab content
-                echo '<div id="tab_g_'.$id.'">'
-                    //.'<h2>'.Lang::t('_'.$plugins_info[strtoupper($canonical_name)][''], 'configuration').'</h2>'
-                    //.'<p>'.Lang::t('_CONF_DESCR_'.$id, 'configuration').'</p>'
-
-                    .Form::openForm('conf_option_'.$id, 'index.php?r=adm/pluginmanager/save');
-
-
-
-                $model->printPageWithElement($id, $canonical_name);
-
-                echo Form::closeElementSpace()
-                    .Form::openButtonSpace()
-                    .Form::getButton('save_config_'.$id, 'save_config', Lang::t('_SAVE', 'configuration'))
-                    .Form::getButton('undo_'.$id, 'undo', Lang::t('_UNDO', 'configuration'))
-                    .Form::closeButtonSpace()
-                    .Form::CloseForm()
-                    .'<br />'
-                    .'</div>';
-            }
-        */
-        ?>
-		</div>-->
-            <div id="tab_message">
-                <ul class="nav nav-tabs">
+        <ul class="nav nav-tabs bordered">
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropdown-toggle" role="button" data-toggle="dropdown">
+                    <span class="dropdown-title"><?php echo Lang::t('_OTHER_OPTION', 'course'); ?></span> &nbsp;<span
+                        class="caret"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-left tabs-dropdown">
                     <?php
                     while (list($id, $canonical_name) = each($plugins)) {
 
@@ -55,45 +21,44 @@
                     reset($plugins);
                     ?>
                 </ul>
-                <div class="tab-content">
-                    <?php
-                    $model->plugins=$plugins;
-                    while(list($id, $canonical_name) = each($plugins)) {
+            </li>
+        </ul>
+        <div class="tab-content">
+            <?php
+            $model->plugins = $plugins;
+            while (list($id, $canonical_name) = each($plugins)) {
 
-                        echo '<div id="tab_plugin_'.$id.'" class="tab-pane'.((($plugins_info[$canonical_name]['id'] == $active_tab) || (!$active_tab && $id == 0)) ? ' active' : '').'">'
-                            //.'<h2>'.Lang::t('_'.$plugins_info[strtoupper($canonical_name)][''], 'configuration').'</h2>'
-                            //.'<p>'.Lang::t('_CONF_DESCR_'.$id, 'configuration').'</p>'
+                echo '<div id="tab_plugin_' . $id . '" class="tab-pane' . ((($plugins_info[$canonical_name]['id'] == $active_tab) || (!$active_tab && $id == 0)) ? ' active' : '') . '">'
+                    //.'<h2>'.Lang::t('_'.$plugins_info[strtoupper($canonical_name)][''], 'configuration').'</h2>'
+                    //.'<p>'.Lang::t('_CONF_DESCR_'.$id, 'configuration').'</p>'
 
-                            .Form::openForm('conf_option_'.$id, 'index.php?r=adm/pluginmanager/save');
+                    . Form::openForm('conf_option_' . $id, 'index.php?r=adm/pluginmanager/save');
 
 
+                $model->printPageWithElement($id, $canonical_name);
 
-                        $model->printPageWithElement($id, $canonical_name);
+                echo Form::closeElementSpace()
+                    . Form::openButtonSpace()
+                    . Form::getButton('save_config_' . $id, 'save_config', Lang::t('_SAVE', 'configuration'))
+                    . Form::getButton('undo_' . $id, 'undo', Lang::t('_UNDO', 'configuration'))
+                    . Form::closeButtonSpace()
+                    . Form::CloseForm()
+                    . '<br />'
+                    . '</div>';
+            }
 
-                        echo Form::closeElementSpace()
-                            .Form::openButtonSpace()
-                            .Form::getButton('save_config_'.$id, 'save_config', Lang::t('_SAVE', 'configuration'))
-                            .Form::getButton('undo_'.$id, 'undo', Lang::t('_UNDO', 'configuration'))
-                            .Form::closeButtonSpace()
-                            .Form::CloseForm()
-                            .'<br />'
-                            .'</div>';
-                    }
-
-                    ?>
-                </div>
-            </div>
-
+            ?>
+        </div>
         <div class="nofloat">&nbsp;</div>
     </div>
 </div>
 <script type="text/javascript">
-    YAHOO.util.Event.onDOMReady(function () {
+    /*YAHOO.util.Event.onDOMReady(function () {
         var targets = YAHOO.util.Selector.query("span[id^=tt_target]");
         new YAHOO.widget.Tooltip("tooltip_info", {
             context: targets,
             effect: {effect: YAHOO.widget.ContainerEffect.FADE, duration: 0.20}
         });
         new YAHOO.widget.TabView('global_conf', {orientation: 'left'});
-    });
+    });*/
 </script>
