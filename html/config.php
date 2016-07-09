fa84b49f0ea16d01e724308964809a7e9c3d42c9
Gianluigi 'cocciagialla' Mammarella <cocciagialla@gmail.com>
2016-07-10 01:50:36 +0200

    Aggiunto file di config

--- html/config.php
+++ html/config.php
@@ -28,6 +28,7 @@ $cfg['db_pass'] = 't33-ppeo78';
 $cfg['db_name'] = 'formalms';
 $cfg['db_charset'] = 'utf8';
 
+
 /**
  * Tables prefix
  * -------------------------------------------------------------------------
@@ -45,6 +46,7 @@ $cfg['prefix_scs'] = 'conference';
 $cfg['prefix_ecom'] = 'ecom';
 $cfg['prefix_crm'] = 'crm';
 
+
 /**
  * File upload
  * -------------------------------------------------------------------------
@@ -57,56 +59,24 @@ $cfg['prefix_crm'] = 'crm';
  */
 $cfg['uploadType'] = 'fs';
 
+
 $cfg['ftphost']  = 'localhost';
 $cfg['ftpport']  = '21';
 $cfg['ftpuser']  = '';
 $cfg['ftppass']  = '';
 $cfg['ftppath']  = '/';
 
+
 /**
  * External smtp config
  * -------------------------------------------------------------------------
  */
 $cfg['use_smtp'] = 'off';
-$cfg['smtp_host'] ='';				// Options: hostname;hostname:port;...
-//$cfg['smtp_port'] ='';			// Options: '' (default port) , port number
-//$cfg['smtp_secure'] = '';			// Options: "", "ssl", "tls"
+$cfg['smtp_host'] ='';
+//$cfg['smtp_port'] = '';			// Options: '' (default port) , port number
+//$cfg['smtp_secure'] = '';			// Options: '', 'ssl', 'tls'
 $cfg['smtp_user'] ='';
 $cfg['smtp_pwd'] ='';
-
-/**
- * Other params
- * -------------------------------------------------------------------------
- * timezone     = default site timezone , if not specified get default from php.ini date.timezone
- *                for valid timezone see http://www.php.net/manual/en/timezones.php
- * set_mysql_tz = set mysql timezone same as php timezone ,  valid value
- *                true = set ,  false = (default) not set
- * keepalive    = set TMO for keepalive scorm tracking. must be < session lifetime, 0 to disable keepalive
- *                default session lifetime - 15 sec
- * enable_customscripts = enable custom scripts processing;  accepted vaule: true , false ; default false
- * enable_plugins = enable plugins processing;  accepted value: true , false ; default false
-*
- */
-
-//$cfg['timezone'] = 'Europe/Rome';		// define if different from php.ini setting
-//$cfg['set_mysql_tz'] = false;			// set mysql timezone same as php timezone , default false
-
-//$cfg['keepalivetmo'] = '';			// timeout for keepalive, must be < session lifetime, 0 to disable keepalive
-
-//$cfg['enable_customscripts'] = false;	// enable custom scripts processing;  accepted value: true , false ; default false
-//$cfg['enable_plugins']       = false; // enable plugins processing;  accepted value: true , false ; default false
-
-/**
- * Template engine custom param
- * -------------------------------------------------------------------------
- * add all template_engine enabled (if exists)
- * parameter :  array value=file extension
- * template_engine available: twig
- *
- * If not defined no alternate template engine. Twig is enabled on default
- */
-$cfg['template_engine']['twig'] = array('ext' => '.html.twig');
-$cfg['twig_debug'] = true;
 
 
 /**
@@ -121,6 +91,7 @@ $cfg['session_lenght']   = (120 * 60);
 $cfg['session_save_path']  = false;
 $cfg['demo_mode']   = false;
 
+
 /**
  * Technical preferences
  * -------------------------------------------------------------------------
@@ -130,19 +101,44 @@ $cfg['demo_mode']   = false;
 $cfg['filter_tool'] = 'htmlpurifier';
 $cfg['mail_br']  = "\r\n";
 
+
+
+
+/**
+ * Other params
+ * -------------------------------------------------------------------------
+ * timezone     = default site timezone , if not specified get default from php.ini date.timezone
+ *                for valid timezone see http://www.php.net/manual/en/timezones.php
+ * set_mysql_tz = set mysql timezone same as php timezone ,  valid value
+ *                true = set ,  false = (default) not set
+ */
+//$cfg['timezone'] = 'Europe/Rome';		//define if different from php.ini setting
+//$cfg['set_mysql_tz'] = false;			//set mysql timezone same as php timezone , default false
+
+
+
+//$cfg['keepalivetmo'] = '';			// timeout for keepalive, must be < session lifetime, 0 to disable keepalive
+
+$cfg['enable_customscripts'] = true;	// enable custom scripts processing;  accepted value: true , false ; default false
+
+$cfg['enable_plugins']       = true; // enable plugins processing;  accepted value: true , false ; default false
+
+/**
+ * Template engine custom param
+ * -------------------------------------------------------------------------
+ * add all template_engine enabled (if exists)
+ * array value=file extension
+ * template_engine available: twig
+ */
+$cfg['template_engine']['twig'] = array('ext' => '.html.twig');
+$cfg['twig_debug'] = true;
+
 /**
  * Certificate Encryption
  * -------------------------------------------------------------------------
- * certificate_encryption: boolean to set whether to enable or not the pdf encryption of certificates (default is TRUE)
+ * certificate_excryption: boolean to set whether to enable or not the pdf encryption of certificates (default is TRUE)
  * certificate_password: password to use when encryption is enabled. It must be exactly 32 chars! (default is a random generated one)
  */
-//$cfg['certificate_encryption'] = true;
-//$cfg['certificate_password']  = "12345678901234567890123456789012";
+$cfg['certificate_excryption'] = false;
+$cfg['certificate_password']  = "12345678901234567890123456789012";
 
-
-/**
- * Social Google Options
- * -------------------------------------------------------------------------
- * use_google_login_oauth2: boolean to set whether to enable or not the oauth2 login in google connection instead of openid (default is TRUE)
- */
-//$cfg['use_google_login_oauth2'] = false;
