e1544391a116159efac8e622d0be09bfe7342698
Giuseppe Nucifora <me@giuseppenucifora.com>
2016-08-24 12:46:11 +0200

    no message

---
+++ html/appLms/Events/Lms/UserListEvent.php
@@ -0,0 +1,42 @@
+<?php
+namespace appLms\Events\Lms;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class UserProfileShowEvent
+ * @package appLms\Events\Lms
+ */
+class UserProfileShowEvent extends Event
+{
+    const EVENT_NAME = 'lms.profileshow.event';
+    
+    /** @var null  */
+    protected $profile;
+
+    /**
+     * UserProfileShowEvent constructor.
+     */
+    public function __construct()
+    {
+        
+        $this->profile = NULL;
+    }
+
+    /**
+     * @param $profile
+     */
+    public function setProfile($profile)
+    {
+        $this->profile = $profile;
+    }
+
+    /**
+     * @return null
+     */
+    public function getProfile()
+    {
+        return $this->profile;
+    }
+
+}
