65fe9aed599e9da8f6f85b148a2b4d706a9f8abf
LucaRZ <l.rizzuti@grifomultimedia.it>
2016-11-11 16:49:09 +0100

    alter table learning_test

--- db/retain_answers_history.sql
+++ db/retain_answers_history.sql
@@ -1,5 +1,5 @@
 ALTER TABLE learning_testtrack_answer ADD COLUMN number_time TINYINT(4) NULL DEFAULT '1' COMMENT '' AFTER user_answer;
-      
+            
 ALTER TABLE learning_test ADD COLUMN retain_answers_history TINYINT(1) NOT NULL DEFAULT '0' COMMENT '' AFTER obj_type;
 
 ALTER TABLE learning_testtrack_answer
