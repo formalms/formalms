b4f92ce372e0a8efa80f48a335418b241ecdb68f
Giuseppe Nucifora <me@giuseppenucifora.com>
2016-12-20 15:26:17 +0100

    - Aggiunto evento per test360

---
+++ html/appLms/Events/Lms/TestCousereportEvent.php
@@ -0,0 +1,53 @@
+<?php
+namespace appLms\Events\Lms;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class TestCousereportEvent extends Event
+{
+    const EVENT_NAME = 'lms.test.coursereport.coursereport';
+
+    /**
+     * @var \Learning_Test
+     */
+    protected $object_test;
+    /**
+     * @var String
+     */
+    protected $overViewTestQuestionLink;
+
+    /**
+     * TestCreateEvent constructor.
+     * @param \Learning_Test $object_test
+     * @param $lang
+     */
+    public function __construct($object_test)
+    {
+        $this->object_test = $object_test;
+    }
+
+    /**
+     * @param String $overViewTestQuestionLink
+     */
+    public function setOverViewTestQuestionLink($overViewTestQuestionLink)
+    {
+        $this->overViewTestQuestionLink = $overViewTestQuestionLink;
+    }
+
+    /**
+     * @return String
+     */
+    public function getOverViewTestQuestionLink()
+    {
+        return $this->overViewTestQuestionLink;
+    }
+
+    /**
+     * @return \Learning_Test
+     */
+    public function getObjectTest()
+    {
+        return $this->object_test;
+    }
+
+}
