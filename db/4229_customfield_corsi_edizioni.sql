a79fadd5846f35d12ef5de6e41ef6712d92b7611
Massimiliano Cavazzini <max.cavazzini@joint-tech.it>
2017-03-03 09:35:29 +0100

    #4229 riportata anche gestione customfield per corsi e edizioni

---
+++ db/4229_customfield_corsi_edizioni.sql
@@ -0,0 +1,2 @@
+INSERT INTO `core_customfield_area` (`area_code`, `area_name`, `area_table`, `area_field`) VALUES('COURSE', 'Course', '%lms_course', 'idCourse');
+INSERT INTO `core_customfield_area` (`area_code`, `area_name`, `area_table`, `area_field`) VALUES('COURSE_EDITION', 'Course Edition', '%lms_course_editions', 'id_edition');
