4230c3e7bb53c8ea07cf49d41ac031283f5fe74c
Gabriele Marconetta <gabriele.marconetta@gmail.com>
2017-03-09 14:45:05 +0100

    pulizia codice

--- html/appLms/views/coursereport/js/testquestion.js
+++ html/appLms/views/coursereport/js/testquestion.js
@@ -13,24 +13,20 @@ window.TestQuestion = (function ($) {
       'id_test': config.idTest,
       'id_quest': config.idQuest
     };
-    var _ajaxCallPath = (config.type == 'extended-text') ? 'extendedQuestDetails' : 'fileUploadQuestDetails';
+    var _ajaxCallPath = (config.type === 'extended-text') ? 'extendedQuestDetails' : 'fileUploadQuestDetails';
 
     $.ajax({
       type: 'post',
       url: 'ajax.adm_server.php?r=lms/coursereport/' + _ajaxCallPath,
       data: _data,
-      beforeSend: function () {
-
-      },
       success: function (data) {
         var parsedData = JSON.parse(data);
-        console.log(parsedData);
         callback(parsedData);
 
         return parsedData;
       },
       error: function (e) {
-        $('.loading').html('errore: ' + e.message);
+        console.log('errore: ' + e.message);
         return false;
       }
     });
@@ -66,17 +62,16 @@ window.TestQuestion = (function ($) {
   var fetchQuestionDetails = function ($elem) {
     var _togglerActiveClass = 'test-question__show-more--active';
     var _tableContainerActiveClass = 'test-question__details-container--is-visible';
-    var _config = {};
     var $tableContainer = $elem.parents('table').find('.test-question__details-container');
 
     if (!$elem.hasClass('is-loaded')) {
-      _config.idTest = $elem.data('idtest');
-      _config.idQuest = $elem.data('idquest');
-      _config.type = $elem.data('type');
-
       loadQuestionDetails(function (data) {
         buildQuestionDetails(data, $elem);
-      }, _config);
+      }, {
+        idTest: $elem.prop('data-idtest'),
+        idQuest: $elem.prop('data-idquest'),
+        type: $elem.prop('data-type')
+      });
     }
 
     $tableContainer.hasClass(_tableContainerActiveClass) ? $tableContainer.removeClass(_tableContainerActiveClass) : $tableContainer.addClass(_tableContainerActiveClass);
