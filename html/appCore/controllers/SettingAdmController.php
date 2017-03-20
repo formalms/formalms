01f7761ead4bb8926a6dccd1f2e4494120a351a7
Giuseppe Nucifora <me@giuseppenucifora.com>
2017-03-20 20:08:21 +0100

    - Reset Twig

--- html/appCore/controllers/SettingAdmController.php
+++ html/appCore/controllers/SettingAdmController.php
@@ -53,6 +53,30 @@ class SettingAdmController extends AdmController {
 
 	}
 
+	public function clearTwigCache(){
+
+        $twigCacheDir = \appCore\Template\TwigManager::getCacheDir();
+
+        $this->rrmdir($twigCacheDir);
+
+        Util::jump_to('index.php?r=adm/setting/show');
+    }
+
+    function rrmdir($dir) {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (is_dir($dir."/".$object))
+                        $this->rrmdir($dir."/".$object);
+                    else
+                        unlink($dir."/".$object);
+                }
+            }
+            rmdir($dir);
+        }
+    }
+
 }
 
 ?>
