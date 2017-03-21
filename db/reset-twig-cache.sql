1526dc3fc4633f835963861e4abdece9ee683810
Giuseppe Nucifora <me@giuseppenucifora.com>
2017-03-21 10:14:10 +0100

    - rimosso /appCore/ nella query

--- db/reset-twig-cache.sql
+++ db/reset-twig-cache.sql
@@ -1,2 +1,2 @@
 INSERT INTO core_setting (param_name, param_value, value_type, max_size, pack, regroup, sequence, param_load, hide_in_modify, extra_info)
-											VALUES ('Clear_Twig_Cache','/appCore/index.php?r=adm/setting/clearTwigCache','button',2,'Twig Cache',13,0,0,0,'')
+											VALUES ('Clear_Twig_Cache','index.php?r=adm/setting/clearTwigCache','button',2,'Twig Cache',13,0,0,0,'')
