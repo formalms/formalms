f0c6927cc6a31359ea6a5376577546b8947ecbca
Gabriele Marconetta <gabriele.marconetta@gmail.com>
2017-04-05 13:42:26 +0200

    allineamento

--- html/appLms/views/elearning/courselist.php
+++ html/appLms/views/elearning/courselist.php
@@ -80,7 +80,7 @@ function TruncateText($the_text, $size)
             <div class="clearfix" id='mia_area_<?php echo $stato_corso; ?>'>
                 <?php foreach ($courselist as $course) : ?>
 
-                    <div class="col-xs-10 col-sm-6 col-lg-4 course-block">
+                    <div class="col-xs-10 col-sm-5 col-lg-4 course-block">
                         <div class="course-box"> <!-- NEW BLOCK -->
                             <div class="course-box__item">
                                 <div class="course-box__title course-icon--active"><?php echo TruncateText($course['name'], 100); ?></div>
