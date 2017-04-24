ae8145374825bd02e563b748748bc68c34ae5aae
Marco Castignoli <marco.castignoli@joint-tech.it>
2017-04-24 10:18:51 +0200

    #10479, #10484: mancavano le view secondarie del controller homepage. (da convertire in GIT)

---
+++ html/templates/standard/layout/appCore/homepage/register.php
@@ -0,0 +1,9 @@
+<?php echo getTitleArea(Lang::t("_REGISTER", "register"), "register") ?>
+<div class="std_block">
+    <?php
+    echo getBackUi("index.php", Lang::t("_BACK", "standard"));    
+    echo    Form::openForm("register", Get::rel_path("base") . "/index.php?r=" . _register_)
+              . $this->model->getRegisterForm()
+          . Form::closeForm();
+    ?>
+</div>
