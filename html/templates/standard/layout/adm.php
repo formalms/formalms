19eb15e8ec57a547e97e88c2acf0755e93fdf459
Gabriele Marconetta <gabriele.marconetta@gmail.com>
2017-05-15 16:25:02 +0200

    integrazione e sviluppo footer

--- html/templates/standard/layout/adm.php
+++ html/templates/standard/layout/adm.php
@@ -88,9 +88,7 @@
 		<!-- footer -->
 		<div id="footer" class="layout_footer">
 				<?php echo Layout::zone('footer'); ?>
-			<div class="copyright">
 				<?php echo Layout::copyright(); ?>
-			</div>
 		</div>
 
 		<!-- scripts -->
