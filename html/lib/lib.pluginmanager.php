99a18db16b084f6c0034aa15411826cf31bceab8
Marco Castignoli <marco.castignoli@joint-tech.it>
2017-05-17 09:46:02 +0200

    Il plugin system utilizzava una nuova funzione ottimizzata per controllare se il plugin è attivo o meno, questa però non controlla se nella cartella sono presenti plugin core.

--- html/lib/lib.pluginmanager.php
+++ html/lib/lib.pluginmanager.php
@@ -38,7 +38,7 @@ class PluginManager {
         if (empty(self::$plugin_list)) {
             require_once _adm_ . '/models/PluginmanagerAdm.php';
             $PluginmanagerAdm = new PluginmanagerAdm();
-            self::$plugin_list = $PluginmanagerAdm->getActivePlugins();
+            self::$plugin_list = $PluginmanagerAdm->getPlugins(true);
         }
         return self::$plugin_list;
     }
