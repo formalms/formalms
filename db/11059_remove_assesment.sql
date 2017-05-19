ed27d9c64781f7159760fa8a0df6a32f1dd8e924
LucaRZ <l.rizzuti@grifomultimedia.it>
2017-05-19 08:45:21 +0200

    ticket: 11059. remove assessment function

---
+++ db/11059_remove_assesment.sql
@@ -0,0 +1,11 @@
+delete from learning_middlearea where obj_index like 'tb_assessment';
+
+delete from core_menu_under_elearning where module_name like 'preassessment';
+
+-- cancellazione della cartella appLms/admin/modules/preassessment.*.*
+
+-- cancellazione del file appLms/controllers/AssessmentLmsController.php
+
+-- cancellazione del file appLms/models/AssessmentLms.php
+
+-- cancellazione cartella appLms/view/assessment/*.*
