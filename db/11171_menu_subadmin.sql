3d04faa9d9bf560b95da0de27db95d1d2ffa050d
Livio Melfi <l.melfi@grifomultimedia.it>
2017-05-24 12:58:19 +0200

    #11171: fixed

---
+++ db/11171_menu_subadmin.sql
@@ -0,0 +1,20 @@
+-- LABEL; QUEST_CATEGORY, PERIOD TIME
+UPDATE `core_menu_under_config` SET `of_platform` = 'lms' WHERE `core_menu_under_config`.`idUnder` = 31;
+
+UPDATE `core_menu_under_config` SET `of_platform` = 'lms' WHERE `core_menu_under_config`.`idUnder` = 1;
+
+UPDATE `core_menu_under_config` SET `of_platform` = 'lms' WHERE `core_menu_under_config`.`idUnder` = 33;
+
+
+-- AREA ELERNING
+UPDATE core_menu_under_elearning set default_op = 'lms';
+
+-- NEWSLETTER
+UPDATE core_menu_under_content SET of_platform = 'framework' WHERE module_name = 'newsletter';
+
+-- MENU BENVENUTO
+INSERT INTO `core_menu_config` (`idMenu`, `name`, `image`, `sequence`, `collapse`) VALUES
+(1, '', '', 1, 'true');
+
+INSERT INTO `core_menu_under_config` (`idUnder`, `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`, `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path`) VALUES
+(35, 1, 'dashboard', '_DASHBOARD', '', 'view', NULL, 1, '', '', 'adm/dashboard/show');
