940cfac5cf85bad5a11aa907a06dec8a141f6c97
Gianluigi 'cocciagialla' Mammarella <cocciagialla@gmail.com>
2017-06-08 13:04:02 +0200

    fixed #11371: Add option CUSTOM_FIELDS_MANDATORY_FOR_ADMIN to turn on the check of users's mandatory fields for the admin too

--- html/upgrade/data/upg_data/20000_db.sql
+++ html/upgrade/data/upg_data/20000_db.sql
@@ -1009,6 +1009,13 @@ DELETE FROM `learning_middlearea` WHERE `obj_index` = "tb_catalog";
 
 DELETE FROM `learning_middlearea` WHERE `obj_index` = "tb_assessment";
 
+-- 7996_custom_fields_mandatory_for_admin
+INSERT IGNORE INTO `core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`) VALUES ('custom_fields_mandatory_for_admin', 'off', 'enum', '3', 'register', '3', '21', '1', '0', '');
+
+INSERT IGNORE INTO `core_lang_text` (`text_key`, `text_module`, `text_attributes`) VALUES ('_CUSTOM_FIELDS_MANDATORY_FOR_ADMIN', 'configuration', '');
+
+INSERT IGNORE INTO `core_lang_translation` (`id_text`, `lang_code`, `translation_text`, `save_date`) VALUES ((SELECT `id_text` FROM `core_lang_text` ORDER BY `id_text` desc LIMIT 1), 'italian', 'Campi supplementari obbligatori anche per gli admin', '2017-01-13 13:50:05');
+
 -- ------------------------------------------------------------------
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
