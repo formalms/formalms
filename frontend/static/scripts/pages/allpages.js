af192b43827484c6bfc8c121a0127e5f59beb271
Gabriele Marconetta <gabriele.marconetta@gmail.com>
2017-06-15 16:31:56 +0200

    pulizia codice html e classi css

--- frontend/static/scripts/pages/allpages.js
+++ frontend/static/scripts/pages/allpages.js
@@ -1,18 +1,41 @@
 'use strict';
 
 var Page = (function () {
-
-	function Page() {
-		console.log('all page constructor');
+	
+	function setScroll(elem, action) {
+		if (action === 'lock') {
+			$(elem).addClass('no-scroll');
+		} else {
+			$(elem).removeClass('no-scroll');
+		}
 	}
 
-	Page.prototype.setData = function (data) {
-		console.log(data);
+	function setInteractions() {
+		$('.o-wrapper').on('click', function () {
+			console.log('lock');
+
+			if ($(this).hasClass('open')) {
+				setScroll('.header', 'unlock');
+			} else {
+				setScroll('.header', 'lock');
+			}
+		});
+
+	}
+
+	function Page() {
+
+		console.log('asd');
+
+		setInteractions();
+	}
+
+	Page.prototype.setData = function () {
+
 	};
 	
 	Page.prototype.load = function () {
-		document.body.classList.add('appIsReady');
-		console.log('all page load');
+
 	};
 
 	return new Page();
