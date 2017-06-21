d50fff06ce45b05b1dbc62854a7046bc64b55163
LucaRZ <l.rizzuti@grifomultimedia.it>
2017-06-21 17:00:22 +0200

    gestione dei tab attraverso plugin

--- html/appLms/admin/modules/middlearea/middlearea.php
+++ html/appLms/admin/modules/middlearea/middlearea.php
@@ -69,8 +69,20 @@ function view_area() {
 		'tb_games' => Lang::t('_CONTEST', 'middlearea'),
 		'tb_communication' => Lang::t('_COMMUNICATIONS', 'middlearea'),
 		'tb_videoconference' => Lang::t('_VIDEOCONFERENCE', 'middlearea'),
-		'tb_kb' => Lang::t('_CONTENT_LIBRARY', 'middlearea')
+		'tb_kb' => Lang::t('_CONTENT_LIBRARY', 'middlearea')  
 	);
+
+    
+ if(Get::cfg('enable_plugins', false)){
+        $pl = new PluginManager();
+        $list_pl = $pl->get_all_plugins();
+        
+        foreach ($list_pl as $key){
+            $plugin_name = strtolower ($key['name']);
+            $tab["tb_".$plugin_name] = Lang::t('_'.strtoupper ($key['name']), 'middlearea');
+        }     
+ }    
+    
 
 	$query_menu = "SELECT obj_index from %lms_middlearea where obj_index like 'tb_%' ORDER BY sequence";
 	$re_tablist = sql_query($query_menu);
