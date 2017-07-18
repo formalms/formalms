166ba82603d8d754b69818d29ffa3f84436d5173
Massimiliano Cavazzini <max.cavazzini@joint-tech.it>
2017-07-18 16:07:56 +0200

    Risolto #12651 - Report Utenti-Corsi (scelta campi aggiuntivi degli utenti)

--- html/appLms/admin/modules/report/class.report_user.php
+++ html/appLms/admin/modules/report/class.report_user.php
@@ -614,7 +614,7 @@ class Report_User extends Report {
 		$box->title = $lang->def('_SELECT_THE_DATA_COL_NEEDED');
 		$box->description = false;
 		//Form::openElementSpace()
-		if (count($custom) > 0) {
+
 		$box->body .= Form::getOpenFieldset($lang->def('_USER_CUSTOM_FIELDS'), 'fieldset_course_fields');
         $box->body .= Form::getCheckBox(Lang::t('_LASTNAME', 'standard'), 'col_sel_lastname', 'cols[]', '_TH_LASTNAME', is_showed('_TH_LASTNAME'));             
 		$box->body .= Form::getCheckBox(Lang::t('_FIRSTNAME', 'standard'), 'col_sel_firstname', 'cols[]', '_TH_FIRSTNAME', is_showed('_TH_FIRSTNAME'));
@@ -622,12 +622,13 @@ class Report_User extends Report {
 		$box->body .= Form::getCheckBox(Lang::t('_REGISTER_DATE', 'standard'), 'col_sel_register_date', 'cols[]', '_TH_REGISTER_DATE', is_showed('_TH_REGISTER_DATE'));
 		$box->body .= Form::getCheckBox(Lang::t('_SUSPENDED', 'standard'), 'col_sel_suspended', 'cols[]', '_TH_SUSPENDED', is_showed('_TH_SUSPENDED'));
 		$box->body .= Form::getCheckBox(Lang::t('_ORGCHART', 'standard'), 'col_sel_organization_chart', 'cols[]', '_TH_ORGANIZATION_CHART', is_showed('_TH_ORGANIZATION_CHART'));
-                
+		if (count($custom) > 0) {
 		foreach ($custom as $key=>$val) {
 			$box->body .= Form::getCheckBox($val['label'], 'col_custom_'.$val['id'], 'custom['.$val['id'].']', $val['id'], $ref['custom_fields'][$key]['selected']);
 		}
-		$box->body .= Form::getCloseFieldset();
 		}
+		$box->body .= Form::getCloseFieldset();
+
 
 
 		$out->add('<script type="text/javascript">
