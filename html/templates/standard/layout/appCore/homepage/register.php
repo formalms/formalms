9847be89585e7f0c4910f8cfd11aca03cbd8c4d2
Andrea Fiadone <andrea.fiadone@purplenetwork.it>
2017-08-01 18:35:05 +0200

    Sviluppo restyling struttura template homepage/login/registrazione

--- html/templates/standard/layout/appCore/homepage/register.php
+++ html/templates/standard/layout/appCore/homepage/register.php
@@ -1,9 +1,6 @@
-<?php echo getTitleArea(Lang::t("_REGISTER", "register"), "register") ?>
-<div class="std_block">
-    <?php
+<?php
     echo getBackUi("index.php", Lang::t("_BACK", "standard"));
     echo    Form::openForm("register", Get::rel_path("base") . "/index.php?r=" . _register_)
               . $this->model->getRegisterForm()
           . Form::closeForm();
     ?>
-</div>
