14fab8488c70729f74ab4c44c37cf2ca0b234f5f
Gabriele Marconetta <gabriele.marconetta@gmail.com>
2017-09-05 13:21:01 +0200

    avanzamenti lavorazioni catalogo esterno

--- html/appLms/views/homecatalogue/courselist.php
+++ html/appLms/views/homecatalogue/courselist.php
@@ -3,7 +3,7 @@ require_once(_lms_ . '/lib/lib.middlearea.php');
 $ma = new Man_MiddleArea();
 $path_course = $GLOBALS['where_files_relative'] . '/appLms/' . Get::sett('pathcourse') . '/';
 $smodel = new CatalogLms();
-
+$html = '';
 
 function TruncateText($the_text, $size)
 {
@@ -11,7 +11,6 @@ function TruncateText($the_text, $size)
         return substr($the_text, 0, $size) . '...';
     return $the_text;
 }
-
 ?>
 
 
@@ -23,43 +22,53 @@ function TruncateText($the_text, $size)
 
 
 <div class="col-md-12">
-<!--    <div class="main">-->
-<!--        <div id="cbp-vm" class="cbp-vm-switcher cbp-vm-view-grid">-->
+    <div class="main">
+        <div id="cbp-vm" class="cbp-vm-switcher cbp-vm-view-grid" style="padding: 0;">
 <!--            <ul>-->
+<!--    <pre>--><?php //var_dump($row); ?><!--</pre>-->
     <div class="row">
         <?php
 
         while ($row = sql_fetch_assoc($result)) {
+//            echo '<pre>'. json_encode($row) . '</pre>';
+//            echo '<pre>'. var_dump($result) . '</pre>';
             $action = '';
             if ($row['course_type'] === 'classroom') {
                 $classrooms = $smodel->classroom_man->getCourseDate($row['idCourse'], false);
                 if (count($classrooms) > 0) {
-                    $action = '<div class="course-box__item" style="top:5px;" id="action_' . $row['idCourse'] . '">'
+//                    $action = '<div class="catalog_action" style="top:5px;" id="action_' . $row['idCourse'] . '">'
+//                        . '<a href="javascript:;" onclick="courseSelection(\'' . $row['idCourse'] . '\', \'0\')" title="' . Lang::t('_SHOW_EDITIONS', 'catalogue') . '"><p class="can_subscribe">' . Lang::t('_SHOW_EDITIONS', 'catalogue') . '</p></a></div>';
+
+
+                    $action = '<div class="course-box__item" id="action_' . $row['idCourse'] . '">'
                         . '<a class="forma-button forma-button--green forma-button--orange-hover" href="javascript:;" onclick="courseSelection(\'' . $row['idCourse'] . '\', \'0\')" title="' . Lang::t('_SHOW_EDITIONS', 'catalogue') . '">'
                         . '<span class="forma-button__label">' . Lang::t('_SHOW_EDITIONS', 'catalogue') . '</span>'
                         . '</a>'
                         . '</div>';
                 } else {
                     $action = '
+                        <div class="course-box__item">
                             <a class="forma-button forma-button--disabled">
                                 <span class="forma-button__label">' . Lang::t('_NO_AVAILABLE_EDITIONS', 'catalogue') . '</span>
-                            </a>';
+                            </a>
+                        </div>';
                 }
-
             } else {
+                $login_link = '<a class="forma-button forma-button--green forma-button--orange-hover" href="index.php"> Login</a>';
+//                $action = '<div class="can_subscribe" style="top:5px;" id="action_' . $row['idCourse'] . '">'
+//                    . str_replace(array('[login]', '[signin]'), array($login_link, $signin_link), ($register_type === 'self' || $register_type === 'self_optin' || $register_type === 'moderate' ? Lang::t('_REGISTER_FOR_COURSE', 'login') : Lang::t('_REGISTER_FOR_COURSE_NO_REG', 'login')))
+//                    . '</div>';
 
-                $login_link = '<a href="index.php"> Login</a>';
-                $action = '<div class="can_subscribe" style="top:5px;" id="action_' . $row['idCourse'] . '">'
+                $action = '
+                    <div class="course-box__item" id="action_' . $row['idCourse'] . '">'
                     . str_replace(array('[login]', '[signin]'), array($login_link, $signin_link), ($register_type === 'self' || $register_type === 'self_optin' || $register_type === 'moderate' ? Lang::t('_REGISTER_FOR_COURSE', 'login') : Lang::t('_REGISTER_FOR_COURSE_NO_REG', 'login')))
                     . '</div>';
-
             }
 
             $arr_cat = $smodel->getMinorCategoryTree((int)$row['idCategory']);
 
-
-            if ($row['course_type'] == "elearning") $img_type_ico = "<span class='elearning'><i class='fa fa-graduation-cap'></i>&nbsp;" . Lang::t('_LEARNING_COURSE', 'cart') . "</span>";
-            if ($row['course_type'] == "classroom") $img_type_ico = "<span class='classroom'><i class='fa fa-university'></i>&nbsp;" . Lang::t('_CLASSROOM_COURSE', 'cart') . "</span>";
+            if ($row['course_type'] == "elearning") $img_type_ico = "<span class='elearning'><i class='fa fa-graduation-cap'></i>" . Lang::t('_LEARNING_COURSE', 'cart') . "</span>";
+            if ($row['course_type'] == "classroom") $img_type_ico = "<span class='classroom'><i class='fa fa-university'></i>" . Lang::t('_CLASSROOM_COURSE', 'cart') . "</span>";
             // start - end
 
             $str_start_end = "";
@@ -113,7 +122,7 @@ function TruncateText($the_text, $size)
 //                                    </div>
 //                                </div> ';
 
-            $html = '
+            $html .= '
                 <div class="col-xs-offset-1 col-xs-10 col-md-offset-0 col-md-6">
                     <div class="course-box">
                         <div class="course-box__item">
@@ -129,29 +138,66 @@ function TruncateText($the_text, $size)
             }
 
             $html .= '
-                            <div class="course-box__img-title">' . $img_type . '</div>
-                        </div>    
-                    </div>
-                    <div class="course-box__item">
-                        <div class="course-box__desc">
-                            ' . TruncateText($row['box_description'], 120) . '
+                                <div class="course-box__img-title">' . $img_type_ico . '</div>
+                            </div>
                         </div>
-                    </div>';
+                        <div class="course-box__item">
+                            <div class="course-box__desc">
+                                ' . TruncateText($row['box_description'], 120) . '
+                            </div>
+                        </div>';
 
-            $strClassStyle = 'style="background-color: transparent;"';
-            if ($data_inizio != "0000-00-00" && $data_end != "0000-00-00") $strClassStyle = "";
+//            $strClassStyle = 'style="background-color: transparent;"';
+//            if ($data_inizio != "0000-00-00" && $data_end != "0000-00-00") $strClassStyle = "";
+//
+//            $html .= '<div class="edizioni_cal cat" ' . $strClassStyle . '>'
+//                . ($data_inizio != "0000-00-00" && $data_end != "0000-00-00" ?
+//                    '<a href="#" class="tooltips" id="classe_data_start" title="INIZIO"><div class="edizioni_start cat"><i class="fa  fa-2x fa-calendar-check-o" aria-hidden="true"></i>' . $data_inizio_format . ' </a></div>
+//                        <a href="#" class="tooltips" id="classe_data_end" title="FINE"><div class="edizioni_end cat"><i class="fa fa-2x fa-calendar-times-o" aria-hidden="true"></i>' . $data_end_format . ' </a></div>
+//                        <div style="clear:both"></div>' : '')
+//                . ($data_inizio == "0000-00-00" && $data_end == "0000-00-00" ? '' : '')
+//                . ($data_inizio != "0000-00-00" && $data_end == "0000-00-00" ? '
+//                        <a href="#" class="tooltips" id="classe_data_start" title="INIZIO"><div class="edizioni_start cat"><i class="fa  fa-2x fa-calendar-check-o" aria-hidden="true"></i>' . $data_inizio_format . ' </a></div><div style="clear:both"></div>' : '')
+//                . ($data_inizio == "0000-00-00" && $data_end != "0000-00-00" ? '
+//                        <a href="#" class="tooltips" id="classe_data_end" title="FINE"><div class="edizioni_end cat"><i class="fa  fa-2x fa-calendar-check-o" aria-hidden="true"></i>' . $data_end_format . ' </a></div><div style="clear:both"></div>' : '') .
+//                '</div>';
 
-            $html .= '  <div class="edizioni_cal cat" ' . $strClassStyle . '>'
-                . ($data_inizio != "0000-00-00" && $data_end != "0000-00-00" ?
-                    '<a href="#" class="tooltips" id="classe_data_start" title="INIZIO"><div class="edizioni_start cat"><i class="fa  fa-2x fa-calendar-check-o" aria-hidden="true"></i>' . $data_inizio_format . ' </a></div>
-                        <a href="#" class="tooltips" id="classe_data_end" title="FINE"><div class="edizioni_end cat"><i class="fa fa-2x fa-calendar-times-o" aria-hidden="true"></i>' . $data_end_format . ' </a></div>
-                        <div style="clear:both"></div>' : '')
-                . ($data_inizio == "0000-00-00" && $data_end == "0000-00-00" ? '' : '')
-                . ($data_inizio != "0000-00-00" && $data_end == "0000-00-00" ? '
-                        <a href="#" class="tooltips" id="classe_data_start" title="INIZIO"><div class="edizioni_start cat"><i class="fa  fa-2x fa-calendar-check-o" aria-hidden="true"></i>' . $data_inizio_format . ' </a></div><div style="clear:both"></div>' : '')
-                . ($data_inizio == "0000-00-00" && $data_end != "0000-00-00" ? '
-                        <a href="#" class="tooltips" id="classe_data_end" title="FINE"><div class="edizioni_end cat"><i class="fa  fa-2x fa-calendar-check-o" aria-hidden="true"></i>' . $data_end_format . ' </a></div><div style="clear:both"></div>' : '') .
-                '</div>';
+            $startDate = $data_inizio == '0000-00-00' ? false : true;
+            $endDate = $data_end == '0000-00-00' ? false : true;
+
+            if ($startDate && $endDate) { //ci sono le date di inizio e fine
+                $html .= '
+                            <div class="course-box__item">
+                                <div class="course-box__date-box calendar-icon--check">' . $data_inizio_format . '</div>
+                                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                <div class="course-box__date-box course-box__date-box--end calendar-icon--green-cross">' . $data_end_format . '</div>
+                            </div>
+                       ';
+            } elseif ($startDate && !$endDate) { //c'è solo la data di inizio
+                $html .= '
+                            <div class="course-box__item">
+                                <div class="course-box__date-box calendar-icon--check">' . $data_inizio_format . '</div>
+                                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                <div class="course-box__date-box course-box__date-box--no-date calendar-icon--gray-cross"></div>
+                            </div>
+                       ';
+            } elseif (!$startDate && $endDate) { //c'è solo la data di fine
+                $html .= '
+                            <div class="course-box__item">
+                                <div class="course-box__date-box course-box__date-box--no-date calendar-icon--check"></div>
+                                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                <div class="course-box__date-box course-box__date-box--no-date calendar-icon--green-cross">' . $data_end_format . '</div>
+                            </div>
+                       ';
+            } else { // non ci sono date, mostro i box vuoti
+                $html .= '
+                            <div class="course-box__item">
+                                <div class="course-box__date-box course-box__date-box--no-date calendar-icon--check"></div>
+                                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                <div class="course-box__date-box course-box__date-box--no-date calendar-icon--gray-cross"></div>
+                            </div>
+                       ';
+            }
 
             if ($row["course_demo"]) {
                 $html .= '<!-- DATE START - DATE END  -->
@@ -167,27 +213,28 @@ function TruncateText($the_text, $size)
                                        </div>
                                  </div>';
             }
-            $html .= '<div class="cbp-vm-add">                                   
-                <div>';
+//            $html .= '<div class="cbp-vm-add">
+//                <div>';
             if ($str_can_enter == true && $row['status'] != CST_CONCLUDED) $html .= $action;
             if ($str_can_enter == false || $row['status'] == CST_CONCLUDED) $html .= "<div class='lock cat'><i class='fa fa-3x fa-lock' aria-hidden='true'></i></div>";
 
             // in caso di corso a tempo, l utente deve potersi iscrivere, se non iscritto
             if (($row['subscribe_method'] == 2 || $row['subscribe_method'] == 1) && $str_can_enter == false && strrpos($action, "subscribed") == false) $html .= $action;
 
-//                    $html .= ' </div>
-//                     </div>
-//                        </li>';
+                    $html .= '</div>
+                           </div>
+                                         
+                      '; //</li>
 
 
         }
 
 
-        if (sql_num_rows($result) <= 0)
+        if (sql_num_rows($result) <= 0) {
             $html = '<p>' . Lang::t('_NO_CONTENT', 'standard') . '</p>';
+        }
 
         echo $html;
-
 
         ?>
     </div>
@@ -195,8 +242,10 @@ function TruncateText($the_text, $size)
 
 
 <!--        </div>-->
-<!--    </div><!-- /main -->
+<!--    </div><!-- /main-->
 
 
+</div>
+</div>
 </div>
 
