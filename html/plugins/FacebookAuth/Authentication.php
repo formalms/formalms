00c90a20479f8149633b3f443a0a4cf4db2b0ebb
Giuseppe Nucifora <me@giuseppenucifora.com>
2017-09-20 17:00:46 +0200

    - Spostata logica di costrutione dell'html delle pagine esterne nel template più che nel controller - Distinte tipologie di form login per spostare i social login nell'interfaccia a piacere.

--- html/plugins/FacebookAuth/Authentication.php
+++ html/plugins/FacebookAuth/Authentication.php
@@ -45,9 +45,8 @@ class Authentication extends \PluginAuthentication implements \PluginAuthenticat
             $url = $facebook_service->getAuthorizationUri();
 
             $form =  "<a href='" . $url . "'>"
-				  // . Get::img("social/facebook-24.png")
-					. '<i class="fa fa-facebook"></i>'
-                  	. "</a>";
+                      . Get::img("social/facebook-24.png")
+                  . "</a>";
         }
 
         return [
