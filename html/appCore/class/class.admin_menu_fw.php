538c8daa9cd6d25e0c67aac25c218c2288bd51ac
davide anceschi <davide.anceschi@joint-tech.it>
2017-10-10 12:18:30 +0200

    Prima versione dopo revisione del menu NOTA: - non viene usata la classe class.admin_menu_fw - sistemare menu_over per la stampa del menu (rimuovere voce "area_clienti") - pulizia (nomi variabili, ecc. TODO: CONTROLLARE AdminrulesAdm WIP

--- html/appCore/class/class.admin_menu_fw.php
+++ html/appCore/class/class.admin_menu_fw.php
@@ -28,7 +28,51 @@ class Admin_Framework extends Admin {
 	 * @return	nothing
 	 * @access public
 	 */
-	function Admin_Framework(&$user) {
+function reorder() {
+    $asrc = $this->menu;
+    $adst = array();
+
+    if (is_array($asrc)) {
+        foreach ($asrc as $key=>$value) {
+            if (is_array($value)) {
+                $adst[$key] = $this->arrorder($value);
+            }
+        }
+    }
+$_a=$adst;
+    $this->menu = $adst;
+    
+}
+
+function arrorder($asrc = array(), $idparent = false) {
+    $adst = array();
+    if (is_array($asrc)) {
+        //get target level
+        foreach ($asrc as $key=>$value) {
+            if (is_array($value)) {
+                $_idparent = $value['idparent'];
+                if ($_idparent == $idparent){
+                    $adst[$key] = $value;
+                }
+            }
+        }
+        //exclude elements got
+        foreach ($asrc as $key=>$value) {
+            if (array_key_exists($key, $adst)) {
+                unset($asrc[$key]);
+            }
+        }
+        //inspect below levels
+        foreach ($adst as $key=>$value) {
+            $aunder = $this->arrorder($asrc, $key);
+            if (is_array($aunder) && ! empty($aunder)) {
+                $adst[$key]['under'] = $aunder;
+            }
+        }
+    }
+    return $adst;
+}
+        function Admin_Framework(&$user) {
 		$this->user =& $user;
 		$this->platform = 'framework';
 		$this->table_level_one = $GLOBALS['prefix_fw'].'_menu';
@@ -37,7 +81,7 @@ class Admin_Framework extends Admin {
 		$lang =& DoceboLanguage::createInstance('menu', $this->platform);
 
 		$query_menu = "
-		SELECT idMenu, idUnder, module_name, default_name, default_op, associated_token, of_platform, mvc_path
+		SELECT idMenu, idUnder, module_name, default_name, default_op, associated_token, of_platform, mvc_path, idParent
 		FROM ".$this->table_level_two."
 		WHERE 1
 		ORDER BY sequence";
@@ -45,7 +89,7 @@ class Admin_Framework extends Admin {
 
 
 		$this->menu = array();
-		while(list($idm, $id, $module_name, $name, $op, $token, $of_platform, $mvc_path) = sql_fetch_row($re_menu)) {
+		while(list($idm, $id, $module_name, $name, $op, $token, $of_platform, $mvc_path, $idparent) = sql_fetch_row($re_menu)) {
 
 			if($this->user->matchUserRole('/'.( $of_platform === NULL ? $this->platform : $of_platform ).'/admin/'.$module_name.'/'.$token)) {
 				 $this->menu[$idm][$id] = array('modname' => $module_name,
@@ -55,9 +99,12 @@ class Admin_Framework extends Admin {
 										: 'index.php?r='.$mvc_path
 									),
 									'name' => ( $name != '' ? $lang->def($name) : $lang->def('_'.strtoupper($module_name)) ),
-									'of_platform' => ( $of_platform === NULL ? $this->platform : $of_platform ) );
+									'of_platform' => ( $of_platform === NULL ? $this->platform : $of_platform ) 
+									, 'idparent' => $idparent 
+                                                                );
 			}
 		}
+                $this->reorder();
 
 	}
 
@@ -68,7 +115,7 @@ class Admin_Framework extends Admin {
 	 *						 	[name]  )
 	 * @access public
 	 */
-	 function getLevelOne() {
+	 function getLevelOne($idmenu = false) {
 
 		$lang =& DoceboLanguage::createInstance('menu', $this->platform);
 
@@ -76,8 +123,11 @@ class Admin_Framework extends Admin {
 		$query_under = "
 		SELECT tab.idMenu, menu.module_name, menu.associated_token, tab.name, tab.image, tab.collapse, menu.of_platform
 		FROM ".$this->table_level_one." AS tab JOIN ".$this->table_level_two." AS menu
-		WHERE tab.idMenu = menu.idMenu
-		ORDER BY tab.sequence";
+		WHERE tab.idMenu = menu.idMenu";
+		if ($idmenu){
+                    $query_under .= " and tab.idMenu=".$idmenu;
+                }
+		$query_under .= " ORDER BY tab.sequence";
 		$re_under = sql_query($query_under);
 		
 
@@ -125,6 +175,49 @@ class Admin_Framework extends Admin {
 	 function getLevelTwo($id_level_one = false) {
 
 		 return $this->menu[$id_level_one];
+	 }
+	/**
+	 * @return	mixed	a list of the first level menu
+	 *					[id] (	[link]
+	 *							[image]
+	 *						 	[name]  )
+	 * @access public
+	 */
+	 function getLevel($idmenu = false, $idparent = false) {
+
+		$lang =& DoceboLanguage::createInstance('menu', $this->platform);
+
+
+		$query_under = "
+		SELECT tab.idMenu, menu.module_name, menu.associated_token, tab.name, tab.image, tab.collapse, menu.of_platform
+		FROM ".$this->table_level_one." AS tab JOIN ".$this->table_level_two." AS menu
+		WHERE tab.idMenu = menu.idMenu";
+		if ($idmenu){
+                    $query_under .= " and tab.idMenu=".$idmenu;
+                }
+		if ($idparent){
+                    $query_under .= " and menu.idParent=".$idparent;
+                }
+                else{
+                    $query_under .= " and menu.idParent is null";
+                }
+		$query_under .= " ORDER BY tab.sequence";
+		$re_under = sql_query($query_under);
+		
+
+		$menu = array();
+		while(list($id_main, $module_name, $token, $name, $image, $collapse, $of_platform) = sql_fetch_row($re_under)) {
+
+			if(!isset($menu[$id_main]) && checkPerm($token, true, $module_name, ( $of_platform === NULL ? $this->platform : $of_platform ) )) {
+
+				$menu[$id_main] = array('link' => 'index.php?op=change_main&new_main='.$id_main.'&of_platform='.( $of_platform === NULL ? $this->platform : $of_platform ),
+									'name' => ( $name != '' ? $lang->def($name)  : '' ),
+									'image' => 'area_title/'.$image,
+									'collapse' => ( $collapse == 'true' ? true : false ),
+									'of_platform' => ( $of_platform === NULL ? $this->platform : $of_platform ));
+			}
+		}
+		return $menu;
 	 }
 }
 
