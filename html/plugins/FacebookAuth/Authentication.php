e1f3a230cbfcadcd84f0858250b398c248ca6812
Andrea Fiadone <andrea.fiadone@purplenetwork.it>
2017-10-10 14:14:38 +0200

    Stilizzazione form login + modifiche social login

--- html/plugins/FacebookAuth/Authentication.php
+++ html/plugins/FacebookAuth/Authentication.php
@@ -45,8 +45,9 @@ class Authentication extends \PluginAuthentication implements \PluginAuthenticat
             $url = $facebook_service->getAuthorizationUri();
 
             $form =  "<a href='" . $url . "'>"
-                      . Get::img("social/facebook-24.png")
-                  . "</a>";
+				  // . Get::img("social/facebook-24.png")
+					. '<i class="fa fa-facebook"></i>'
+                  	. "</a>";
         }
 
         return [
