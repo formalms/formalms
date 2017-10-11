357eb9838134f7c0267a0355a1e852b4d1787c61
marcocastignoli <marco.castignoli@gmail.com>
2017-10-11 11:10:39 +0200

    #13572 Ora la priorità funziona ma non è ancora possibile settarla dalla pagina plugin

--- html/appCore/models/PluginmanagerAdm.php
+++ html/appCore/models/PluginmanagerAdm.php
@@ -187,7 +187,7 @@ class PluginmanagerAdm extends Model {
     }
 
     public function getActivePlugins(){
-        $query = "SELECT * FROM ".$this->table." WHERE  active=1 or core=1";
+        $query = "SELECT * FROM ".$this->table." WHERE  active=1 or core=1 ORDER BY priority ASC";
         $re = $this->db->query($query);
         $plugins=array();
         while($row = sql_fetch_assoc($re)){
