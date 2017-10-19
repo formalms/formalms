3ccafce59c009b44b2c58949df73b91fa3388d7f
davide anceschi <davide.anceschi@joint-tech.it>
2017-10-19 10:30:40 +0200

    Sistemato link su livello 0 senza sottolivelli. Da verificare

--- html/appCore/menu/menu_over.php
+++ html/appCore/menu/menu_over.php
@@ -76,12 +76,13 @@ function AdminBar()
         
         $strLabel = $p_name['name'];//$lang->def($p_name['name'], 'menu', $current_platform);
         $strIco = $p_name['image'];
-        $admin_menu_bar .= '<li><a href="#">' . $strIco . '&nbsp;' . $strLabel . '</a><ul>' . PHP_EOL;
 
         $idmenu=$p_man->menu[$p_code]['idMenu'];
         //Level1
         $main_voice = $p_man->getLevel($idmenu);
         if (!empty($main_voice)) {
+            $admin_menu_bar .= '<li><a href="#">' . $strIco . '&nbsp;' . $strLabel . '</a>' . PHP_EOL;
+            $admin_menu_bar .= '<ul>' . PHP_EOL;
             foreach ($main_voice as $id_m => $v_main) {
                 $admin_menu_bar .= '<li>';
                 $admin_menu_bar .= '<a href="' . Util::str_replace_once('&', '&amp;', $v_main['link']) . '" >' . $v_main['name'] . '</a>';
@@ -99,6 +100,11 @@ function AdminBar()
                 }
             }
             $admin_menu_bar .= '</ul></li>' . PHP_EOL;
+        }
+        else{
+            $admin_menu_bar .= '<li>';
+            $admin_menu_bar .= '<a href="' . Util::str_replace_once('&', '&amp;', $p_name['link']) . '" >' . $p_name['name'] . '</a>';
+            $admin_menu_bar .= '</li>';
         }
     }
 
