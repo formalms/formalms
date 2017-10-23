2ffe714384e86f876ecd802c1115025b80043772
Gabriele Marconetta <gabriele.marconetta@gmail.com>
2017-10-23 10:01:37 +0200

    fix logo catalogo esterno

--- html/templates/standard/layout/home_catalogue.php
+++ html/templates/standard/layout/home_catalogue.php
@@ -33,7 +33,9 @@
 				<?php echo Lang::t('_CHANGELANG', 'register').': '.Layout::change_lang(); ?>
 			</div>
 			<!-- <h1 id="main_title"><a href="index.php"><?php echo Lang::t('_MAIN_TITLE', 'login'); ?></a></h1>-->
-			<a href="index.php"><img class="left_logo" src="<?php echo Layout::path(); ?>images/company_logo.png" alt="Left logo" /></a>
+			<a href="index.php">
+				<img class="left_logo" src="<?php echo Layout::path(); ?>images/company_logo.png" alt="Left logo" />
+			</a>
 			<div class="nofloat"></div>
 			<div class="divider"></div>
 		</div>
