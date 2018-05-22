59805d8972b1620c43414f486c05351f988fed83
Massimiliano Cavazzini <max.cavazzini@joint-tech.it>
2018-05-22 17:31:38 +0200

    #17570 - GDPR •Lunghezza minima psw 8 caratteri •obbligo cambio password al primo accesso

--- html/upgrade/data/upg_data/20000_db.sql
+++ html/upgrade/data/upg_data/20000_db.sql
@@ -1025,6 +1025,11 @@ CREATE TABLE `core_privacypolicy_user` (
   `idst` int(11)  NOT NULL,
   `accept_date` datetime NOT NULL
 );
+
+-- 17570 - GDPR
+UPDATE `core_setting` SET `param_value` = '8' WHERE `core_setting`.`param_name` = 'pass_min_char';
+UPDATE `core_setting` SET `param_value` = 'on' WHERE `core_setting`.`param_name` = 'pass_change_first_login';
+
 -- ------------------------------------------------------------------
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
