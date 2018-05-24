d0e4c99dce5fd6866b3f90cddc94b58c5db6103e
Alessandro Micelli <alessandro.micelli@email.it>
2018-05-24 15:30:24 +0100

    #17778 - TAB HOME NON VISUALIZZA CONTENUTI

--- html/appLms/views/home/_tabs_block.php
+++ html/appLms/views/home/_tabs_block.php
@@ -1,7 +1,6 @@
     <!-- welcome page (main tab) -->
     <div id="yui-main">
-        <div class="yui-b">
-
+        <div class="yui-bx">
             <div>
                 <?php
                 $this->widget('lms_tab', array(
@@ -9,13 +8,21 @@
                 ));
                 ?>
             </div>
-            <div class="quick_search_form navbar forma-quick-search-form"> 
-
-            </div> 
-
+            <div class="quick_search_form navbar forma-quick-search-form">
+                <div id="tab_content"></div>
+            </div>
         </div>
     </div>
     <div class="nofloat"></div>
 <script type="text/javascript">
-    //document.getElementById('tab_content').innerHTML = '<?php echo addslashes($_content); ?>';
+    document.getElementById('tab_content').innerHTML = '<?php echo addslashes($_content); ?>';
 </script>
+
+<style type="text/css">
+    #tab_content {
+        background: #fff;
+        margin: 6px;
+        text-align: left;
+        padding: 1rem;
+    }
+</style>
