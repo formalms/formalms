490c06cc7ff694ce8fa3ea1435f26e0e6fdc7ed3
Alessandro Micelli <alessandro.micelli@email.it>
2018-05-25 09:47:53 +0100

    #17778 - TAB HOME NON VISUALIZZA CONTENUTI Fix show outside the tab

--- html/appLms/views/home/_tabs_block.php
+++ html/appLms/views/home/_tabs_block.php
@@ -1,19 +1,17 @@
-    <!-- welcome page (main tab) -->
-    <div id="yui-main">
-        <div class="yui-bx">
-            <div>
-                <?php
-                $this->widget('lms_tab', array(
-                    'active' => 'home'
-                ));
-                ?>
-            </div>
-            <div class="quick_search_form navbar forma-quick-search-form">
-                <div id="tab_content"></div>
-            </div>
+<!-- welcome page (main tab) -->
+<div id="yui-main">
+    <div class="yui-a">
+        <div>
+            <?php
+            $this->widget('lms_tab', array(
+                'active' => 'home'
+            ));
+            ?>
         </div>
     </div>
-    <div class="nofloat"></div>
+    <div id="tab_content"></div>
+</div>
+
 <script type="text/javascript">
     document.getElementById('tab_content').innerHTML = '<?php echo addslashes($_content); ?>';
 </script>
@@ -24,5 +22,8 @@
         margin: 6px;
         text-align: left;
         padding: 1rem;
+    }
+    #tab_content h1 {
+        padding-bottom: 1rem;
     }
 </style>
