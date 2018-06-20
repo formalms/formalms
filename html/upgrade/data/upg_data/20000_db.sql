0f87a02ca0ecba0a83a09ab3268442415d7776ee
dance <bart@joint-tech.it>
2018-06-20 09:13:20 +0200

    rimosso commento che offende

--- html/upgrade/data/upg_data/20000_db.sql
+++ html/upgrade/data/upg_data/20000_db.sql
@@ -1013,42 +1013,6 @@ ALTER TABLE `core_task` CHANGE `sequence` `sequence` INT(3) NOT NULL AUTO_INCREM
 ALTER TABLE `learning_coursereport` ADD COLUMN `show_in_detail` tinyint(1) NULL DEFAULT 1;
 ALTER TABLE `learning_test` DROP COLUMN `show_in_coursereport`;
 
-/* **
-INSERT INTO `core_lang_text` (`id_text` ,`text_key` ,`text_module` ,`text_attributes`) VALUES (NULL , '_SHOW_IN_DETAIL', 'test', '');
-INSERT INTO `core_lang_translation` ( `id_text`, `lang_code`,  `translation_text`, `save_date` )
-  SELECT lt.id_text, l.lang_code, t1.translation_text as translation_text, now() AS save_date
-  FROM core_lang_text lt , core_lang_language l ,
-    ( SELECT lt.id_text, t.lang_code, t.translation_text
-      FROM   core_lang_text lt, core_lang_translation t
-      WHERE t.id_text = lt.id_text AND lt.text_key ='_SHOW_IN_DETAIL'  AND lt.text_module = 'test'  ) t1
-  WHERE lt.text_key = '_SHOW_IN_DETAIL' AND lt.text_module = 'test' AND t1.lang_code = l.lang_code
-;
-** */
-
-/* **
-INSERT IGNORE INTO `core_lang_text` (`id_text` ,`text_key` ,`text_module` ,`text_attributes`) VALUES (NULL , '_OVERVIEW', 'coursereport', '');
-INSERT IGNORE INTO `core_lang_translation` ( `id_text`, `lang_code`,  `translation_text`, `save_date` )
-  SELECT lt.id_text, l.lang_code, t1.translation_text as translation_text, now() AS save_date
-  FROM core_lang_text lt , core_lang_language l ,
-    ( SELECT lt.id_text, t.lang_code, t.translation_text
-      FROM   core_lang_text lt, core_lang_translation t
-      WHERE t.id_text = lt.id_text AND lt.text_key ='_OVERVIEW'  AND lt.text_module = 'coursereport'  ) t1
-  WHERE lt.text_key = '_OVERVIEW' AND lt.text_module = 'coursereport' AND t1.lang_code = l.lang_code
-;
-** */
-
-/* **
-INSERT IGNORE INTO `core_lang_text` (`id_text` ,`text_key` ,`text_module` ,`text_attributes`) VALUES (NULL , '_DETAILS', 'coursereport', '');
-INSERT IGNORE INTO `core_lang_translation` ( `id_text`, `lang_code`,  `translation_text`, `save_date` )
-  SELECT lt.id_text, l.lang_code, t1.translation_text as translation_text, now() AS save_date
-  FROM core_lang_text lt , core_lang_language l ,
-    ( SELECT lt.id_text, t.lang_code, t.translation_text
-      FROM   core_lang_text lt, core_lang_translation t
-      WHERE t.id_text = lt.id_text AND lt.text_key ='_DETAILS'  AND lt.text_module = 'coursereport'  ) t1
-  WHERE lt.text_key = '_DETAILS' AND lt.text_module = 'coursereport' AND t1.lang_code = l.lang_code
-;
-** */
-
 -- Add lastedit and validity
 ALTER TABLE `core_privacypolicy` ADD `lastedit_date` DATETIME NOT NULL AFTER `name`, ADD `validity_date` DATETIME NOT NULL AFTER `lastedit_date`;
 
