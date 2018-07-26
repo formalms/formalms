d2cc33596918e021db63a22951cf795d8636b4dd
Alessandro Micelli <alessandro.micelli@email.it>
2018-07-26 16:20:01 +0100

    #18791 - GESTIONE PERMESSO VIEW_ALL Upgrade

--- html/upgrade/data/upg_data/20100_db.sql
+++ html/upgrade/data/upg_data/20100_db.sql
@@ -6,3 +6,162 @@
 --
 
 -- ------------------------------------------------------------------
+
+INSERT INTO `core_customfield_area` (`area_code`, `area_name`, `area_table`, `area_field`) VALUES
+('ORG_CHART', 'Org Chart Tree', 'core_org_chart_tree', 'idOrg');
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_menucustom m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcustom, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/coursestats/view')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/coursestats/view_all')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_course m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcourse, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/coursestats/view')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/coursestats/view_all')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_menucustom m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcustom, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/statistic/view')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/statistic/view_all')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_course m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcourse, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/statistic/view')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/statistic/view_all')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_menucustom m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcustom, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/stats/view_user')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/stats/view_all_statuser')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_course m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcourse, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/stats/view_user')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/stats/view_all_statuser')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_menucustom m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcustom, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/stats/view_course')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/stats/view_all_stat_course')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_course m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcourse, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/stats/view_course')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/stats/view_all_stat_course')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_menucustom m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcustom, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/light_repo/view')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/light_repo/view')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_course m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcourse, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/light_repo/view')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/light_repo/view_all')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_menucustom m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcustom, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/coursereport/view')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/coursereport/view')
+	) x
+);
+
+DELETE FROM core_role_members
+WHERE idst IN (
+	SELECT idst FROM (
+		select DISTINCT(ra.idst)
+		from learning_course m
+		join (select 1 lvl union select 2 lvl union select 3 lvl union select 4 lvl union select 5 lvl union select 6 lvl union select 7 lvl) core_lvl
+		join core_group g on g.groupid like concat('/lms/custom/', m.idcourse, '/', lvl)
+		join core_role r on r.roleid like concat('/lms/course/private/coursereport/view')
+		join core_role_members rm on r.idst = rm.idst and g.idst = rm.idstMember
+		join core_role ra on ra.roleid like concat('/lms/course/private/coursereport/view')
+	) x
+);
