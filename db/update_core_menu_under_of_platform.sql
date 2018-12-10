4bea93f1c35ed7c97c2581820b2298746cdb7eb3
Alessandro Chiodaroli <alessandro.chiodaroli@joint-tech.it>
2018-12-10 10:26:43 +0100

    Aggiornato script.

--- db/update_core_menu_under_of_platform.sql
+++ db/update_core_menu_under_of_platform.sql
@@ -1,6 +1,9 @@
-UPDATE core_menu_under
-SET of_platform = 'alms'
-WHERE of_platform = 'lms';
+UPDATE core_menu_under mu
+INNER JOIN core_menu m
+    ON mu.idMenu = m.idMenu
+SET mu.of_platform = 'alms'
+WHERE mu.of_platform = 'lms'
+    AND m.of_platform NOT IN ('lms');
 
 UPDATE core_menu_under
 SET of_platform = 'framework'
