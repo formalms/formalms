b251fe4f475f0a654c38257930dddd8500f2423b
Alessandro Chiodaroli <alessandro.chiodaroli@joint-tech.it>
2019-02-18 17:56:51 +0100

    Impedito riesecuzione update.

--- html/upgrade/data/upg_data/20200_db.sql
+++ html/upgrade/data/upg_data/20200_db.sql
@@ -21,7 +21,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 ALTER TABLE `core_menu` ADD COLUMN `of_platform` VARCHAR(255) NOT NULL DEFAULT 'framework';
 
 UPDATE `core_menu_under` SET `of_platform` = 'framework' WHERE `of_platform` IS NULL;
-UPDATE `core_menu_under` SET `of_platform` = 'alms' WHERE `of_platform` = 'lms';
+SET @alms_exists = (SELECT COUNT(*) > 0 FROM `core_menu_under` WHERE `of_platform` = 'alms');
+UPDATE `core_menu_under` SET `of_platform` = 'alms' WHERE `of_platform` = 'lms' AND NOT @alms_exists;
 
 INSERT IGNORE INTO `core_menu` (`idMenu`, `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform`) VALUES(596, '_MYCOURSES', '', 1, 'true', 'true', NULL, NULL, 'lms');
 INSERT IGNORE INTO `core_menu` (`idMenu`, `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform`) VALUES(597, '_CATALOGUE', '', 2, 'false', 'true', NULL, NULL, 'lms');
