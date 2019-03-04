3deca5a2c22be2fc8aa086e6fb70a1c6905d3b9e
dance <bart@joint-tech.it>
2019-03-04 17:58:16 +0100

    evidenziato il legame fra invio [x] New Password    [x] send new password

--- html/appCore/views/usermanagement/multimod.php
+++ html/appCore/views/usermanagement/multimod.php
@@ -22,25 +22,7 @@ $mask .= Form::getHidden('multimod_users', 'users', "");
 $mask .= Lang::t('_USERS', 'standard').': <b>'.(int)$users_count.'</b>';
 
 $mask .= $_open_table;
-/*
-$mask .= $_open_row.$_open_cell;
-$mask .= Form::getInputCheckbox('multimod_sel_firstname', 'multimod_sel[firstname]', 1, false, "");
-$mask .= $_close_cell.$_open_cell;
-$mask .= Form::getTextField(Lang::t('_FIRSTNAME', 'standard'), 'firstname', 'firstname', 50, $info['firstname']).'</div>';
-$mask .= $_close_cell.$_close_row;
 
-$mask .= $_open_row.$_open_cell;
-$mask .= Form::getInputCheckbox('multimod_sel_lastname', 'multimod_sel[lastname]', 1, false, "");
-$mask .= $_close_cell.$_open_cell;
-$mask .= Form::getTextField(Lang::t('_LASTNAME', 'standard'), 'lastname', 'lastname', 50, $info['lastname']).'</div>';
-$mask .= $_close_cell.$_close_row;
-
-$mask .= $_open_row.$_open_cell;
-$mask .= Form::getInputCheckbox('multimod_sel_email', 'multimod_sel[email]', 1, false, "");
-$mask .= $_close_cell.$_open_cell;
-$mask .= Form::getTextField(Lang::t('_EMAIL', 'standard'), 'email', 'email', 50, $info['email']).$_close_row;
-$mask .= $_close_cell.$_close_row;
-*/
 $mask .= $_open_row.$_open_cell;
 $mask .= Form::getInputCheckbox('multimod_sel_password', 'multimod_sel[password]', 1, false, "");
 $mask .= $_close_cell.$_open_cell;
@@ -48,15 +30,24 @@ $mask .= Form::getPassword(Lang::t('_NEW_PASSWORD', 'register'), 'new_password',
 $mask .= $_close_cell.$_close_row;
 
 $mask .= $_open_row.$_open_cell;
-$mask .= "&nbsp;";//Form::getInputCheckbox('multimod_sel_pwd_confirm', 'multimod_sel[pwd_confirm]', 1, false, "");
+$mask .= "&nbsp;";
 $mask .= $_close_cell.$_open_cell;
 $mask .= Form::getPassword(Lang::t('_RETYPE_PASSWORD', 'register'), 'new_password_confirm', 'new_password_confirm', 50, "");
 $mask .= $_close_cell.$_close_row;
 
 $mask .= $_open_row.$_open_cell;
-$mask .= Form::getInputCheckbox('multimod_sel_send_alert', 'multimod_sel[send_alert]', 1, false, "disabled=true");
+$mask .= "&nbsp;";
 $mask .= $_close_cell.$_open_cell;
+$mask .= "&nbsp;";
+$mask .= Form::getInputCheckbox('multimod_sel_send_alert', 'multimod_sel[send_alert]', 1, false, "disabled=true");
 $mask .= "&nbsp;"."<b>".Lang::t('_SEND_NEW_CREDENTIALS_ALERT', 'user_managment')."</b>";
+$mask .= $_close_cell.$_close_row;
+
+$mask .= $_open_row.$_open_cell;
+$mask .= "&nbsp;";
+$mask .= $_close_cell.$_open_cell;
+$mask .= "&nbsp;";
+//$mask .= '<div class="up_name">'.''.'</div>';
 $mask .= $_close_cell.$_close_row;
 
 $mask .= $_open_row.$_open_cell;
@@ -76,15 +67,7 @@ $mask .= Form::getInputCheckbox('multimod_sel_level', 'multimod_sel[level]', 1, 
 $mask .= $_close_cell.$_open_cell;
 $mask .= Form::getDropdown(Lang::t('_LEVEL', 'admin_directory'), 'level', 'level', $levels, $info['level']);
 $mask .= $_close_cell.$_close_row;
-/*
-foreach ($modify_mask as $id_item => $item) {
-	$mask .= $_open_row.$_open_cell;
-	$mask .= Form::getInputCheckbox('multimod_selpref_'.$id_item, 'multimod_selpref['.$id_item.']', 1, false, "");
-	$mask .= $_close_cell.$_open_cell;
-	$mask .= $item;
-	$mask .= $_close_cell.$_close_row;
-}
-*/
+
 foreach ($fields_mask as $id_item => $item) {
 	if (!in_array($id_item, $fields_to_exclude)) {
 		$mask .= $_open_row.$_open_cell;
@@ -95,38 +78,6 @@ foreach ($fields_mask as $id_item => $item) {
 	}
 }
 
-/* $social =new Social();
-if ($social->isActive('facebook')) {
-	$mask .= $_open_row.$_open_cell;
-	$mask .= Form::getInputCheckbox('multimod_sel_facebook_id', 'multimod_sel[facebook_id]', 1, false, "");
-	$mask .= $_close_cell.$_open_cell;
-	$mask .= Form::getTextField(Lang::t('_FACEBOOK_ID', 'standard'), 'facebook_id', 'facebook_id', 255, $info['facebook_id']);
-	$mask .= $_close_cell.$_close_row;
-}
-
-if ($social->isActive('twitter')) {
-	$mask .= $_open_row.$_open_cell;
-	$mask .= Form::getInputCheckbox('multimod_sel_twitter_id', 'multimod_sel[twitter_id]', 1, false, "");
-	$mask .= $_close_cell.$_open_cell;
-	$mask .= Form::getTextField(Lang::t('_TWITTER_ID', 'standard'), 'twitter_id', 'twitter_id', 255, $info['twitter_id']);
-	$mask .= $_close_cell.$_close_row;
-}
-
-if ($social->isActive('linkedin')) {
-	$mask .= $_open_row.$_open_cell;
-	$mask .= Form::getInputCheckbox('multimod_sel_linkedin_id', 'multimod_sel[linkedin_id]', 1, false, "");
-	$mask .= $_close_cell.$_open_cell;
-	$mask .= Form::getTextField(Lang::t('_LINKEDIN_ID', 'standard'), 'linkedin_id', 'linkedin_id', 255, $info['linkedin_id']);
-	$mask .= $_close_cell.$_close_row;
-}
-
-if ($social->isActive('google')) {
-	$mask .= $_open_row.$_open_cell;
-	$mask .= Form::getInputCheckbox('multimod_sel_google_id', 'multimod_sel[google_id]', 1, false, "");
-	$mask .= $_close_cell.$_open_cell;
-	$mask .= Form::getTextField(Lang::t('_GOOGLE_ID', 'standard'), 'google_id', 'google_id', 255, $info['google_id']);
-	$mask .= $_close_cell.$_close_row;
-} */
 
 $mask .= $_close_table;
 
