1c07caabe3c2a2065752a3c553ce51c1aafaac06
Giuseppe Nucifora <me@giuseppenucifora.com>
2019-04-17 17:36:31 +0200

    [be] - #SNNF-2 - Update translattion xml files - Fix Sql queries

--- db/snnf-2_user_notification.sql
+++ db/snnf-2_user_notification.sql
@@ -1,3 +1,10 @@
+-- Add translation for Super-Administrators --
+INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_EVENT_RECIPIENTS_GOD', 'event_manager', '');
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','Super-Administrators', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','Super-Administrators', now());
+
 -- User change his data --
 SET @max = (SELECT MAX(idClass)+1 FROM `core_event_class`);
 
@@ -8,19 +15,29 @@ INSERT INTO core_event_consumer_class (idConsumer, idClass) VALUES ('1', @max);
 INSERT INTO core_event_manager (idEventMgr, idClass, permission, channel, recipients, show_level) VALUES (@max, @max, 'not_used', 'email', '_EVENT_RECIPIENTS_GOD', 'godadmin');
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_EVENT_CLASS_UserModSuperAdmin', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User status is change in modified', now());
+SET @lastID = LAST_INSERT_ID();
 
-INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_EVENT_RECIPIENTS_GOD', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','Super-Administrators', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User status is change in modified', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User status is change in modified', now());
+
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_MOD_USER_SBJ', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User has been modified', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User has been modified', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User has been modified', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_MOD_USER_TEXT', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User with [firstname] [lastname] and userId : [username] has been modified in platform : [url]', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User with [firstname] [lastname] and userId : [username] has been modified in platform : [url]', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User with [firstname] [lastname] and userId : [username] has been modified in platform : [url]', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_MOD_USER_TEXT_SMS', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User with [firstname] [lastname] and userId : [username] has been modified in platform : [url]', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User with [firstname] [lastname] and userId : [username] has been modified in platform : [url]', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User with [firstname] [lastname] and userId : [username] has been modified in platform : [url]', now());
 
 
 -- User suspended in forma --
@@ -33,16 +50,28 @@ INSERT INTO core_event_consumer_class (idConsumer, idClass) VALUES ('1', @max);
 INSERT INTO core_event_manager (idEventMgr, idClass, permission, channel, recipients, show_level) VALUES (@max, @max, 'not_used', 'email', '_EVENT_RECIPIENTS_GOD', 'godadmin');
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_EVENT_CLASS_UserSuspendedSuperAdmin', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User has been suspended', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User has been suspended', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User has been suspended', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_SUSPENDED_USER_SBJ', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User has been suspended', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User has been suspended', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User has been suspended', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_SUSPENDED_USER_TEXT', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User with [firstname] [lastname] and userId : [username] has been suspended in platform : [url]', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User with [firstname] [lastname] and userId : [username] has been suspended in platform : [url]', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User with [firstname] [lastname] and userId : [username] has been suspended in platform : [url]', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_SUSPENDED_USER_TEXT_SMS', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User with [firstname] [lastname] and userId : [username] has been suspended in platform : [url]', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User with [firstname] [lastname] and userId : [username] has been suspended in platform : [url]', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User with [firstname] [lastname] and userId : [username] has been suspended in platform : [url]', now());
 
 
 
@@ -56,16 +85,28 @@ INSERT INTO core_event_consumer_class (idConsumer, idClass) VALUES ('1', @max);
 INSERT INTO core_event_manager (idEventMgr, idClass, permission, channel, recipients, show_level) VALUES (@max, @max, 'not_used', 'email', '_EVENT_RECIPIENTS_GOD', 'godadmin');
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_EVENT_CLASS_UserModNodeSuperAdmin', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User Node is changes', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User Node is changes', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User Node is changes', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_CHANGE_NODE_USER_SBJ', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User has been change node', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User has been change node', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User has been change node', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_CHANGE_NODE_USER_TEXT', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User with [firstname] [lastname] and userId : [username] has been change node in platform : [url]', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User with [firstname] [lastname] and userId : [username] has been change node in platform : [url]', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User with [firstname] [lastname] and userId : [username] has been change node in platform : [url]', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_CHANGE_NODE_USER_TEXT_SMS', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User with [firstname] [lastname] and userId : [username] has been change node in platform : [url]', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User with [firstname] [lastname] and userId : [username] has been change node in platform : [url]', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User with [firstname] [lastname] and userId : [username] has been change node in platform : [url]', now());
 
 
 -- User suspended from a course --
@@ -78,13 +119,26 @@ INSERT INTO core_event_consumer_class (idConsumer, idClass) VALUES ('1', @max);
 INSERT INTO core_event_manager (idEventMgr, idClass, permission, channel, recipients, show_level) VALUES (@max, @max, 'not_used', 'email', '_EVENT_RECIPIENTS_TEACHER_GOD', 'godadmin,admin,user');
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_EVENT_CLASS_UserCourseSuspendedSuperAdmin', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User has been suspended', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User has been suspended', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User has been suspended', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_COURSE_SUSPENDED_USER_SBJ', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User has been suspended', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User has been suspended', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User has been suspended', now());
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_COURSE_SUSPENDED_USER_TEXT', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User with [firstname] [lastname] and userId : [username] has been suspended from course : [course] in platform : [url]', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User with [firstname] [lastname] and userId : [username] has been suspended from course : [course] in platform : [url]', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User with [firstname] [lastname] and userId : [username] has been suspended from course : [course] in platform : [url]', now());
+
 
 INSERT INTO core_lang_text (id_text, text_key, text_module, text_attributes) VALUES (NULL, '_COURSE_SUSPENDED_USER_TEXT_SMS', 'event_manager', '');
-INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (LAST_INSERT_ID(), 'english','User with [firstname] [lastname] and userId : [username] has been suspended from course : [course] in platform : [url]', now());
+SET @lastID = LAST_INSERT_ID();
+
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'english','User with [firstname] [lastname] and userId : [username] has been suspended from course : [course] in platform : [url]', now());
+INSERT INTO core_lang_translation ( id_text,lang_code,  translation_text, save_date ) VALUES  (@lastID, 'italian','User with [firstname] [lastname] and userId : [username] has been suspended from course : [course] in platform : [url]', now());
