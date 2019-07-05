3935c5ea55ad15ef37ac40aea19db0d070e20137
Alessandro Micelli <alessandro.micelli@email.it>
2019-07-05 13:30:05 +0100

    Incolonnato gruppi pack in settings e sostituito dropdown con tabs

--- html/appCore/views/setting/show.php
+++ html/appCore/views/setting/show.php
@@ -1,35 +1,22 @@
 <?php echo getTitleArea(Lang::t('_CONFIGURATION', 'configuration')); ?>
 <div class="std_block">
-
 	<div id="global_conf" class="tabs-wrapper">
-		<ul class="nav nav-tabs bordered">
-			<li class="dropdown">
-				<a href="javascript:void(0)" class="dropdown-toggle" role="button" data-toggle="dropdown">
-          <span class="dropdown-title"><?php echo Lang::t('_OTHER_OPTION', 'course'); ?></span> &nbsp;<span class="caret"></span>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-left tabs-dropdown">
-				<?php
-				while(list($id, $canonical_name) = each($regroup)) {
-					// echo '<li'.($id == $active_tab?' class="selected"':'').'>'
-					echo '<li class="'.($id == $active_tab ? ' active' : '').'"">'
-						.'<a data-toggle="tab" href="#tab_g_'.$id.'">'
-						.'<em>'.Lang::t('_'.strtoupper($canonical_name), 'configuration').'</em>'
-						.'</a>'
-						.'</li>';
-				}
-				reset($regroup);
-				?>
-				</ul>
-			</li>
+		<ul class="nav nav-tabs"><?php
+			while(list($id, $canonical_name) = each($regroup)) {?>
+				<li class="<?php echo ($id == $active_tab ? ' active' : '');?>">
+					<a data-toggle="tab" href="#tab_g_<?php echo $id;?>" class="nav-link">
+						<em><?php echo Lang::t('_'.strtoupper($canonical_name), 'configuration');?></em>
+					</a>
+				</li><?php
+			}
+			reset($regroup);?>
 		</ul>
-		<div class="tab-content">
-			<?php
+		<div class="tab-content"><?php
 			while(list($id, $canonical_name) = each($regroup)) {
 
 				// print the tab content
 				echo '<div class="tab-pane'.($id == $active_tab ? ' active' : '').'" id="tab_g_'.$id.'">'
 					.'<h2>'.Lang::t('_'.strtoupper($canonical_name), 'configuration').'</h2>'
-					//.'<p>'.Lang::t('_CONF_DESCR_'.$id, 'configuration').'</p>'
 
 					.Form::openForm('conf_option_'.$id, 'index.php?r=adm/setting/save')
 					.Form::openElementSpace()
@@ -51,12 +38,12 @@
 					.Form::CloseForm()
 					.'<br />'
 					.'</div>';
-			}
-			?>
+			}?>
 		</div>
 		<div class="nofloat">&nbsp;</div>
 	</div>
 </div>
+
 <script type="text/javascript">
 $(function(){
     // check on page reload
