97926b817e5dcc8a36a77ad37dad1628ab1877f7
Giuseppe Nucifora <me@giuseppenucifora.com>
2019-07-09 17:53:29 +0200

    [be] - fix status course

--- html/appLms/models/DashboardBlockCoursesLms.php
+++ html/appLms/models/DashboardBlockCoursesLms.php
@@ -72,7 +72,7 @@ class DashboardBlockCoursesLms extends DashboardBlockLms
 		];
 
 		$params = [
-			':id_user' => (int)Docebo::user()->getId()
+			':id_user' => (int) Docebo::user()->getId()
 		];
 
 		// course status : all status, new, completed, in progress
@@ -123,7 +123,7 @@ class DashboardBlockCoursesLms extends DashboardBlockLms
 		}
 
 
-		$query = "SELECT c.*, cu.status AS user_status, cu.level, cu.date_inscr, cu.date_first_access, cu.date_complete, cu.waiting"
+		$query = "SELECT c.*, c.status AS course_status, cu.status AS user_status, cu.level, cu.date_inscr, cu.date_first_access, cu.date_complete, cu.waiting"
 			. " FROM %lms_course AS c "
 			. " JOIN %lms_courseuser AS cu ON (c.idCourse = cu.idCourse)  "
 			. " WHERE " . $this->compileWhere($conditions, $params)
@@ -140,57 +140,27 @@ class DashboardBlockCoursesLms extends DashboardBlockLms
 
 		$rs = $db->query($query);
 
-		$result = array();
-		$courses = array();
-		while ($data = $db->fetch_assoc($rs)) {
+		$result = [];
+		while ($course = $db->fetch_assoc($rs)) {
 
-			$data['enrolled'] = 0;
-			$data['numof_waiting'] = 0;
-			$data['first_lo_type'] = FALSE;
+			$course['enrolled'] = 0;
+			$course['numof_waiting'] = 0;
+			$course['first_lo_type'] = FALSE;
 
 
 			//** name category
-			$data['nameCategory'] = $this->getCategory($data['idCategory']); //$this->getCategory($data['idCategory']);
+			$course['nameCategory'] = $this->getCategory($course['idCategory']);
+			$course['status'] = $this->calculateStatus($course);
 
-			$courses[] = $data['idCourse'];
-			$result[$data['idCourse']] = $data;
-		}
-
-
-		if (!empty($courses)) {
-			// find subscriptions
-			$re_enrolled = $db->query(
-				"SELECT c.idCourse, COUNT(*) as numof_associated, SUM(waiting) as numof_waiting"
-				. " FROM %lms_course AS c "
-				. " JOIN %lms_courseuser AS cu ON (c.idCourse = cu.idCourse) "
-				. " WHERE c.idCourse IN (" . implode(',', $courses) . ") "
-				. " GROUP BY c.idCourse"
-			);
-			while ($data = $db->fetch_assoc($re_enrolled)) {
-
-				$result[$data['idCourse']]['enrolled'] = $data['numof_associated'] - $data['numof_waiting'];
-				$result[$data['idCourse']]['numof_waiting'] = $data['numof_waiting'];
-			}
-
-
-			#3562 Grifo multimedia - LR
-			$query_lo = "select org.idOrg, org.idCourse, org.objectType from (SELECT o.idOrg, o.idCourse, o.objectType 
-                          FROM %lms_organization AS o WHERE o.objectType != '' AND o.idCourse IN (" . implode(',', $courses) . ") ORDER BY o.path) as org 
-                          GROUP BY org.idCourse ";
-
-
-			// find first LO type
-			$re_firstlo = $db->query($query_lo);
-			while ($data = $db->fetch_assoc($re_firstlo)) {
-				$result[$data['idCourse']]['first_lo_type'] = $data['objectType'];
-			}
+			$result[$course['idCourse']] = $course;
 		}
 
 		return $result;
 	}
 
-	private function getUserCoursePathCourses( $id_user ) {
-		require_once(_lms_.'/lib/lib.coursepath.php');
+	private function getUserCoursePathCourses($id_user)
+	{
+		require_once(_lms_ . '/lib/lib.coursepath.php');
 		$cp_man = new Coursepath_Manager();
 		$output = array();
 		$cp_list = $cp_man->getUserSubscriptionsInfo($id_user);
@@ -201,29 +171,45 @@ class DashboardBlockCoursesLms extends DashboardBlockLms
 		return $output;
 	}
 
-	private function compileWhere($conditions, $params) {
+	private function compileWhere($conditions, $params)
+	{
 
-		if(!is_array($conditions)) return "1";
+		if (!is_array($conditions)) return "1";
 
 		$where = array();
 		$find = array_keys($params);
-		foreach($conditions as $key=>$value) {
+		foreach ($conditions as $key => $value) {
 
 			$where[] = str_replace($find, $params, $value);
 		}
 		return implode(" AND ", $where);
 	}
 
-	private function getCategory($idCat){
+	private function getCategory($idCat)
+	{
 		$db = DbConn::getInstance();
-		$query = "select path from %lms_category where idCategory=".$idCat;
+		$query = "select path from %lms_category where idCategory=" . $idCat;
 		$res = $db->query($query);
 		$path = "";
 		if ($res && $db->num_rows($res) > 0) {
 			list($path) = $db->fetch_row($res);
 		}
 		return $path;
+	}
 
+	private function calculateStatus($course)
+	{
+		if ($course['date_end'] !== '0000-00-00' &&  $course['date_end'] !== '0000-00-00 00:00:00') {
+			
+			$earlier = new DateTime();
+			$later = new DateTime($course['date_end']);
 
+			$days = $later->diff($earlier)->format("%a");
+			
+			if($days === 0){
+				return false;
+			}
+		}
+		return true;
 	}
 }
