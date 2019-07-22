7ee8ff561f04b4bb6fdb1b26f9d25a81c459bb6f
Giuseppe Nucifora <me@giuseppenucifora.com>
2019-07-22 18:21:30 +0200

    [BE] - #FAY-9 IN Add Dashboard in menu and in configuration

--- db/FAY-9-dashboard.sql
+++ db/FAY-9-dashboard.sql
@@ -14,3 +14,7 @@ VALUES ((SELECT id_text FROM core_lang_text where text_key = '_DASHBOARD ' and t
         'italian', 'Dashboard');
 
 INSERT INTO `learning_module` VALUES (47, 'dashboard', 'show', '_DASHBOARD', 'view', '', '', 'all', 'lms/dashboard/show');
+
+INSERT INTO `core_menu`(`idMenu`, `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform`) VALUES (600, '_DASHBOARD', '', 4, 'true', 'true', NULL, NULL, 'lms');
+
+INSERT INTO `core_menu_under`(`idUnder`, `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`, `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path`) VALUES (600, 600, 'course', '_DASHBOARD', NULL, 'view', 'lms', 4, NULL, NULL, 'lms/dashboard/show');
