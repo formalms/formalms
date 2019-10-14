b921fe961e9f35647a744387b62b1781710d2e6e
marcocastignoli <marco.castignoli@gmail.com>
2019-10-14 12:22:17 +0200

    #3630 Miglioramento grafica plugin system

--- html/appCore/views/pluginmanager/show.php
+++ html/appCore/views/pluginmanager/show.php
@@ -1,128 +1,137 @@
 <?php
-require_once(_base_.'/lib/lib.table.php');
-
 echo getTitleArea(Lang::t('_PLUGIN_LIST', 'configuration'));
-
-$table = new Table(0, Lang::t('_PLUGIN_LIST', 'configuration'), Lang::t('_PLUGIN_LIST', 'configuration'));
-
-$table->setTableId("table_plugin");
-
-$cont_h = array(
-    Lang::t('_PLUGIN_NAME', 'configuration'),
-    Lang::t('_PLUGIN_VERSION', 'configuration'),
-    Lang::t('_PLUGIN_AUTHOR', 'configuration'),
-    Lang::t('_PLUGIN_CATEGORY', 'configuration'),
-    Lang::t('_PLUGIN_DESCRIPTION', 'configuration'),
-    "",
-    "",
-    "",
-    ""
-);
-
-$type_h = array('', 'align_center', 'align_center', '', '');
-
-$table->setColsStyle($type_h);
-
-$table->addHead($cont_h);
-
-foreach ($plugins as $info){
-    $errors="";
-    $settings="";
-    $install="";
-    $activate="";
-    $priority="";
-    //if already in database
-    if (isset($info['plugin_id'])){
-        if ($info['core']==="0"){
-            if (!$info['version_error']){
-                if ($info['dependence_of']){
-                    $dependencies = "";
-                    foreach ($info['dependence_of'] as $k => $v){
-                        $dependencies .= "\n".$k.": ".$v;
-                    }
-                    $install.='<div style="color: grey; cursor: help;" title="'.Lang::t('_PLUGIN_ERROR_UNINSTALL_DEPENDENCE', 'configuration').' '.$dependencies.'">'.Lang::t('_PLUGIN_UNINSTALL', 'configuration').'</div>';
-                    $activate = '<div style="color: grey;cursor: help;" title="'.Lang::t('_PLUGIN_ERROR_DEACTIVATE_DEPENDENCE', 'configuration').' '.$dependencies.'">'.Lang::t('_PLUGIN_DEACTIVATE', 'configuration').'</div>';
-                } else {
-                    $install.='<a style="color: #C84000;" href="javascript:askUninstall(\'index.php?r=adm/pluginmanager/uninstall'.'&plugin='.$info['name'].'\');">'.Lang::t('_PLUGIN_UNINSTALL', 'configuration').'</a>';
-                    if ($info['update']){
-                        $error="";
-                        if (!class_exists('ZipArchive')){
-                            $error.= Lang::t('_PLUGIN_ERROR_NOT_ONLINE_UPDATE', 'configuration')."<br>";
-                        }
-                        $install.= ' <a title="'.$error.'" style="color: #006d07;" href="index.php?r=adm/pluginmanager/update'.'&plugin='.$info['name'].'&online='.$info['online'].'">'.Lang::t('_PLUGIN_UPDATE', 'configuration').'</a>';
-                    }
-                    //if active
-                    if ($info['active']=="1"){
-                        $activate.=' <a style="color: #C84000;" href="index.php?r=adm/pluginmanager/deactivate'.'&plugin='.$info['name'].'">'.Lang::t('_PLUGIN_DEACTIVATE', 'configuration').'</a>';
-                        //if not active
-                    } else {
-                        $activate.=' <a style="color: #C84000;" href="index.php?r=adm/pluginmanager/activate'.'&plugin='.$info['name'].'">'.Lang::t('_PLUGIN_ACTIVATE', 'configuration').'</a>';
-                    }
-                }
-            } else {
-                $errors.= '<a style="color: #c80014;" href="javascript:;">'.Lang::t('_PLUGIN_ERROR_OLD_VERSION', 'configuration').'</a>';
-            }
-        } else {
-            $install.='<div style="color: grey;cursor: help;"title="'.Lang::t('_PLUGIN_ERROR_CORE_UNINSTALL', 'configuration').'">'.Lang::t('_PLUGIN_UNINSTALL', 'configuration').'</div>';
-            $activate = '<div style="color: grey;cursor: help;" title="'.Lang::t('_PLUGIN_ERROR_CORE_DEACTIVATE', 'configuration').'">'.Lang::t('_PLUGIN_DEACTIVATE', 'configuration').'</div>';
-        }
-        $priority.='
-            <form>
-                <input type="hidden" name="r" value="adm/pluginmanager/set_priority"/>
-                <input type="hidden" name="plugin" value="'.$info['name'].'"/>
-                <input type="number" name="priority" value="'.$info['priority'].'"/>
-                <button submit>'.Lang::t('_PLUGIN_SET_PRIORITY', 'configuration').'</button>
-            </form>
-        ';
-        // <a style="color: #C84000;" href="index.php?r=adm/pluginmanager/set_priority'.'&plugin='.$info['name'].'&priority='. 1 .'">'.Lang::t('_PLUGIN_SET_PRIORITY', 'configuration').'</a>
-        $settings=' <a style="color: #C84000;" href="index.php?r=adm/pluginmanager/showSettings'.'&plugin='.$info['name'].'">'.Lang::t('_PLUGIN_SETTINGS', 'configuration').'</a>';
-        //if not in database
-    } else {
-        if (!$info['dependencies_unsatisfied']){
-            $install.='<a style="color: #C84000;" href="index.php?r=adm/pluginmanager/install&plugin='.$info['name'].'">'.Lang::t('_PLUGIN_INSTALL', 'configuration').'</a>';
-            $install.=' <a style="color: red;" href="javascript:askPurge(\'index.php?r=adm/pluginmanager/purge'.'&plugin='.$info['name'].'\');">'.Lang::t('_PLUGIN_PURGE', 'configuration').'</a>';
-        } else {
-            $dependencies = "";
-            foreach ($info['dependencies_unsatisfied'] as $k => $v){
-                $dependencies .= "\n".$k.": ".$v;
-            }
-            $install.='<div style="color: grey;cursor: help;" title="'.Lang::t('_PLUGIN_ERROR_NO_DEPENDENCIES', 'configuration').':'.$dependencies.'">'.Lang::t('_PLUGIN_INSTALL', 'configuration').'</div>';
-        }
-    }
-    $table->addBody(array(
-        $info['title'],
-        $info['version'],
-        $info['author'],
-        $info['category'],
-        $info['description'],
-        $settings,
-        $install,
-        $activate,
-        $priority
-    ));
-}
 echo "<h2>".Lang::t('_PLUGIN_UPLOAD', 'configuration')."</h2><br/><br/>";
 echo Form::openForm( "plugin_upload" , "index.php?r=adm/pluginmanager/upload", false, false, "multipart/form-data");
 echo Form::getInputFilefield("", "plugin_file_upload", "plugin_file_upload", "", "",  "");
 echo "<br/>";
-echo Form::getButton("", "submit_upload", "Upload", "btn btn-primary");
+echo Form::getButton("", "submit_upload", "Upload", "forma-button forma-button--orange-hover", 'style="width: auto; padding-left: 10px; padding-right: 10px;"');
 echo Form::closeForm();
-echo "<hr/>";
-echo $table->getTable();
+?>
 
-echo '
+<hr/>
+<?php include Forma::inc(_lib_ . '/formatable/include.php');?>
+<div><?php echo $feedback; ?></div>
+<table class="table table-striped table-bordered display" style="width:100%" id="plugins"></table>
 <script>
 function askUninstall(link){
-    if (confirm("'.Lang::t('_PLUGIN_UNINSTALL_CONFIRMATION', 'configuration').'")) {
+    if (confirm("<?php echo Lang::t('_PLUGIN_UNINSTALL_CONFIRMATION', 'configuration')?>")) {
         location.href=link;
     }
 }
 function askPurge(link){
-    if (confirm("'.Lang::t('_PLUGIN_PURGE_CONFIRMATION', 'configuration').'")) {
+    if (confirm("<?php echo Lang::t('_PLUGIN_PURGE_CONFIRMATION', 'configuration')?>")) {
         location.href=link;
     }
 }
+$(function() {
+    var table = $('#plugins').FormaTable({
+        rowId: "plugin_id",  // cambia
+        processing: true,
+        serverSide: false,
+        paging: false,
+        columns: [  // definisco le colonne
+            { data: 'title', title: '<?php echo Lang::t('_PLUGIN_NAME', 'configuration'); ?>', sortable: true, width: '15%'},
+            { data: 'version', title: '<?php echo Lang::t('_PLUGIN_VERSION', 'configuration'); ?>', sortable: true, width: 0 },
+            { data: 'author', title: '<?php echo Lang::t('_PLUGIN_AUTHOR', 'configuration'); ?>', sortable: true, width: '10%' },
+            { data: 'category', title: '<?php echo Lang::t('_PLUGIN_CATEGORY', 'configuration'); ?>', sortable: true, width: '10%' },
+            { data: 'description', title: '<?php echo Lang::t('_PLUGIN_DESCRIPTION', 'configuration'); ?>', sortable: false },
+            { title: '<?php echo Lang::t('_PLUGIN_INSTALL', 'configuration'); ?>', width: '5%', sortable: false, searchable: false, render: function(data, type, row, meta) {
+                $info = row
+                $install = ""
+                $errors="";
+                if ($info['plugin_id']){
+                    if ($info['core']==="0"){
+                        if (!$info['version_error']){
+                            if ($info['dependence_of']){
+                                $dependencies = "";
+                                for (var $k = 0; $k < $info['dependence_of'].length; $k++) {
+                                    $dependencies += "\n" + $k + ": " + $info['dependence_of'][$k];
+                                }
+                                $install+='<div style="color: grey; cursor: help;" title=" <?php echo Lang::t('_PLUGIN_ERROR_UNINSTALL_DEPENDENCE', 'configuration')?>' + $dependencies + '"><?php echo Lang::t('_PLUGIN_UNINSTALL', 'configuration')?></div>';
+                            } else {
+                                $install+='<a style="color: #C84000;" href="javascript:askUninstall(\'index.php?r=adm/pluginmanager/uninstall'+'&plugin=' + $info['name'] + '\');"><?php echo Lang::t('_PLUGIN_UNINSTALL', 'configuration')?></a>';
+                                if ($info['update']){
+                                    $error="";
+                                    <?php if (!class_exists('ZipArchive')): ?>
+                                        $error+= '<?php echo Lang::t('_PLUGIN_ERROR_NOT_ONLINE_UPDATE', 'configuration')?><br>';
+                                    <?php endif; ?>
+                                    $install+= ' <a title="' + $error + '" style="color: #006d07;" href="index.php?r=adm/pluginmanager/update' + '&plugin=' + $info['name'] + '&online=' + $info['online'] + '"><?php echo Lang::t('_PLUGIN_UPDATE', 'configuration')?></a>';
+                                }
+                            }
+                        }else {
+                            $errors+= '<a style="color: #c80014;" href="javascript:;"><?php echo Lang::t('_PLUGIN_ERROR_OLD_VERSION', 'configuration')?></a>';
+                        }
+                    }
+                } else {
+                    if (!$info['dependencies_unsatisfied']){
+                        $install+='<a style="color: #C84000;" href="index.php?r=adm/pluginmanager/install&plugin=' + $info['name'] + '"><?php echo Lang::t('_PLUGIN_INSTALL', 'configuration')?></a>';
+                    } else {
+                        $dependencies = "";
+                        for (var $k = 0; $k < $info['dependencies_unsatisfied'].length; $k++) {
+                            $dependencies += "\n" + $k + ": " + $info['dependencies_unsatisfied'][$k];
+                        }
+                        $install+='<div style="color: grey;cursor: help;" title="<?php echo Lang::t('_PLUGIN_ERROR_NO_DEPENDENCIES', 'configuration')?>:' + $dependencies + '"><?php echo Lang::t('_PLUGIN_INSTALL', 'configuration')?></div>';
+                    }
+                }
+                return $install + $errors
+            }},
+            { width: '5%', sortable: false, searchable: false, render: function(data, type, row, meta) {
+                if (row['plugin_id']){
+                    return ' <a style="color: #C84000;" href="index.php?r=adm/pluginmanager/showSettings'+'&plugin=' + row['name'] + '"><?php echo Lang::t('_PLUGIN_SETTINGS', 'configuration')?></a>';
+                } else {
+                    return '<a style="color: red;" href="javascript:askPurge(\'index.php?r=adm/pluginmanager/purge'+'&plugin=' + $info['name'] + '\');"><?php echo Lang::t('_PLUGIN_PURGE', 'configuration')?></a>';
+                }
+            }},
+            { title: '<?php echo Lang::t('_PLUGIN_ACTIVATE', 'configuration'); ?>', width: '5%', sortable: false, searchable: false, render: function(data, type, row, meta) {
+                $info = row
+                $activate="";
+                if ($info['plugin_id']){
+                    if ($info['core']==="0"){
+                        if (!$info['version_error']){
+                            if ($info['dependence_of']){
+                                $dependencies = "";
+                                for (var $k = 0; $k < $info['dependence_of'].length; $k++) {
+                                    $dependencies += "\n" + $k + ": " + $info['dependence_of'][$k];
+                                }
+                                $activate = '<div style="color: grey;cursor: help;" title="<?php echo Lang::t('_PLUGIN_ERROR_DEACTIVATE_DEPENDENCE', 'configuration')?> ' + $dependencies + '"><?php echo Lang::t('_PLUGIN_DEACTIVATE', 'configuration')?></div>';
+                            } else {
+                                //if active
+                                if ($info['active']=="1"){
+                                    $activate+=' <a style="color: #C84000;" href="index.php?r=adm/pluginmanager/deactivate'+'&plugin=' + $info['name'] + '"><?php echo Lang::t('_PLUGIN_DEACTIVATE', 'configuration')?></a>';
+                                    //if not active
+                                } else {
+                                    $activate+=' <a style="color: #C84000;" href="index.php?r=adm/pluginmanager/activate'+'&plugin=' + $info['name'] + '"><?php echo Lang::t('_PLUGIN_ACTIVATE', 'configuration')?></a>';
+                                }
+                            }
+                        }
+                    }
+                }
+                return $activate;
+        }},
+        { title: '<?php echo Lang::t('_PRIORITY', 'configuration'); ?>', width: '150px', sortable: false, searchable: false, render: function(data, type, row, meta) {
+            $info = row
+            if ($info['plugin_id']){
+                return '' +
+                    '<form>' +
+                        '<input type="hidden" name="r" value="adm/pluginmanager/set_priority"/>' +
+                        '<input type="hidden" name="plugin" value="' + $info['name'] + '"/>' +
+                        '<input style="height: 25px; max-width: 70px; font-size: 13px;" class="form-control" type="number" name="priority" value="' + $info['priority'] + '"/>' +
+                        '<button style="display: inline-block; width: auto; margin-left: 10px; height: 25px; line-height: 20px; padding-left: 10px; padding-right:10px;" class="forma-button forma-button--orange-hover" submit><?php echo Lang::t('_SET', 'configuration')?></button>' +
+                    '</form>'+
+                '';
+            }
+        }},
+        ],
+        ajax: {
+            url: "<?php echo Get::rel_path() . "/ajax.adm_server.php?r=pluginmanager/getTableData"; ?>",
+            type: "GET"
+        },
+        order: [[ 0, "asc" ]],
+    });
+});
 </script>
-';
-
-echo $feedback;
+<style>
+    #plugins tr td{
+        height: 28px;
+    }
+</style>
