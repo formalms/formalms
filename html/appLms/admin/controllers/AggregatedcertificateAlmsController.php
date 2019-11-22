f3c9e2a7e20d84ef8922d7fa894c97724cd6e587
ggiorginogrifo <52072967+ggiorginogrifo@users.noreply.github.com>
2019-11-22 09:05:30 +0100

    Update AggregatedcertificateAlmsController.php

--- html/appLms/admin/controllers/AggregatedcertificateAlmsController.php
+++ html/appLms/admin/controllers/AggregatedcertificateAlmsController.php
@@ -76,7 +76,7 @@ Class AggregatedcertificateAlmsController extends AlmsController
                 'del_association' => 'delAssociations',
                 'saveMetadataAssoc' => 'saveMetadataAssoc',
                 
-                'associationusers' => 'associationUsers',
+                'associationusers' => 'associationusers',
                 'associationCourses' => 'associationCourses',
                 
                 
