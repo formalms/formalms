48a84a17cf639e65d680c05d72a7016a4170a32d
Gregorio <giorgino.greg@gmail.com>
2020-02-23 11:47:14 +0100

    Modificate alcune label, aggiunte traduzioni, aggiunto bugfix cristina

--- html/appLms/admin/views/aggregatedcertificate/associationsManagement.php
+++ html/appLms/admin/views/aggregatedcertificate/associationsManagement.php
@@ -1,7 +1,7 @@
 <?php
     
 
-    cout(    getTitleArea(Lang::t('_ASSOCIATIONS_AGGREGATED_CERTIFICATES', 'certificate'), 'certificate')
+    cout(    getTitleArea(Lang::t('_ASSOCIATIONS_AGGREGATED_CERTIFICATES'), 'certificate')
                 .'<div class="std_block">');
 
     
--- html/appLms/admin/views/aggregatedcertificate/show.php
+++ html/appLms/admin/views/aggregatedcertificate/show.php
@@ -1,6 +1,6 @@
 <?php
 
-cout(	getTitleArea(Lang::t('_TITLE_META_CERTIFICATE','certificate'), 'certificate')
+cout(	getTitleArea(Lang::t('_TITLE_AGGREGATED_CERTIFICATES'), 'certificate')
     .'<div class="std_block">'	);
 
 $form = new Form();
