46f1e1ad20fc4a61427c79a7d65521d8112707ed
Alessandro Micelli <alessandro.micelli@email.it>
2020-03-18 11:20:52 +0000

    Add courselist event
    (cherry picked from commit 8287002ae22a2edacf9fe7cb3110f62c94213cc9)

---
+++ html/appCore/Events/Core/Courses/CoursesListEvent.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace appCore\Events\Core\Courses;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class CoursesListEvent extends Event
+{
+    const EVENT_NAME_LIST = 'core.course.list';
+
+    /** @var array */
+    protected $courses;
+
+    /** @var array */
+    protected $coursesWithCertificates;
+
+    /** @var array */
+    protected $coursesWithCompetences;
+
+    /** @var array */
+    protected $coursesList;
+
+    /** @var string */
+    protected $base_link_course;
+
+    /** @var string */
+    protected $base_link_classroom;
+
+    /** @var string */
+    protected $base_link_edition;
+
+    /** @var string */
+    protected $base_link_subscription;
+
+    /** @var string */
+    protected $base_link_competence;
+
+    /**
+     * CoursesListEvent constructor.
+     * @param $coursesList
+     */
+    public function __construct($coursesList, $courses, $coursesWithCertificates, $coursesWithCompetences)
+    {
+        $this->coursesList = $coursesList;
+        $this->courses = $courses;
+        $this->coursesWithCertificates = $coursesWithCertificates;
+        $this->coursesWithCompetences = $coursesWithCompetences;
+
+        $this->base_link_course = 'alms/course';
+        $this->base_link_classroom = 'alms/classroom';
+        $this->base_link_edition = 'alms/edition';
+        $this->base_link_subscription = 'alms/subscription';
+        $this->base_link_competence = 'adm/competences';
+    }
+
+    /**
+     * @return array
+     */
+    public function getCoursesList()
+    {
+        return $this->coursesList;
+    }
+
+    /**
+     * @param array $coursesList
+     */
+    public function setCoursesList($coursesList)
+    {
+        $this->coursesList = $coursesList;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCourses(): array
+    {
+        return $this->courses;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCoursesWithCertificates(): array
+    {
+        return $this->coursesWithCertificates;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCoursesWithCompetences(): array
+    {
+        return $this->coursesWithCompetences;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseLinkCourse(): string
+    {
+        return $this->base_link_course;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseLinkClassroom(): string
+    {
+        return $this->base_link_classroom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseLinkEdition(): string
+    {
+        return $this->base_link_edition;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseLinkSubscription(): string
+    {
+        return $this->base_link_subscription;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseLinkCompetence(): string
+    {
+        return $this->base_link_competence;
+    }
+
+
+}
