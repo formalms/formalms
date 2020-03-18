9fd3ccb06017dc2fce0734e0c5c5e9e9a6255f83
Giuseppe Nucifora <me@giuseppenucifora.com>
2020-03-18 11:06:38 +0100

    - #19687 Languages - Increase text_key field lenght to 255

--- html/upgrade/data/upg_data/30000_db.sql
+++ html/upgrade/data/upg_data/30000_db.sql
@@ -1,1 +1,5 @@
+-- #FAY-44 migrate course info to mvc
 UPDATE `learning_module` SET `mvc_path` = 'lms/course/infocourse' WHERE `learning_module`.`module_name` = "course" AND `learning_module`.`default_op` = "infocourse"
+
+-- #19687 Languages - Increase text_key field lenght to 255
+ALTER TABLE `core_lang_text` MODIFY COLUMN `text_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' AFTER `id_text`
