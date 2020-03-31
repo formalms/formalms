778524b6cb29ed6a8f962503927f50b1e81023bd
Giuseppe Nucifora <me@giuseppenucifora.com>
2020-03-31 08:30:22 +0200

    [be] - add dashboard queries in install and upgrade

--- html/upgrade/data/upg_data/30000_db.sql
+++ html/upgrade/data/upg_data/30000_db.sql
@@ -2,11 +2,11 @@
 UPDATE `learning_module`
 SET `mvc_path` = 'lms/course/infocourse'
 WHERE `learning_module`.`module_name` = "course"
-  AND `learning_module`.`default_op` = "infocourse"
+  AND `learning_module`.`default_op` = "infocourse";
 
 -- #19687 Languages - Increase text_key field lenght to 255
 ALTER TABLE `core_lang_text`
-    MODIFY COLUMN `text_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' AFTER `id_text`
+    MODIFY COLUMN `text_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' AFTER `id_text`;
 
 
 
@@ -41,3 +41,43 @@ VALUES (7, 'DashboardBlockCalendarLms'),
        (2, 'DashboardBlockProfileLms'),
        (8, 'DashboardBlockVideoLms'),
        (1, 'DashboardBlockWelcomeLms');
+
+
+INSERT INTO learning_middlearea (`obj_index`, `disabled`, `idst_list`, `sequence`)
+VALUES ('tb_dashboard', '1', 'a:0:{}', '0');
+
+
+-- label
+INSERT IGNORE INTO core_lang_text (text_key, text_module, text_attributes)
+VALUES ('_DASHBOARD', 'middlearea', '');
+
+INSERT IGNORE INTO core_lang_translation (id_text, lang_code, translation_text)
+VALUES ((SELECT id_text FROM core_lang_text where text_key = '_DASHBOARD ' and text_module = ' middlearea '),
+        'english', 'Dashboard');
+INSERT IGNORE INTO core_lang_translation (id_text, lang_code, translation_text)
+VALUES ((SELECT id_text FROM core_lang_text where text_key = '_DASHBOARD ' and text_module = ' middlearea '),
+        'italian', 'Dashboard');
+
+INSERT INTO `learning_module`
+VALUES (47, 'dashboard', 'show', '_DASHBOARD', 'view', '', '', 'all', 'lms/dashboard/show');
+
+SET @max = (SELECT MAX(idMenu) + 1
+            FROM `core_menu`);
+
+INSERT INTO `core_menu`(`idMenu`, `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform`)
+VALUES (@max, '_DASHBOARD', '', 4, 'true', 'true', NULL, NULL, 'lms');
+
+INSERT INTO `core_menu_under`(`idUnder`, `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`,
+                              `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path`)
+VALUES (@max, @max, 'course', '_DASHBOARD', NULL, 'view', 'lms', 4, NULL, NULL, 'lms/dashboard/show');
+
+SET @max = (SELECT MAX(idMenu) + 1
+            FROM `core_menu`);
+
+INSERT INTO `core_menu`(`idMenu`, `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform`)
+VALUES (@max, '_DASHBOARD_CONFIGURATION', '', 4, 'true', 'true', '5', NULL, 'framework');
+
+INSERT INTO `core_menu_under`(`idUnder`, `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`,
+                              `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path`)
+VALUES (@max, @max, 'dashboardsettings', '_DASHBOARD_CONFIGURATION', '', 'view', 'framework', 1, '', '',
+        'adm/dashboardsettings/show');
