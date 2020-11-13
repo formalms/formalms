7e5ba3a626fad118f3a1631095317bca04247d32
Alessandro Micelli <alessandro.micelli@email.it>
2020-11-13 09:30:13 +0000

    Fix num partecipazioni reg val dettaglio utente

--- html/appLms/controllers/CoursereportLmsController.php
+++ html/appLms/controllers/CoursereportLmsController.php
@@ -1254,7 +1254,7 @@ class CoursereportLmsController extends LmsController
         SELECT DATE_FORMAT(tt.date_attempt, '%d/%m/%Y %H:%i'), tt.score, tt.idTest, t.idUser, tt.number_time
         FROM " . $GLOBALS['prefix_lms'] . "_testtrack_times AS tt
         LEFT JOIN " . $GLOBALS['prefix_lms'] . "_testtrack AS t ON tt.idTrack=t.idTrack
-        WHERE tt.idTest = '" . $idTest . "' ORDER BY tt.date_attempt"; // tt.idTrack = '" . $idTrack . "' AND
+        WHERE tt.idTrack = '" . $idTrack . "' AND tt.idTest = '" . $idTest . "' ORDER BY tt.date_attempt";
 		$re_testreport = sql_query($query_testreport);
 
 		$test_man = new GroupTestManagement();
