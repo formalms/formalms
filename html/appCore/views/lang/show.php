18d839605a7fdf200f4c398f17be9f9b9b2f90d2
Alessandro Micelli <alessandro.micelli@email.it>
2021-01-15 11:25:28 +0000

    #19810 - Error in changing language Fix modale per modifica lingua

--- html/appCore/views/lang/show.php
+++ html/appCore/views/lang/show.php
@@ -5,6 +5,20 @@
 <a id="addlang_bottom" href="#" class="ico-wt-sprite subs_add" title="<?php echo Lang::t('_ADD', 'standard'); ?>"><span><?php echo Lang::t('_ADD', 'standard'); ?></span></a>
 <a href="index.php?r=adm/lang/import" class="ico-wt-sprite subs_import" title="<?php echo Lang::t('_IMPORT', 'standard'); ?>"><span><?php echo Lang::t('_IMPORT', 'standard'); ?></span></a>
 
+<!-- Modal edit -->
+<div class="modal" id="modModal" style="display: none; z-index: 9999;">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-body" data-val="body">
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-val="cancel"><?php echo Lang::t('_CANCEL', 'standard'); ?></button>
+				<button type="button" class="btn btn-primary" data-val="submit"><?php echo Lang::t('_SAVE', 'standard'); ?></button>
+			</div>
+		</div>
+	</div>
+</div>
+
 <!-- Modal confirm -->
 <div class="modal" id="confirmModal" style="display: none; z-index: 9999;">
 	<div class="modal-dialog">
@@ -18,7 +32,6 @@
 		</div>
 	</div>
 </div>
-
 <?php
 
 $this->widget('dialog', array(
@@ -30,21 +43,21 @@ $this->widget('dialog', array(
 		array('caller' => 'addlang_bottom', 'event' => 'click')
 	)
 ));
+
 ?>
 <script type="text/javascript">
 	var confirmDialog = function(modalSelector, message, onConfirm) {
+		var modal = $(modalSelector);
 		var fClose = function() {
 			modal.modal("hide");
 		};
-		var modal = $(modalSelector);
 		modal.modal("show");
 		modal.find("*[data-val=body]").empty().append(message);
 		modal.find("*[data-val=yes]").unbind().one('click', onConfirm).one('click', fClose);
 		modal.find("*[data-val=no]").unbind().one("click", fClose);
-	}
+	};
 
 	var delFunc = function(id) {
-		console.log(id)
 		confirmDialog("#confirmModal", "<?php echo Lang::t('_AREYOUSURE', 'standard'); ?>", function() {
 			$.ajax({
 				type: 'POST',
@@ -56,6 +69,45 @@ $this->widget('dialog', array(
 					location.reload();
 				}
 			});
+		});
+	};
+
+	var openModFunc = function(id) {
+		var modalSelector = '#modModal';
+		var modal = $(modalSelector);
+		var fClose = function() {
+			modal.modal("hide");
+		};
+		$.ajax({
+			type: 'GET',
+			url: 'ajax.adm_server.php?r=adm/lang/mod',
+			data: {
+				lang_code: id
+			},
+			success: function(data) {
+				var form = JSON.parse(data).body;
+				modal.find('*[data-val=body]').html(form);
+				modal.modal("show");
+				modal.find("*[data-val=cancel]").unbind().one("click", fClose);
+				modal.find("*[data-val=submit]").unbind().one("click", modFunc);
+			}
+		});
+	};
+
+	var modFunc = function() {
+		var modalSelector = '#modModal';
+		var modal = $(modalSelector);
+
+		$.ajax({
+			type: 'POST',
+			url: 'ajax.adm_server.php?r=adm/lang/updatelang',
+			data: modal.find('form').serialize(),
+			success: function(data) {
+				data = JSON.parse(data);
+				if (data.success) {
+					location.reload();
+				}
+			}
 		});
 	};
 
@@ -118,7 +170,7 @@ $this->widget('dialog', array(
 			item.lang_diff = link;
 			link = '<a id="' + item.id + '" href="' + item.lang_export + '" class="ico-sprite subs_download" title="<?php echo Lang::t("_EXPORT_XML", "admin_lang"); ?>"><span></span></a>'
 			item.lang_export = link;
-			link = '<a id="' + item.id + '" href="' + item.lang_mod + '" class="ico-sprite subs_mod" title="<?php echo Lang::t("_MOD", "admin_lang"); ?>"><span></span></a>'
+			link = '<a id="' + item.id + '" href="#" onclick="openModFunc(\'' + item.lang_code + '\')" class="ico-sprite subs_mod" title="<?php echo Lang::t("_MOD", "admin_lang"); ?>"><span></span></a>'
 			item.lang_mod = link;
 			link = '<a id="' + item.id + '" href="#" onclick="delFunc(\'' + item.lang_code + '\')" class="ico-sprite subs_del" title="<?php echo Lang::t("_DEL", "admin_lang"); ?>"><span></span></a>'
 			item.lang_del = link;
