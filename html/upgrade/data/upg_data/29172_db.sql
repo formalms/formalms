858b558b3d038f2058afdead3638eccd9720acb4
lucarz <l.rizzuti@grifomultimedia.it>
2021-03-03 18:21:53 +0100

    #19837 aggiunta query in upgrade per la rimozione della chat dal menu

--- html/upgrade/data/upg_data/29172_db.sql
+++ html/upgrade/data/upg_data/29172_db.sql
@@ -1,2 +1,4 @@
 INSERT IGNORE INTO `dashboard_blocks` (`id`, `block_class`, `created_at`)
 VALUES (9, 'DashboardBlockWelcomeLms',CURRENT_TIMESTAMP);
+
+delete from learning_module where module_name like 'chat';
