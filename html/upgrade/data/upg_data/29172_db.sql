79e5be01891d36118e534df2a73d887c34b38715
Massimiliano Cavazzini <max.cavazzini@joint-tech.it>
2021-03-04 14:50:42 +0100

    #19767 - Add ignoreScore in global setting - upgrade

--- html/upgrade/data/upg_data/29172_db.sql
+++ html/upgrade/data/upg_data/29172_db.sql
@@ -1,4 +1,14 @@
 INSERT IGNORE INTO `dashboard_blocks` (`id`, `block_class`, `created_at`)
 VALUES (9, 'DashboardBlockWelcomeLms',CURRENT_TIMESTAMP);
 
+-- delete menu chat
 delete from learning_module where module_name like 'chat';
+
+-- add property ignorescore in scorm
+ALTER TABLE `learning_organization` ADD `ignoreScore` TINYINT( 4 ) NOT NULL DEFAULT '0';
+
+-- setting ignore_score
+INSERT INTO `core_setting` 
+(`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`) 
+VALUES
+('ignore_score', 'off', 'enum', 3, '0', 4, 16, 1, 0, '');
