0a0170fdef74cbfe0605a270b0cf42965200e303
Giuseppe Nucifora <me@giuseppenucifora.com>
2021-04-09 09:46:06 +0200

    - Fix minutes required in certificate assign
    (cherry picked from commit 00db2682d85250f57ccb323705bc72ce66dca7b8)

--- html/appLms/admin/controllers/CourseAlmsController.php
+++ html/appLms/admin/controllers/CourseAlmsController.php
@@ -1104,10 +1104,10 @@ Class CourseAlmsController extends AlmsController
 		if(isset($_POST['assign']))
 		{
 			$point_required = Get::req('point_required', DOTY_INT, 0);
-			
+            $minutesRequired = Get::req('certificate_assign_minutes', DOTY_INT, 0);
 			// , $list_of_assign_obj, $list_of_who
 			
-			if(!$cert->updateCertificateCourseAssign($id_course, $_POST['certificate_assign'], $_POST['certificate_ex_assign'], $point_required))
+			if(!$cert->updateCertificateCourseAssign($id_course, $_POST['certificate_assign'], $_POST['certificate_ex_assign'], $point_required,$minutesRequired))
 				Util::jump_to('index.php?r='.$this->base_link_course.'/show&err=_up_cert_err');
 			Util::jump_to('index.php?r='.$this->base_link_course.'/show&res=_up_cert_ok');
 		}
