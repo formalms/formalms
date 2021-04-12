9bf175e3d49495c87ed703ef14653d41ceaabe02
Alessandro Micelli <alessandro.micelli@email.it>
2021-04-12 10:00:38 +0100

    Added link on tooltip dashboard calendar click

--- frontend-src/standard/src/scripts/modules/DashboardCalendar.js
+++ frontend-src/standard/src/scripts/modules/DashboardCalendar.js
@@ -43,6 +43,7 @@ export const RenderDashBoardCalendar = () => {
                   successCallback(
                       parsedData.response.map((item) => {
                         return {
+                          id: item.id,
                           title: item.title,
                           start: item.startDate,
                           type: item.type,
@@ -79,6 +80,7 @@ export const RenderDashBoardCalendar = () => {
                   successCallback(
                       parsedData.response.map((item) => {
                         return {
+                          id: item.id,
                           title: item.title,
                           start: item.startDate,
                           type: item.type,
@@ -115,6 +117,7 @@ export const RenderDashBoardCalendar = () => {
                   successCallback(
                       parsedData.response.map((item) => {
                         return {
+                          id: item.id,
                           title: item.title,
                           start: item.startDate,
                           type: item.type,
@@ -135,12 +138,13 @@ export const RenderDashBoardCalendar = () => {
             color: '#007CC8'
           },
         ],
-        eventClick: function() {
-          // renderPopup(event);
-          // console.log(item)
+        eventClick: function(item) {
+          const id = item.event.id;          
+          const url = `index.php?modname=course&op=aula&idCourse=${id}`;
+
+          window.location = url;
         },
         eventRender: function(item) {
-          // console.log(item);
           if (item.event.extendedProps.status) {
             item.el.classList.add('is-open');
           } else {
@@ -166,7 +170,6 @@ const renderPopup = (item) => {
   const desc = item.event.extendedProps.description;
   const hours = item.event.extendedProps.hours;
   const title = item.event.title;
-  // console.log(item.event)
 
   el += '<div class="d-popup">';
   el += '<div class="d-popup__item is-' + type + '">';
