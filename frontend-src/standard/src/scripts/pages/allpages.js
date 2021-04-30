df2d627e4c3d2fbd9960db3a525cd181a43d90fd
Alessandro Micelli <alessandro.micelli@email.it>
2021-04-30 12:02:35 +0100

    fix tiny url absolute in window.config

--- frontend-src/standard/src/scripts/pages/allpages.js
+++ frontend-src/standard/src/scripts/pages/allpages.js
@@ -3,6 +3,7 @@ require('../modules/folder-view');
 require('../modules/slider-menu');
 require('../modules/text-editor');
 require('../modules/modal-accordion');
+
 import { InfoCourse } from '../modules/InfoCourse';
 import { RenderDashBoardCalendar } from '../modules/DashboardCalendar';
 import { DashboardVideo } from '../modules/DashboardVideo';
