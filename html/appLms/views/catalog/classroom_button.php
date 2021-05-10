f9e928b06b3cafa066925b21271706501766a3b1
LivioM <l.melfi@grifomultimedia.it>
2021-05-10 20:17:38 +0200

    #course catalog refactoring

---
+++ html/appLms/views/catalog/classroom_button.php
@@ -0,0 +1,51 @@
+<div class="course-box__item" id="action_<?=$course['idCourse']?>">
+<?php if ($course['is_enrolled']) { ?> 
+        <?php if ( $course['canEnter'] ) {?> 
+                <a class="forma-button forma-button--orange-hover" href="index.php?modname=course&op=aula&idCourse=<?=$course['idCourse']?>"
+                        title="<?=$course['name']?>" <?=$str_rel?>>
+                    <span class="forma-button__label">
+                    <?=Lang::t('_USER_STATUS_ENTER', 'catalogue')?> 
+                    </span>
+               </a>
+        <?php } else {  ?>   
+                <a class="forma-button forma-button--disabled" href="javascript:void(0);">
+                        <span class="forma-button__label">
+                        <?=Lang::t('_DISABLED', 'course')?>
+                        </span>
+                </a>
+        <?php }  ?>
+
+<?php } else { ?>
+        <?php if ($course['selling'] == 0) { ?> 
+                <?php if ($course['subscribe_method'] == 2) { ?>
+                        <a class="forma-button forma-button--green forma-button--orange-hover" href="javascript:;" 
+                        onclick ="chooseEdition(<?=$course['idCourse']?>)">
+                            <span class="forma-button__label"><?=Lang::t('_SUBSCRIBE', 'catalogue')?></span>
+                        </a>
+                 <?php } elseif ($course['subscribe_method'] == 1) {  ?>      
+                        <a class="forma-button forma-button--green forma-button--orange-hover" href="javascript:;" 
+                            onclick ="chooseEdition(<?=$course['idCourse']?>)">
+                            <span class="forma-button__label"><?=Lang::t('_COURSE_S_MODERATE', 'catalogue')?></span>
+                        </a>
+                 <?php } elseif ($course['subscribe_method'] == 0) {  ?> 
+                        <a href="javascript:void(0);" class="forma-button forma-button--disabled">
+                            <span class="forma-button__label"><?=Lang::t('_COURSE_S_GODADMIN', 'catalogue')?></span>
+                        </a>
+                 <?php }  ?>                              
+        <?php } ?>     
+<?php } ?>
+</div>
+
+<?php if( !$course['is_enrolled'] && $course['selling'] == 0 && ($course['subscribe_method'] == 2 || $course['subscribe_method'] == 1 )) {?>
+    <div id ="editionOption" style='display: none;'>
+        ciccio
+    </div>
+<?php } ?>    
+
+
+
+
+
+
+
+
