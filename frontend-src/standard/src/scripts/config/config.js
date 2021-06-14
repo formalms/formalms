a0eab33018eebc49272687a47a27e7103ad471dd
esavino <savino.emiliano@gmail.com>
2021-06-14 15:03:08 +0200

    [fe] - fixed config.js

--- frontend-src/standard/src/scripts/config/config.js
+++ frontend-src/standard/src/scripts/config/config.js
@@ -1,5 +1,9 @@
 var Config = {
   projectName: 'formalms',
+  baseUrl: `${window.location.protocol}//${window.location.host}`,
+  apiUrl: `${window.location.protocol}//${window.location.host}/appLms/index.php?r=`,
+  shareUrl: `${window.location.protocol}//${window.location.host}/`,
+  assetsUrl: '/static'
 };
 
 module.exports = Config;
