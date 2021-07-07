444c9bdcf30ce50312bacf57ff9ca5a20fe73bbb
Giuseppe Nucifora <me@giuseppenucifora.com>
2021-07-07 17:05:22 +0200

    #19901 - Update installer with new date management

--- html/install/data/sql/lms.sql
+++ html/install/data/sql/lms.sql
@@ -3507,6 +3507,20 @@ INSERT IGNORE INTO `core_menu_under`(`idUnder`, `idMenu`, `module_name`, `defaul
 VALUES (@max, @max, 'dashboardsettings', '_DASHBOARD_CONFIGURATION', '', 'view', 'framework', 1, '', '',
         'adm/dashboardsettings/show');
 
+-- #19901 refactor day dates
+ALTER TABLE `learning_course_date_day`
+    ADD COLUMN `id` int(11) NOT NULL AUTO_INCREMENT FIRST,
+    ADD COLUMN `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP AFTER `pause_end`,
+    ADD COLUMN `updated_at` datetime NULL ON UPDATE CURRENT_TIMESTAMP AFTER `created_at`,
+    ADD COLUMN `calendarId` varchar(255) NOT NULL AFTER `pause_end`,
+    ADD COLUMN `deleted` tinyint(1) NULL DEFAULT 0 AFTER `calendarId`,
+    DROP PRIMARY KEY,
+    ADD PRIMARY KEY (`id`) USING BTREE,
+    DROP INDEX `id_date`,
+    ADD INDEX `id_day_date`(`id_day`, `id_date`) USING BTREE;
+
+ALTER TABLE `learning_course` ADD COLUMN `sendCalendar` tinyint(1) NULL DEFAULT 0;
+ALTER TABLE `learning_course` ADD COLUMN `calendarId` varchar(255) NOT NULL AFTER `sendCalendar`;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
