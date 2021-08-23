381e2fc827a82eecd065b67688549386206af0a4
Giuseppe Nucifora <me@giuseppenucifora.com>
2021-08-23 11:28:08 +0200

    - fix upgrader queries

--- html/upgrade/data/upg_data/29176_db.sql
+++ html/upgrade/data/upg_data/29176_db.sql
@@ -1,7 +1,9 @@
+SET @max = ((SELECT MAX( idClass )+1 FROM core_event_class));
+
 INSERT INTO core_event_class ( idClass, class, platform, description )
 VALUES 
-((SELECT MAX( idClass ) FROM core_event_class b) +1, 'UserCourseInsertOverbooking', 'lms-a', 'A user requests subscription to course that has set overbooking');
+(@max, 'UserCourseInsertOverbooking', 'lms-a', 'A user requests subscription to course that has set overbooking');
 
-INSERT INTO core_event_consumer_class (idConsumer, idClass) VALUES (1, (SELECT MAX( idClass ) FROM core_event_class));
+INSERT INTO core_event_consumer_class (idConsumer, idClass) VALUES (1, @max);
 
-INSERT INTO `core_event_manager` (`idEventMgr`, `idClass`, `permission`, `channel`, `recipients`, `show_level`) VALUES ((SELECT MAX( idClass ) FROM core_event_class), (SELECT MAX( idClass ) FROM core_event_class), 'mandatory', 'email', '_EVENT_RECIPIENTS_TEACHER', 'godadmin,admin,user');
+INSERT INTO `core_event_manager` (`idEventMgr`, `idClass`, `permission`, `channel`, `recipients`, `show_level`) VALUES (@max, @max, 'mandatory', 'email', '_EVENT_RECIPIENTS_TEACHER', 'godadmin,admin,user');
