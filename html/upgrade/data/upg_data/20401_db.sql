0a7a4115366c86f7af76a530908f84f0f6234161
LivioM <l.melfi@grifomultimedia.it>
2021-10-19 13:35:58 +0200

    #19953  Missing idempotence instructions
    (cherry picked from commit 154e09c122b610a9a316556d1e9fb14fa7781983)

--- html/upgrade/data/upg_data/20401_db.sql
+++ html/upgrade/data/upg_data/20401_db.sql
@@ -13,17 +13,17 @@ VALUES ('smtp_user', '', 'string', 255, 'Smtp User', 14, 4, 1, 0, '');
 INSERT IGNORE INTO core_setting (param_name, param_value, value_type, max_size, pack, regroup, sequence, param_load, hide_in_modify, extra_info)
 VALUES ('smtp_pwd', '', 'string', 255, 'Smtp Password', 14, 5, 1, 0, '');
 
-INSERT INTO `core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`)
+INSERT IGNORE INTO `core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`)
 VALUES ('smtp_auto_tls', '', 'on_off', 255, 'Smtp Auto TLS', 14, 7, 1, 0, '');
 
-INSERT INTO `core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`)
+INSERT IGNORE INTO `core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`)
 VALUES ('smtp_debug', '0', 'string', 255, 'Smtp Debug', 14, 8, 1, 0, '');
 
 INSERT IGNORE INTO `core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`)
 VALUES
 	('purchase_user', '', 'string', 255, 'ecommerce', 4, 18, 1, 0, '');
 
-INSERT INTO `core_event_class` (`idClass`, `class`, `platform`, `description`)
+INSERT IGNORE INTO `core_event_class` (`idClass`, `class`, `platform`, `description`)
 VALUES
 	(50, 'PurchaseCourse', 'lms', NULL);
 
