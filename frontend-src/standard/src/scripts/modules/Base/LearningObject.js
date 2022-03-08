bbd2be7d8f5b3b1c9e66d7f2b1fc02023771ad1b
Giuseppe Nucifora <me@giuseppenucifora.com>
2022-03-08 17:23:16 +0100

    #20038 - fix scorm close tracking

--- frontend-src/standard/src/scripts/modules/Base/LearningObject.js
+++ frontend-src/standard/src/scripts/modules/Base/LearningObject.js
@@ -1,35 +1,36 @@
-
 import ModalElement from '../../components/Modal';
 
 
 class LearningObject {
 
-  constuctor() {
-    
-  }
+    constuctor() {
 
-  scormLightbox(src, title, type, modatTarget = '#lms_main_container') {
+    }
 
-    const lightbox = new ModalElement('scorm-modal',modatTarget);
-    lightbox.Content = `<iframe src="${src}" frameborder="false" width="100%" height="100%" id="overlay_iframe" name="overlay_iframe">`;
-    lightbox.Title = title;
-    lightbox.OnClose = () => {
-      try {
-        window.frames['overlay_iframe'].uiPlayer.closePlayer(true, window);
-      } catch (e) {
-        if(window.overlay_iframe.uiPlayer) { 
-          window.overlay_iframe.uiPlayer.closePlayer(true, window);
+    scormLightbox(src, title, type, modatTarget = '#lms_main_container') {
+
+        const lightbox = new ModalElement('scorm-modal', modatTarget);
+        lightbox.Content = `<iframe src="${src}" frameborder="false" width="100%" height="100%" id="overlay_iframe" name="overlay_iframe">`;
+        lightbox.Title = title;
+        lightbox.OnClose = () => {
+            try {
+                window.frames['overlay_iframe'].uiPlayer.scormPlayer.api.forceFinish();
+                window.frames['overlay_iframe'].uiPlayer.closePlayer(true, window);
+            } catch (e) {
+                if (window.overlay_iframe.uiPlayer) {
+                    window.overlay_iframe.uiPlayer.scormPlayer.api.forceFinish();
+                    window.overlay_iframe.uiPlayer.closePlayer(true, window);
+                }
+            }
+            const container = document.querySelector('*[data-container=' + type + ']');
+            if (container) {
+                container.dispatchEvent(new CustomEvent('refreshTree', {
+                    detail: {selectedId: window.localStorage.getItem('selectedId')}
+                }))
+            }
         }
-      }
-      const container = document.querySelector('*[data-container=' + type + ']');
-      if (container) {
-        container.dispatchEvent(new CustomEvent('refreshTree', {
-          detail: {selectedId: window.localStorage.getItem('selectedId')}
-        }))
-      }
-    } 
-    lightbox.Open();
-  }
+        lightbox.Open();
+    }
 
 }
 
