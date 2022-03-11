2e0290e458a3c4c2f4b5a67c1a27033f76c51576
Giuseppe Nucifora <me@giuseppenucifora.com>
2022-03-11 17:05:10 +0100

    - Fix privacy and password renewal check

--- html/appCore/controllers/HomepageAdmController.php
+++ html/appCore/controllers/HomepageAdmController.php
@@ -389,9 +389,9 @@ class HomepageAdmController extends AdmController
 
         $res = $this->model->login($plugin);
 
-        if (!is_null($loginRedirect)) {
-            $url = substr_replace($loginRedirect, '', 0, strlen(trim(dirname($_SERVER['SCRIPT_NAME']), DIRECTORY_SEPARATOR)) + 1);
-            Util::jump_to($url);
+        if(!empty($loginRedirect)) {
+            $url = substr_replace($loginRedirect, "", 0, strlen(trim(dirname($_SERVER['SCRIPT_NAME']), DIRECTORY_SEPARATOR)) + 1);
+            $redirection['req'] = $url;
         }
 
         switch ($res) {
