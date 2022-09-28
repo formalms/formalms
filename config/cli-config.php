7a982b7f29cd965765f2ab96851a05ad6ee4fde2
Alex Vivona <ing.alessandro.vivona@gmail.com>
2022-09-28 11:58:24 +0200

    advance doctrine

--- config/cli-config.php
+++ config/cli-config.php
@@ -3,21 +3,23 @@
 define('IN_FORMA',true);
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Configuration\Migration\YamlFile;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
 
+use Symfony\Component\Console\Helper\HelperSet;
 // replace with file to your own project bootstrap
 require __DIR__ . '/../html/vendor/autoload.php';
 
 require __DIR__ . '/../html/config.php';
 
-use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
-use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-use Symfony\Component\Console\Helper\HelperSet;
-
 $paths = [__DIR__ . "/../html/Entity"];
 $isDevMode = true;
-
+$config = new YamlFile(__DIR__ . '/../migrations.yaml');
 // the connection configuration
 $dbParams = [
     'driver' => 'pdo_mysql',
@@ -27,29 +29,16 @@ $dbParams = [
     'host' => $cfg['db_host']
 ];
 
-//$cache = new \Doctrine\Common\Cache\ArrayCache();
-//
-//$reader = new \Doctrine\Common\Annotations\AnnotationReader();
-//$driver = new \Doctrine\ORM\Mapping\Driver\AnnotationDriver($reader, $paths);
 
-$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
-#$config->setMetadataCacheImpl( $cache );
-#$config->setQueryCacheImpl( $cache );
-#$config->setMetadataDriverImpl( $driver );
+$ORMconfig = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, null, false);
 
-$entityManager = EntityManager::create($dbParams, $config);
+
+$entityManager = EntityManager::create($dbParams, $ORMconfig);
 
 $platform = $entityManager->getConnection()->getDatabasePlatform();
 $platform->registerDoctrineTypeMapping('enum', 'string');
 
-#return new HelperSet([
-#    'em' => new EntityManagerHelper($entityManager),
-#    'db' => new ConnectionHelper($entityManager->getConnection()),
-#]);
-
-// replace with file to your own project bootstrap
-//require_once 'bootstrap.php';
-
+DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
 
 
 return ConsoleRunner::createHelperSet($entityManager);
