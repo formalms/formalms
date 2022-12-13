dda2d17708f644cc510e700ba32b2a48409791d8
Alex Vivona <ing.alessandro.vivona@gmail.com>
2022-12-13 17:54:57 +0100

    fix dashboard block calendar enabling actions

--- frontend-src/standard/src/scripts/modules/DashboardCalendar.js
+++ frontend-src/standard/src/scripts/modules/DashboardCalendar.js
@@ -4,161 +4,21 @@ import initialLocaleCode from '@fullcalendar/core/locales/it';
 
 export const DashBoardCalendar = () => {
   const els = document.querySelectorAll('.js-dashboard-calendar');
-
+  const colors = {
+      'getElearningCalendar' : '#A478EA',
+      'getClassroomCalendar' : '#007CC8',
+      'getReservationCalendar' : '#007CC8'
+    };
   if (els.length) {
+      
     for (let i = 0; i < els.length; i++) {
-      /* const actions = els[i].getAttribute('data-action');
+      const actions = els[i].getAttribute('data-action');
       const actionsArray = actions.split(',');
-      const sourcesArray = [];
-      let event = {};
-
-       for (i = 0; i < actionsArray.length; i++) {
-        if (actionsArray[i] == 'getElearningCalendar') {
-
-        } else if (actionsArray[i] == 'getClassroomCalendar') {
-
-        } else if (actionsArray[i] == 'getReservationCalendar') {
-
-        }
-      }*/
 
       const calendar = new Calendar(els[i], {
         plugins: [dayGridPlugin],
         locale: initialLocaleCode,
         height: 'auto',
-        eventSources: [
-          {
-            events: (fetchInfo, successCallback, failureCallback) => {
-              var dateSt = new Date(fetchInfo.startStr);
-              var startDate = dateSt.getFullYear()+'-' + ('0' + (dateSt.getMonth()+1)).slice(-2) + '-' + ('0'+dateSt.getDate()).slice(-2);
-              var dateEnd = new Date(fetchInfo.endStr);
-              var endDate = dateEnd.getFullYear() +'-' + ('0' + (dateEnd.getMonth()+1)).slice(-2) + '-' + ('0'+dateEnd.getDate()).slice(-2);
-           
-              $.ajax({
-                type: 'post',
-                url: window.dashboardCalendarAjaxUrl,
-                data: {
-                  blockAction: 'getElearningCalendar',
-                  authentic_request: window.dashboardCalendarAjaxSignature,
-                  block: 'DashboardBlockCalendarLms',
-                  dashboardLayoutId : window.dashboardLayoutId,
-                  startDate: startDate,
-                  endDate: endDate
-                },
-                success: function (data) {
-                  const parsedData = JSON.parse(data);
-                  successCallback(
-                      parsedData.response.map((item) => {
-                        return {
-                          id: item.id,
-                          title: item.title,
-                          start: item.startDate,
-                          type: item.type,
-                          status: item.status,
-                          description: item.description,
-                          hours: item.hours
-                        }
-                      })
-                  )
-                },
-                error: function (e) {
-                  failureCallback(
-                      () => console.log(e)
-                  )
-                }
-              });
-            },
-            color: '#A478EA'
-          },
-          {
-            events: (fetchInfo, successCallback, failureCallback) => {
-              var dateSt = new Date(fetchInfo.startStr);
-              var startDate = dateSt.getFullYear()+'-' + ('0' + (dateSt.getMonth()+1)).slice(-2) + '-' + ('0'+dateSt.getDate()).slice(-2);
-              var dateEnd = new Date(fetchInfo.endStr);
-              var endDate = dateEnd.getFullYear() +'-' + ('0' + (dateEnd.getMonth()+1)).slice(-2) + '-' + ('0'+dateEnd.getDate()).slice(-2);
-           
-              $.ajax({
-                type: 'post',
-                url: window.dashboardCalendarAjaxUrl,
-                data: {
-                  blockAction: 'getClassroomCalendar',
-                  authentic_request: window.dashboardCalendarAjaxSignature,
-                  block: 'DashboardBlockCalendarLms',
-                  dashboardLayoutId : window.dashboardLayoutId,
-                  startDate: startDate,
-                  endDate: endDate
-                },
-                success: function (data) {
-                  const parsedData = JSON.parse(data);
-
-                  successCallback(
-                      parsedData.response.map((item) => {
-                        return {
-                          id: item.id,
-                          title: item.title,
-                          start: item.startDate,
-                          type: item.type,
-                          status: item.status,
-                          description: item.description,
-                          hours: item.hours
-                        }
-                      })
-                  )
-                },
-                error: function (e) {
-                  failureCallback(
-                      () => console.log(e)
-                  )
-                }
-              });
-            },
-            color: '#007CC8'
-          },
-          {
-            events: (fetchInfo, successCallback, failureCallback) => {
-              var dateSt = new Date(fetchInfo.startStr);
-              var startDate = dateSt.getFullYear()+'-' + ('0' + (dateSt.getMonth()+1)).slice(-2) + '-' + ('0'+dateSt.getDate()).slice(-2);
-              var dateEnd = new Date(fetchInfo.endStr);
-              var endDate = dateEnd.getFullYear() +'-' + ('0' + (dateEnd.getMonth()+1)).slice(-2) + '-' + ('0'+dateEnd.getDate()).slice(-2);
-           
-              $.ajax({
-                type: 'post',
-                url: window.dashboardCalendarAjaxUrl,
-                data: {
-                  blockAction: 'getReservationCalendar',
-                  authentic_request: window.dashboardCalendarAjaxSignature,
-                  block: 'DashboardBlockCalendarLms',
-                  dashboardLayoutId : window.dashboardLayoutId,
-                  startDate: startDate,
-                  endDate: endDate
-                },
-                success: function (data) {
-                  const parsedData = JSON.parse(data);
-
-                  successCallback(
-                      parsedData.response.map((item) => {
-                        return {
-                          id: item.id,
-                          title: item.title,
-                          start: item.startDate,
-                          type: item.type,
-                          status: item.status,
-                          description: item.description,
-                          hours: item.hours
-                        }
-                      })
-                  )
-                },
-                error: function (e) {
-                  failureCallback(
-                      () => console.log(e)
-                  )
-                }
-              });
-            },
-            color: '#007CC8'
-          },
-        ],
         eventClick: function(item) {
           const id = item.event.id;          
           const url = `index.php?modname=course&op=aula&idCourse=${id}`;
@@ -175,8 +35,58 @@ export const DashBoardCalendar = () => {
         }
       });
 
+
       if (initialLocaleCode !== window.frontend.config.lang.currentLangCode) {
         calendar.setOption('locale', window.frontend.config.lang.currentLangCode);
+      }
+
+      for (i = 0; i < actionsArray.length; i++) {
+        let dataBlock = actionsArray[i];
+        let event = {
+          events: (fetchInfo, successCallback, failureCallback) => {
+            var dateSt = new Date(fetchInfo.startStr);
+            var startDate = dateSt.getFullYear()+'-' + ('0' + (dateSt.getMonth()+1)).slice(-2) + '-' + ('0'+dateSt.getDate()).slice(-2);
+            var dateEnd = new Date(fetchInfo.endStr);
+            var endDate = dateEnd.getFullYear() +'-' + ('0' + (dateEnd.getMonth()+1)).slice(-2) + '-' + ('0'+dateEnd.getDate()).slice(-2);
+         
+            $.ajax({
+              type: 'post',
+              url: window.dashboardCalendarAjaxUrl,
+              data: {
+                blockAction: dataBlock,
+                authentic_request: window.dashboardCalendarAjaxSignature,
+                block: 'DashboardBlockCalendarLms',
+                dashboardLayoutId : window.dashboardLayoutId,
+                startDate: startDate,
+                endDate: endDate
+              },
+              success: function (data) {
+                const parsedData = JSON.parse(data);
+                successCallback(
+                    parsedData.response.map((item) => {
+                      return {
+                        id: item.id,
+                        title: item.title,
+                        start: item.startDate,
+                        type: item.type,
+                        status: item.status,
+                        description: item.description,
+                        hours: item.hours
+                      }
+                    })
+                )
+              },
+              error: function (e) {
+                failureCallback(
+                    () => console.log(e)
+                )
+              }
+            });
+          },
+          color: colors[actionsArray[i]]
+        };
+   
+        calendar.addEventSource(event);
       }
 
       calendar.render();
