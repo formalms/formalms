f55b724be1e400f5192bd0f7da52c34a310417f0
Favour <frozmane@gmail.com>
2022-12-16 11:17:15 +0100

    Eliminato il file lib.domxml4.php e tutti isuoi riferimenti

--- html/lib/lib.domxml.php
+++ html/lib/lib.domxml.php
@@ -25,8 +25,4 @@ defined('IN_FORMA') or exit('Direct access is forbidden.');
  * @author		Emanuele Sandri <emanuele (@) docebo (.) com>
 **/
 
-if (version_compare(phpversion(), '5.0.0') == -1) {
-    require_once dirname(__FILE__) . '/lib.domxml4.php';
-} else {
-    require_once dirname(__FILE__) . '/lib.domxml5.php';
-}
+require_once dirname(__FILE__) . '/lib.domxml5.php';
