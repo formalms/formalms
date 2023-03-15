171b1581c249a332891daae8289bf11ec4c07767
Alex Vivona <ing.alessandro.vivona@gmail.com>
2023-03-15 10:20:46 +0100

    mini fixes

--- html/appLms/lib/lib.catalogue.php
+++ html/appLms/lib/lib.catalogue.php
@@ -289,6 +289,7 @@ class Catalogue_Manager
     public function getUserAllCatalogueId($id_user)
     {
         $catalogues = [];
+        $this->acl = new DoceboACL();
         $user_groups = $this->acl->getSTGroupsST($id_user);
 
         if (empty($user_groups)) {
