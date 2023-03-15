35224a544235bb74e1f718298a23b46d283e783e
Alessandro Vivona <ing.alessandro.vivona@gmail.com>
2023-03-15 19:44:34 +0100

    add event remapping

--- html/appLms/lib/lib.catalogue.php
+++ html/appLms/lib/lib.catalogue.php
@@ -289,6 +289,8 @@ class Catalogue_Manager
     public function getUserAllCatalogueId($id_user)
     {
         $catalogues = [];
+
+        $this->acl = Docebo::user()->getAclManager();
         $user_groups = $this->acl->getSTGroupsST($id_user);
 
         if (empty($user_groups)) {
