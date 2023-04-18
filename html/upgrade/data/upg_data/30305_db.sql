5f8cbc648a801294852bc166bb68d0578f453ca4
Giuseppe Nucifora <me@giuseppenucifora.com>
2023-04-18 17:54:02 +0200

    - fix dashboard permission

--- html/upgrade/data/upg_data/30305_db.sql
+++ html/upgrade/data/upg_data/30305_db.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `learning_test` ADD COLUMN `show_quest_score` tinyint(1) NOT NULL;
+UPDATE `core_role` SET `roleid` = '/lms/dashboard/view' WHERE `roleid` = '/lms/course/public/dashboard/view';
