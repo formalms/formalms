c87f2d785bfa8309fec53a4e2ff7ccf99f685907
LivioM <l.melfi@grifomultimedia.it>
2023-05-05 18:10:21 +0200

    #20146: added missing SQL instructions

--- html/install/data/sql/lms.sql
+++ html/install/data/sql/lms.sql
@@ -3486,7 +3486,8 @@ VALUES (1, 'DashboardBlockCalendarLms', CURRENT_TIMESTAMP),
        (6, 'DashboardBlockWelcomeLms',CURRENT_TIMESTAMP),
        (7, 'DashboardBlockBannerLms',CURRENT_TIMESTAMP),
        (8, 'DashboardBlockCommunicationLms',CURRENT_TIMESTAMP),
-       (9, 'DashboardBlockNewsLms',CURRENT_TIMESTAMP);
+       (9, 'DashboardBlockNewsLms',CURRENT_TIMESTAMP),
+	   (10, 'DashboardBlockCourseAttendanceGraphLms',CURRENT_TIMESTAMP);
 
 INSERT IGNORE INTO learning_middlearea (`obj_index`, `disabled`, `idst_list`, `sequence`)
 VALUES ('tb_dashboard', '1', 'a:0:{}', '0');
@@ -3535,6 +3536,38 @@ ALTER TABLE `core_field_son` ADD INDEX `idCommonSon_idFiled_langCode_idx` (`id_c
 
 
 ALTER TABLE learning_light_repo ADD repo_teacher_alert boolean default false;
+
+INSERT IGNORE INTO `core_menu` ( `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform` )
+VALUES
+    ( '_MANAGEMENT_COMMUNICATION', '', '4', TRUE, TRUE, (SELECT `idMenu` FROM ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_CONTENTS' LIMIT 1 ) tbl), NULL, 'framework' );
+
+ INSERT IGNORE INTO `core_menu` ( `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform` )
+VALUES
+    ( '_CATEGORIES', '', '1', TRUE, TRUE, (SELECT `idMenu` FROM ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_MANAGEMENT_COMMUNICATION' ) tbl), NULL, 'framework' );
+
+UPDATE `core_menu` SET `idParent` =  (SELECT `idMenu` FROM ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_MANAGEMENT_COMMUNICATION' LIMIT 1) tbl) WHERE name = '_COMMUNICATION_MAN';
+INSERT IGNORE INTO `core_menu_under` ( `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`, `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path` )
+VALUES
+    ( ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_CATEGORIES' LIMIT 1 ) ,
+    'communication',
+    '_CATEGORIES',
+    NULL,
+    'view',
+    'framework',
+    1,
+    NULL,
+    NULL,
+    'alms/communication/showCategories' 
+    );
+ALTER TABLE `learning_communication_category_lang` ADD COLUMN `description` TEXT AFTER `translation`;
+CREATE TABLE IF NOT EXISTS learning_communication_lang (
+    id_comm int,
+    lang_code varchar(255),
+    title varchar(255),
+    description text
+);
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
