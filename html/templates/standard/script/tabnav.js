4a0761f6fcc06ee51bdde95eeefedb39735d2375
LivioM <l.melfi@grifomultimedia.it>
2023-06-30 18:15:40 +0200

    # implemented tab navigation on my courses
    (cherry picked from commit 87601efd6e6b85d8cca08044ae7009aa9b8ff9fe)

--- html/templates/standard/script/tabnav.js
+++ html/templates/standard/script/tabnav.js
@@ -6,10 +6,71 @@ window.TabNav = (function ($) {
 
     $(document).on('click', '.js-tab-nav', function () {
       var _toggledClass = $(this).data('tab');
-
       $(this).addClass('selected').siblings().removeClass('selected');
       $('.user-tab--' + _toggledClass).addClass('is-visible').siblings().removeClass('is-visible');
 
     });
   });
 })(jQuery);
+
+
+let slider;
+let slider_button;
+let profile_button
+window.addEventListener('DOMContentLoaded', function() {
+  manageTabNavigation('#lms_menu_container',1);
+  manageTabNavigation('#middlearea',2);
+  manageTabNavigation('#c-menu--slide-right',-1);
+  slider = document.getElementById('c-menu--slide-right');
+  slider_button = document.getElementById('c-button--slide-right');
+  profile_button = document.getElementById('open_profile');
+
+  slider_button.addEventListener("click", function() {
+    manageTabNavigation('#lms_menu_container',-1);
+    manageTabNavigation('#middlearea',-1);
+    manageTabNavigation('#div_course',-1);
+    manageTabNavigation('#footer', -1);
+    manageTabNavigation('#c-menu--slide-right', 1);
+    profile_button.setAttribute('aria-hidden', 'false');
+  });
+
+  profile_button.addEventListener("click", function(event) {
+    manageTabNavigation('#lms_menu_container',1);
+    manageTabNavigation('#middlearea',2);
+    manageTabNavigation('#div_course',3);
+    manageTabNavigation('#footer', 4);
+    manageTabNavigation('#c-menu--slide-right', -1);
+    profile_button.setAttribute('aria-hidden', 'true');
+  });
+
+  slider.addEventListener('keydown', function(event) {
+    if (event.key === 'Tab') {
+      const focusableElements = slider.querySelectorAll('a, button, input');
+      const firstElement = focusableElements[0];
+      const lastElement = focusableElements[focusableElements.length - 1];
+
+      if (event.shiftKey && document.activeElement === firstElement) {
+        event.preventDefault();
+        lastElement.focus();
+      } else if (!event.shiftKey && document.activeElement === lastElement) {
+        event.preventDefault();
+        firstElement.focus();
+      }
+    }
+  });
+
+});
+
+function manageTabNavigation(selector, start_tab){
+  if (selector.startsWith('.')) {
+    divElement = document.querySelector(selector);
+  } else if (selector.startsWith('#')) {
+    var id = selector.slice(1); // Rimuovi il cancelletto dal selettore di ID
+    divElement = document.getElementById(id);
+  }
+  const elements = divElement.querySelectorAll('a, button, input');
+  let tabIndex = start_tab;
+  elements.forEach(function(element) {
+    element.setAttribute('tabindex', tabIndex);
+  });
+}
