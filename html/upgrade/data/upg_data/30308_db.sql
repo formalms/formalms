fe3ecc2cd438ca12327b4cbfe146f2dd9ca3d566
Giuseppe Nucifora <me@giuseppenucifora.com>
2023-08-01 08:38:15 +0200

    fix 3.3.8 add index query to solve if index exists

--- html/upgrade/data/upg_data/30308_db.sql
+++ html/upgrade/data/upg_data/30308_db.sql
@@ -1,1 +1,11 @@
-ALTER TABLE `learning_courseuser` ADD INDEX `courseuser_course_idx` (`idCourse`);
+select if (
+               exists(
+                       select distinct index_name from information_schema.statistics
+                       where table_schema = DATABASE()
+                         and table_name = 'learning_courseuser' and index_name like 'courseuser_course_idx'
+                   )
+           ,'select ''index courseuser_course_idx exists'' _______;'
+           ,'ALTER TABLE learning_courseuser ADD INDEX courseuser_course_idx (idCourse)') into @a;
+PREPARE stmt1 FROM @a;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
