0ab4c882cb23984b24ea56df78dffccaa10fc4e0
Giuseppe Nucifora <me@giuseppenucifora.com>
2024-05-17 17:11:24 +0200

    fix certificate

--- html/appLms/lib/lib.certificate.php
+++ html/appLms/lib/lib.certificate.php
@@ -329,7 +329,7 @@ class Certificate
         foreach ($res as $row) {
             if (
                 $this->certificateAvailableForUser($row['id_certificate'], $row['id_course'], $row['id_user'])
-                && $this->canRelExceptional($row['id_user'], $row['id_course'])
+                && $this->canRelExceptional($row['id_certificate'],$row['id_user'], $row['id_course'])
             ) {
                 $assignable[] = $row;
             }
