d45fd9a2cbc696843f1a5b28f594c4fe8695d2ae
Giuseppe Nucifora <me@giuseppenucifora.com>
2024-07-26 14:30:03 +0200

    fix api authentication not neded for explicit route

--- html/api/lib/api.user.php
+++ html/api/lib/api.user.php
@@ -18,6 +18,13 @@ require_once _base_ . '/api/lib/lib.api.php';
 
 class User_API extends API
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->needAuthentication = true;
+        $this->endpoingWithoutAuthentication = ['api/user/downloadCertificate'];
+    }
+
     protected function _getBranch($like, $parent = false, $lang_code = false)
     {
         if (!$like) {
@@ -33,7 +40,7 @@ class User_API extends API
         }
         $res = $this->db->query($query);
         if ($this->db->num_rows($res) > 0) {
-            list($output) = $this->db->fetch_row($res);
+            [$output] = $this->db->fetch_row($res);
 
             return $output;
         } else {
@@ -59,7 +66,7 @@ class User_API extends API
 
         $res = $this->db->query($query);
         if ($this->db->num_rows($res) > 0) {
-            list($output) = $this->db->fetch_row($res);
+            [$output] = $this->db->fetch_row($res);
 
             return $output;
         } else {
@@ -708,7 +715,7 @@ class User_API extends API
 
         $q = $this->db->query($qtxt);
         if ($q) {
-            list($tot) = $this->db->fetch_row($q);
+            [$tot] = $this->db->fetch_row($q);
             $output['users_count'] = $tot;
         } else {
             $output['success'] = false;
@@ -796,7 +803,7 @@ class User_API extends API
         $q = "SELECT idst from core_group where groupid = '/framework/adminrules/" . $profile_name . "'";
         $res = $this->db->query($q);
         if ($res) {
-            list($out) = $this->db->fetch_row($res);
+            [$out] = $this->db->fetch_row($res);
         }
 
         return $out;
@@ -817,7 +824,7 @@ class User_API extends API
         $q = 'SELECT count(*) as t from core_group_members where idst = 4 and idstMember =' . $idst;
         $res = $this->db->query($q);
         if ($res) {
-            list($out) = $this->db->fetch_row($res);
+            [$out] = $this->db->fetch_row($res);
         }
 
         return $out;
@@ -873,7 +880,7 @@ class User_API extends API
             $q = "select idst from core_group where core_group.groupid = '/" . $params['group_name'] . "'";
             $rs = $this->db->query($q);
             if ($rs) {
-                list($selected_group) = $this->db->fetch_row($rs);
+                [$selected_group] = $this->db->fetch_row($rs);
             }
         }
 
@@ -882,7 +889,7 @@ class User_API extends API
             $q = "select idst_ocd from core_org_chart_tree where code = '" . $params['orgchart_code'] . "'";
             $rs = $this->db->query($q);
             if ($rs) {
-                list($selected_group) = $this->db->fetch_row($rs);
+                [$selected_group] = $this->db->fetch_row($rs);
             }
         }
 
@@ -891,11 +898,11 @@ class User_API extends API
             $q = "select id_dir from core_org_chart where translation = '" . $params['orgchart_name'] . "' LIMIT 1";
             $rs = $this->db->query($q);
             if ($rs) {
-                list($id_org) = $this->db->fetch_row($rs);
+                [$id_org] = $this->db->fetch_row($rs);
                 $q = 'select idst_ocd from core_org_chart_tree where idOrg = ' . intval($id_org);
                 $rs = $this->db->query($q);
                 if ($rs) {
-                    list($selected_group) = $this->db->fetch_row($rs);
+                    [$selected_group] = $this->db->fetch_row($rs);
                 }
             }
         }
@@ -905,7 +912,7 @@ class User_API extends API
             $q = "select idst from core_user where userid = '/" . $params['single_user'] . "'";
             $rs = $this->db->query($q);
             if ($rs) {
-                list($selected_group) = $this->db->fetch_row($rs);
+                [$selected_group] = $this->db->fetch_row($rs);
             }
         }
 
@@ -1004,7 +1011,7 @@ class User_API extends API
 
         $rs = $this->db->query($query);
         if ($rs) {
-            list($idParent) = $this->db->fetch_row($rs);
+            [$idParent] = $this->db->fetch_row($rs);
         }
 
         return $idParent;
@@ -1208,7 +1215,7 @@ class User_API extends API
 
         $rs = $this->db->query($query);
         if ($rs) {
-            list($numChild) = $this->db->fetch_row($rs);
+            [$numChild] = $this->db->fetch_row($rs);
         }
 
         if ($numChild > 0) {
@@ -1235,7 +1242,8 @@ class User_API extends API
 
     public function downloadCertificate()
     {
-        $downloadString = end(explode('/', $this->request->get('q')));
+        $queryArray = explode('/', $this->request->get('q'));
+        $downloadString = end($queryArray);
         $fileName = SSLEncryption::decryptDownloadUrl($downloadString);
         $baseUrl = $_SERVER['DOCUMENT_ROOT'] . '/files/appLms/certificate/';
         $fileUrl = $baseUrl . $fileName;
