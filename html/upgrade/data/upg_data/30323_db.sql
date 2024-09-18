fd5970a1755d582ad3e83cf31d92048aa2315f35
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-09-17 22:50:19 -0300

    Allow update script to run twice
    Useful if update process fails, user can run this script anytime

--- html/upgrade/data/upg_data/30323_db.sql
+++ html/upgrade/data/upg_data/30323_db.sql
@@ -13,25 +13,31 @@ DELETE FROM learning_coursereport_score
                               
                                 );
 
--- DROP INDEX IF EXISTS
-SELECT COUNT(*) AS index_exists
-FROM information_schema.STATISTICS
-WHERE table_schema = DATABASE()
-  AND table_name = 'learning_coursereport'
-  AND index_name = 'unique_coursereport';
+-- Variables for table and constraint
+SET @table_name = DATABASE();
+SET @table_name = 'learning_coursereport';
+SET @constraint_name = 'unique_coursereport';
 
-SET @statement := IF(
-    @index_exists > 0,
-    -- 'SELECT "info: index exists."',
-    'DROP INDEX `unique_coursereport` ON `learning_coursereport`',
-    'SELECT "info: index does not exist."'
+-- Check if the constraint already exists
+SET @constraint_exists = (
+    SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_TYPE = 'UNIQUE'
+      AND TABLE_NAME = @table_name
+      AND CONSTRAINT_NAME = @constraint_name
 );
-PREPARE statement FROM @statement;
-EXECUTE statement;
-DEALLOCATE PREPARE statement;
 
-ALTER TABLE learning_coursereport
-        ADD CONSTRAINT unique_coursereport UNIQUE (source_of, id_course, id_source);
+-- Conditionally add the constraint if it does not exist
+SET @sql = IF(
+    @constraint_exists = 0,
+    CONCAT('ALTER TABLE ', @table_name, ' ADD CONSTRAINT ', @constraint_name, ' UNIQUE (source_of, id_course, id_source);'),
+    'SELECT "Constraint already exists.";'
+);
+
+-- Execute the SQL
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
 INSERT IGNORE INTO `core_setting` (`param_name`, `param_value`, `value_type`, `max_size`, `pack`, `regroup`, `sequence`, `param_load`, `hide_in_modify`, `extra_info`) VALUES
                         ('force_scorm_finish', 'on', 'enum', 3, '0', 4, 17, 1, 0, '');
@@ -50,26 +56,42 @@ UPDATE core_menu_under SET module_name = 'dashboard' WHERE default_name = '_DASH
 INSERT IGNORE INTO core_role ( idst, roleId )
 SELECT max(idst)+1, '/lms/course/public/dashboard/view' FROM core_st LIMIT 1;
 
-INSERT IGNORE INTO `core_menu` ( `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform` )
-VALUES
-    ( '_MANAGEMENT_COMMUNICATION', '', '4', TRUE, TRUE, (SELECT `idMenu` FROM ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_CONTENTS' LIMIT 1 ) tbl), NULL, 'framework' );
+SET @idParent = ( SELECT `idMenu` FROM `core_menu` WHERE `name` = '_CONTENTS' LIMIT 1 );
+INSERT INTO `core_menu` ( `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform` )
+SELECT * FROM (
+    SELECT '_MANAGEMENT_COMMUNICATION', '', '4', TRUE AS `true1`, TRUE AS `true2`, @idParent, NULL, 'framework' 
+ ) AS tmp
+WHERE NOT EXISTS (
+    SELECT 'idParent' FROM `core_menu`
+        WHERE `name` = '_MANAGEMENT_COMMUNICATION'
+) LIMIT 1;
 
-INSERT IGNORE INTO `core_menu` ( `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform` )
-VALUES
-    ( '_CATEGORIES', '', '1', TRUE, TRUE, (SELECT `idMenu` FROM ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_MANAGEMENT_COMMUNICATION' ) tbl), NULL, 'framework' );
+SET @idParent = ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_MANAGEMENT_COMMUNICATION' LIMIT 1 );
+INSERT INTO `core_menu` ( `name`, `image`, `sequence`, `is_active`, `collapse`, `idParent`, `idPlugin`, `of_platform` )
+SELECT * FROM (
+    SELECT '_CATEGORIES', '', '1', TRUE AS `true1`, TRUE AS `true2`, @idParent, NULL, 'framework' 
+ ) AS tmp
+WHERE NOT EXISTS (
+    SELECT 'idParent' FROM `core_menu`
+        WHERE `name` = '_CATEGORIES'
+) LIMIT 1;
 
 UPDATE `core_menu` SET `idParent` =  (SELECT `idMenu` FROM ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_MANAGEMENT_COMMUNICATION' LIMIT 1) tbl) WHERE name = '_COMMUNICATION_MAN';
 
-INSERT IGNORE INTO `core_menu_under` ( `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`, `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path` )
-VALUES
-    ( ( SELECT `idMenu` FROM `core_menu` WHERE NAME = '_CATEGORIES' LIMIT 1 ) ,
+SET @idMenu = ( SELECT `idMenu` FROM `core_menu` WHERE `name` = '_CATEGORIES' LIMIT 1 );
+INSERT INTO `core_menu_under` ( `idMenu`, `module_name`, `default_name`, `default_op`, `associated_token`, `of_platform`, `sequence`, `class_file`, `class_name`, `mvc_path` )
+SELECT * FROM (SELECT @idMenu,
     'communication',
     '_CATEGORIES',
-    NULL,
+    NULL AS `null1`,
     'view',
     'framework',
     1,
-    NULL,
-    NULL,
+    NULL AS `null2`,
+    NULL AS `null3`,
     'alms/communication/showCategories'
-    );
+    ) AS tmp
+WHERE NOT EXISTS (
+    SELECT `idMenu` FROM `core_menu_under`
+        WHERE `idMenu` = @idMenu
+) LIMIT 1;
